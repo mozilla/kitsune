a2d94c80404cbc1fa30757817cf1e0e1691788b0
Erik Rose <erik@mozilla.com>
2011-02-11 15:06:31 -0800

    [bug 633628] Add admin UIs for Watch and WatchFilter for QA.
    Also add a unicode representation for WatchFilters so they show up in the admin UI informatively.

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 
-from notifications.models import EventWatch
+from notifications.models import EventWatch, Watch, WatchFilter
 
 
 class EventWatchAdmin(admin.ModelAdmin):
-    list_filter = ('content_type', 'event_type', 'locale')
+    list_filter = ['content_type', 'event_type', 'locale']
+
+
+class FilterInline(admin.TabularInline):
+    model = WatchFilter
+
+
+class WatchAdmin(admin.ModelAdmin):
+    list_filter = ['content_type', 'event_type']
+    raw_id_fields = ['user']
+    inlines = [FilterInline]
+
+
+class WatchFilterAdmin(admin.ModelAdmin):
+    list_filter = ['name']
+    raw_id_fields = ['watch']
 
 
 admin.site.register(EventWatch, EventWatchAdmin)
+admin.site.register(Watch, WatchAdmin)
+admin.site.register(WatchFilter, WatchFilterAdmin)
