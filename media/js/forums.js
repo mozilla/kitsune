20818db2796f38be93010b05b211e6fed1286504
James Socol <james@mozilla.com>
2011-02-24 17:39:28 -0500

    Make 'Reply' links in the forums quote the post. [bug 634600]

--- media/js/forums.js
+++ media/js/forums.js
@@ -8,6 +8,21 @@
 
     function init() {
         Marky.createSimpleToolbar('.editor-tools', '#reply-content, #id_content');
+        $('span.post-action a.reply').click(function() {
+            var post = $(this).data('post'),
+                $post = $('#post-' + post),
+                text = $post.find('div.content-raw').text(),
+                user = $post.find('a.author-name').text(),
+                reply = template("''{user} [[#post-{post}|said]]''\n<blockquote>\n{text}\n</blockquote>\n\n"),
+                reply_text,
+                $textarea = $('#id_content'),
+                oldtext = $textarea.val();
+
+            reply_text = reply({'user': user, 'post': post, 'text': text});
+
+            $textarea.val(oldtext + reply_text);
+            return true;
+        });
     }
 
     $(document).ready(init);
--- settings.py
+++ settings.py
@@ -362,7 +362,7 @@ MINIFY_BUNDLES = {
             'global/mobile.css',
             'css/mobile.css',
             'css/wiki_syntax.css',
-        )
+        ),
     },
     'js': {
         'common': (
@@ -373,6 +373,7 @@ MINIFY_BUNDLES = {
             'js/kbox.js',
             'global/menu.js',
             'js/main.js',
+            'js/format.js',
         ),
         'libs/jqueryui': (
             'js/libs/jqueryui.min.js',
