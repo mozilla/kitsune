cc0335ef9f971c4eca2803456666802a21ce2ee5
Erik Rose <erik@mozilla.com>
2011-03-01 13:21:50 -0800

    [bug 637836] Delete orphaned reply tweets so adding foreign key contraint doesn't fail.

--- migrations/86-tweet-relationships.sql
+++ migrations/86-tweet-relationships.sql
@@ -1,3 +1,12 @@
+-- Delete orphaned replies so the foreign key we're about to add doesn't freak
+-- out:
+CREATE TEMPORARY TABLE tweets_to_delete (id int)
+    SELECT child.id FROM customercare_tweet child
+        LEFT JOIN customercare_tweet parent ON child.reply_to=parent.tweet_id
+        WHERE parent.id IS NULL AND
+              child.reply_to IS NOT NULL;
+DELETE FROM customercare_tweet WHERE id IN (SELECT id FROM tweets_to_delete);
+
 -- Remove surrogate key, and use tweet ID instead. Make reply_to a foreign key,
 -- and rename it to conform to our conventions.
 ALTER TABLE customercare_tweet DROP id, ADD PRIMARY KEY (tweet_id);
