a1c8afeecfac619d3b36ad02a8926fd378327a4c
James Socol <james@mozilla.com>
2011-03-08 11:06:39 -0500

    Try to pick the best language [bug 639818]
    Given Accept-Language: en-gb, en;q=0.8, fr-fr;q=0.6, es;q=0.2, don't pick 'es'.

--- apps/sumo/tests/test_locale_middleware.py
+++ apps/sumo/tests/test_locale_middleware.py
@@ -1,4 +1,7 @@
+from nose.tools import eq_
+
 from sumo.tests import TestCase
+from sumo.urlresolvers import get_best_language
 
 
 class TestLocaleMiddleware(TestCase):
@@ -49,3 +52,32 @@ class TestLocaleMiddleware(TestCase):
         response = self.client.get('/search', follow=True,
                                    HTTP_ACCEPT_LANGUAGE='en-US,fr;q=0.3')
         self.assertRedirects(response, '/en-US/search', status_code=302)
+
+
+class BestLanguageTests(TestCase):
+    def test_english_only(self):
+        best = get_best_language('en-us, en;q=0.8')
+        eq_('en-US', best)
+
+    def test_en_GB(self):
+        """Stick with English if you can."""
+        best = get_best_language('en-gb, fr;q=0.8')
+        eq_('en-US', best)
+
+    def test_not_worst_choice(self):
+        """Try not to fall back to 'es' here."""
+        best = get_best_language('en-gb, en;q=0.8, fr-fr;q=0.6, es;q=0.2')
+        eq_('en-US', best)
+
+    def test_fr_FR(self):
+        best = get_best_language('fr-FR, es;q=0.8')
+        eq_('fr', best)
+
+    def test_non_existent(self):
+        best = get_best_language('xx-YY, xx;q=0.8')
+        eq_(False, best)
+
+    def test_prefix_matching(self):
+        """en-US is a better match for en-gb, es;q=0.2 than es."""
+        best = get_best_language('en-gb, es;q=0.2')
+        eq_('en-US', best)
