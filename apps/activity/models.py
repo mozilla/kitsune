6e4ff7d016ca5e0367561284cc56be03a0712f29
James Socol <james@mozilla.com>
2011-03-10 15:38:51 -0500

    New method for storing and formatting actions.
    * Drop the `Activity` class and table, replace with `Action`.
    * And associated renaming.
    * Define the concept of an `ActionFormatter` and a base class.
    * Add a README.

--- apps/activity/models.py
+++ apps/activity/models.py
@@ -8,29 +8,46 @@ from django.db import models
 from sumo.models import ModelBase
 
 
-class Activity(ModelBase):
+class Action(ModelBase):
     """Represents a unit of activity in a user's 'inbox.'"""
-    user = models.ForeignKey(User, related_name='activity_inbox')
+    users = models.ManyToManyField(User, related_name='action_inbox')
     creator = models.ForeignKey(User, null=True, blank=True,
-                                related_name='activity')
+                                related_name='actions')
     created = models.DateTimeField(default=datetime.now, db_index=True)
-    title = models.CharField(max_length=120)
-    content = models.CharField(max_length=400, blank=True)
+    data = models.CharField(max_length=400, blank=True)
     url = models.URLField(null=True, blank=True)
     content_type = models.ForeignKey(ContentType, null=True, blank=True)
     object_id = models.PositiveIntegerField(null=True, blank=True)
     content_object = generic.GenericForeignKey()
+    formatter_cls = models.CharField(max_length=200,
+                                     default='activity.ActionFormatter')
 
     class Meta(object):
         ordering = ['-created']
 
+    @property
+    def formatter(self):
+        if not hasattr(self, 'fmt'):
+            mod, _, cls = self.formatter_cls.rpartition('.')
+            fmt_cls = getattr(__import__(mod, fromlist=[cls]), cls)
+            self.fmt = fmt_cls(self)
+        return self.fmt
+
     def __unicode__(self):
-        return self.title
+        return unicode(self.formatter)
+
+    @property
+    def title(self):
+        return self.formatter.title
+
+    @property
+    def content(self):
+        return self.formatter.content
 
     def get_absolute_url(self):
         return self.url
 
 
-class ActivityMixin(object):
+class ActionMixin(object):
     """Add a GenericRelation to a model."""
-    activity = generic.GenericRelation(Activity)
+    actions = generic.GenericRelation(Action)
