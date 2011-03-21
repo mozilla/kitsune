f0fae3ed403e8420b59683a6b8574b99b5b48341
Ricky Rosario <rickyrosario@gmail.com>
2011-03-21 10:53:36 -0400

    "My profile" now shows the dashboard tabs. Replaced Profile link with a Dashboard link. [bug 643407]

--- apps/users/tests/test_templates.py
+++ apps/users/tests/test_templates.py
@@ -328,7 +328,7 @@ class ViewProfileTests(TestCaseBase):
         r = self.client.get(reverse('users.profile', args=[47963]))
         eq_(200, r.status_code)
         doc = pq(r.content)
-        eq_('Edit my profile', doc('#doc-tabs li a').eq(1).text())
+        eq_('Edit my profile', doc('#doc-tabs li:last').text())
         self.client.logout()
 
 
