2c5c76f3c3793d7544c8c917f81e080c6b1fb9e7
James Socol <james@mozilla.com>
2011-03-21 09:50:34 -0400

    Name of Taiwan.

--- lib/countries.py
+++ lib/countries.py
@@ -215,7 +215,7 @@ COUNTRIES = (
     ('SE', 'Sweden'),
     ('CH', 'Switzerland'),
     ('SY', 'Syrian Arab Republic'),
-    ('TW', 'Taiwan, Province of China'),
+    ('TW', 'Taiwan'),
     ('TJ', 'Tajikistan'),
     ('TZ', 'Tanzania, United Republic of'),
     ('TH', 'Thailand'),
