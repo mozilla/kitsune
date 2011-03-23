98b1ee2b48af7aeb2eac038a96eb3874824e172c
Ricky Rosario <rickyrosario@gmail.com>
2011-03-23 11:49:31 -0400

    Hide not-yet-ready dashboards. [bug 643580]

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import redirect_to
 urlpatterns = patterns('dashboards.views',
     url(r'^dashboard$', redirect_to, {'url': 'dashboard/forums'}),
     url(r'^dashboard/forums$', 'review', name='dashboards.review'),
-    url(r'^dashboard/questions$', 'questions', name='dashboards.questions'),
+    #url(r'^dashboard/questions$', 'questions', name='dashboards.questions'),
     url(r'^localization$', 'localization', name='dashboards.localization'),
     url(r'^contributors$', 'contributors', name='dashboards.contributors'),
     url(r'^wiki-rows/(?P<readout_slug>[^/]+)', 'wiki_rows',
