8b05d662326696122e653184fd8f4ba772f8af5a
Erik Rose <erik@mozilla.com>
2011-05-30 17:51:41 -0700

    Receives messages okay.
    Hitting Reload on a 2nd chat window causes the first to start receiving duplicates (of messages sent from anywhere).

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -47,21 +47,27 @@ def make_nonce():
 
 # TODO: Kill all the middleware that's doing SQL blockets.
 def socketio(request):
+    def subscriber(io):
+        try:
+            redis_in = redis_client('chat')
+            redis_in.subscribe('world')
+            redis_in_generator = redis_in.listen()
+            while io.connected():
+                for from_redis in redis_in_generator:
+                    print 'Incoming: %s' % from_redis
+                    if from_redis['type'] == 'message':  # There are also subscription notices.
+                        io.send(from_redis['data'])
+        finally:
+            print "EXIT SUBSCRIBER %s" % io.session
+
     CHANNEL = 'world'
     io = request.environ['socketio']
-    # Do some setup the first time:
     if io.on_connect():
         print "CONNECT %s" % io.session
     else:
-        print "SOMETHING OTHER THAN CONNECT!"
+        print "SOMETHING OTHER THAN CONNECT!"  # I have never seen this happen.
 
-#     def subscriber(io, redis_in_generator):
-#         while True:
-#             for from_redis in redis_in_generator:
-#                 print 'Incoming: %s' % from_redis
-#                 if from_redis['type'] == 'message':  # There are also subscription notices.
-#                     io.send(from_redis['data'])
-#     in_greenlet = Greenlet.spawn(subscriber, io, redis_in_generator)  # TODO: Need to hang onto a ref to keep it from the GC?
+    in_greenlet = Greenlet.spawn(subscriber, io)  # TODO: Need to hang onto a ref to keep it from the GC?
 
     # Now run forever and service this long-term request?
     while io.connected():
@@ -73,6 +79,9 @@ def socketio(request):
 
     print "EXIT %s" % io.session
 
-    #in_greenlet.kill()
+    # Each time I close the 2nd chat window, wait for the old socketio() view to exit, and then reopen the chat page, the number of Incomings increases by one. The subscribers are never exiting. Thus...
+    in_greenlet.kill()  # Should fall out on its own once not io.connected() but doesn't.
 
     return HttpResponse()
+
+# TODO: Hitting Reload on the 2nd chat window causes the first to start receiving duplicates (of messages sent from anywhere). Does this happen even logged in as different users or on different IPs? Aha, ultimately (like in longer than 5 minutes. I don't know if it's time-determined.), the superfluous listeners and subscribers exit with something like this: EXIT session_id='422908985144' client_queue[1] server_queue[1] hits=120 heartbeats=100.
