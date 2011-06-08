1536f5ed7d4b52e9307a76c9492975b07ed760f6
James Socol <james@mozilla.com>
2011-06-08 18:22:53 -0400

    Tolerate missing Redis servers. [bug 662942]

--- apps/kadmin/admin.py
+++ apps/kadmin/admin.py
@@ -10,6 +10,7 @@ from django.views import debug
 
 import celery.conf
 import jinja2
+from redis import ConnectionError
 
 from sumo.utils import redis_client
 
@@ -71,12 +72,16 @@ def redis_info(request):
     for key in django_settings.REDIS_BACKENDS.keys():
         redis_info[key] = {}
         client = redis_client(key)
-        cfg = client.config_get()
-        redis_info[key]['config'] = [{'key': k, 'value': cfg[k]} for k in
-                                     sorted(cfg)]
-        info = client.info()
-        redis_info[key]['info'] = [{'key': k, 'value': info[k]} for k in
-                                   sorted(info)]
+        redis_info[key]['connection'] = django_settings.REDIS_BACKENDS[key]
+        try:
+            cfg = client.config_get()
+            redis_info[key]['config'] = [{'key': k, 'value': cfg[k]} for k in
+                                         sorted(cfg)]
+            info = client.info()
+            redis_info[key]['info'] = [{'key': k, 'value': info[k]} for k in
+                                       sorted(info)]
+        except ConnectionError:
+            redis_info[key]['down'] = True
 
     return render_to_response('kadmin/redis.html',
                               {'redis_info': redis_info,
