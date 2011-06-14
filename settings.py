61cca2fdf5153a4eb6d6ee9b30cd36b22e3b7a1c
Ricky Rosario <rickyrosario@gmail.com>
2011-06-14 11:04:41 -0400

    Updated vendor and enableded view timing middleware.

--- settings.py
+++ settings.py
@@ -178,6 +178,7 @@ MIDDLEWARE_CLASSES = (
     'twitter.middleware.SessionMiddleware',
     'sumo.middleware.PlusToSpaceMiddleware',
     'commonware.middleware.HidePasswordOnException',
+    'commonware.response.middleware.GraphiteRequestTimingMiddleware',
 )
 
 # Auth
