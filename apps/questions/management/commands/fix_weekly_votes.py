f66a2d797cd17d1ea8d9a402a27715eb704ac240
James Socol <james@mozilla.com>
2011-06-15 14:21:27 -0400

    Add a command to synchronize weekly vote count.

---
+++ apps/questions/management/commands/fix_weekly_votes.py
@@ -0,0 +1,23 @@
+import time
+
+from django.core.management.base import NoArgsCommand
+from django.db import connection
+
+
+class Command(NoArgsCommand):
+    help = 'Recalculate question weekly votes.'
+
+    def handle_noargs(self, *a, **kw):
+        cursor = connection.cursor()
+        start = time.time()
+        rows = cursor.execute("""
+            UPDATE questions_question q
+            SET num_votes_past_week = (
+                SELECT COUNT(created)
+                FROM questions_questionvote qv
+                WHERE qv.question_id = q.id
+                AND qv.created >= SUBDATE(NOW(), 7)
+            );
+        """)
+        d = time.time() - start
+        print u'Updated %d rows in %0.3f seconds.' % (rows, d)
