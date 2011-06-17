c7d80dbbb96115e09a75aff6d8b04e42629d5d0e
Tanay Gavankar <tgavankar@gmail.com>
2011-06-17 13:31:57 -0400

    Style fixes.

--- apps/wiki/tests/test_cron.py
+++ apps/wiki/tests/test_cron.py
@@ -4,29 +4,33 @@ import waffle
 from sumo.tests import TestCase
 from wiki.cron import migrate_helpfulvotes
 from wiki.models import (HelpfulVote, HelpfulVoteOld)
-from wiki.tests import document, revision
+from wiki.tests import revision
 from users.tests import user
 
 
+anon_id = '69beab04be927353c2f0046db2232643'
+ua = '''Mozilla/5.0 (Windows; U; Windows NT 6.1;
+        pt-BR; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'''
+
+
 class HelpfulMigrationTestCase(TestCase):
+
     @mock.patch.object(waffle, 'switch_is_active')
     def test_normal_migrate_anon(self, switch_is_active):
         switch_is_active.return_value = True
         rev = revision()
         rev.save()
         old = HelpfulVoteOld()
-        old.document=rev.document
-        old.helpful=1
-        old.anonymous_id='69beab04be927353c2f0046db2232643'
-        old.user_agent='''Mozilla/5.0 (Windows; U; Windows NT 6.1;
-                        pt-BR; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'''
+        old.document = rev.document
+        old.helpful = 1
+        old.anonymous_id = anon_id
+        old.user_agent = ua
         old.save()
         migrate_helpfulvotes()
         new = HelpfulVote.objects.filter(revision=rev,
             helpful=1,
-            anonymous_id='69beab04be927353c2f0046db2232643',
-            user_agent='''Mozilla/5.0 (Windows; U; Windows NT 6.1;
-                        pt-BR; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'''
+            anonymous_id=anon_id,
+            user_agent=ua
             )
         assert new.exists()
 
@@ -41,11 +45,10 @@ class HelpfulMigrationTestCase(TestCase):
         usr = user(save=True)
 
         old = HelpfulVoteOld()
-        old.document=rev.document
-        old.helpful=1
-        old.creator=usr
-        old.user_agent='''Mozilla/5.0 (Windows; U; Windows NT 6.1;
-                        pt-BR; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'''
+        old.document = rev.document
+        old.helpful = 1
+        old.creator = usr
+        old.user_agent = ua
         old.save()
 
         migrate_helpfulvotes()
@@ -53,8 +56,7 @@ class HelpfulMigrationTestCase(TestCase):
         new = HelpfulVote.objects.filter(revision=rev,
             helpful=1,
             creator=usr,
-            user_agent='''Mozilla/5.0 (Windows; U; Windows NT 6.1;
-                        pt-BR; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'''
+            user_agent=ua
             )
         assert new.exists()
 
