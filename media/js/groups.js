9942b046f039feea78f10895aebe1b3e21ea4a81
Ricky Rosario <rickyrosario@gmail.com>
2011-06-17 12:20:31 -0400

    Made group leaders editable from profile page. [bug 663809]

--- media/js/groups.js
+++ media/js/groups.js
@@ -10,6 +10,26 @@ function init() {
     // Marky for information edit:
     var buttons = Marky.allButtonsExceptShowfor();
     Marky.createCustomToolbar('.editor-tools', '#id_information', buttons);
+
+    initInlineEditing();
+}
+
+function initInlineEditing() {
+    // Enable managing of member and leader lists.
+    $('.editable a.edit').each(function() {
+        var $this = $(this),
+            originalText = $this.text();
+        $this.click(function(ev){
+            var $container = $this.closest('.editable');
+            $container.toggleClass('edit-on');
+            if ($container.hasClass('edit-on')) {
+                $this.text(gettext('Cancel'));
+            } else {
+                $this.text(originalText);
+            }
+            ev.preventDefault();
+        });
+    });
 }
 
 $(document).ready(init);
