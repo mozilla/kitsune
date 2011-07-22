2c222864cfafd3844d75f7e69adf45202654865d
Ricky Rosario <rickyrosario@gmail.com>
2011-07-22 15:41:21 -0400

    New global header (behind 'new-header' flag for now). [bug 669840]

--- apps/landings/tests/test_templates.py
+++ apps/landings/tests/test_templates.py
@@ -8,7 +8,7 @@ from sumo.urlresolvers import reverse
 class MobileHomeTestCase(TestCase):
     def test_top_text(self):
         response = self.client.get(reverse('home.mobile'), follow=True)
-        self.assertContains(response, 'Firefox Help for Mobile')
+        self.assertContains(response, 'Firefox for Mobile')
 
     def test_no_plugin_check(self):
         response = self.client.get(reverse('home.mobile'), follow=True)
--- apps/sumo/tests/test_templates.py
+++ apps/sumo/tests/test_templates.py
@@ -80,6 +80,8 @@ class BaseTemplateTests(MockRequestTests):
         html = jingo.render_to_string(self.request, self.template)
         doc = pq(html)
         eq_(0, len(doc('div#greeting').text()))
+        # TODO: when launching new-header, change above for:
+        # eq_(0, len(doc('li.account')))
 
     @mock.patch.object(settings._wrapped, 'READ_ONLY', False)
     def test_not_readonly_login_link_enabled(self):
@@ -87,6 +89,8 @@ class BaseTemplateTests(MockRequestTests):
         html = jingo.render_to_string(self.request, self.template)
         doc = pq(html)
         assert len(doc('div#greeting').text()) > 0
+        # TODO: when launching new-header, change above for:
+        # assert len(doc('li.account')) > 0
 
 
 class ErrorListTests(MockRequestTests):
