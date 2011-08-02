112f0003eb0349eaf1574aeb31ba41e05f258566
Erik Rose <erik@mozilla.com>
2011-08-02 16:21:24 -0700

    [bug 675628] Add FF7 to Help With menu.

--- apps/wiki/models.py
+++ apps/wiki/models.py
@@ -69,15 +69,14 @@ CATEGORIES = (
 # FF versions used to filter article searches, power {for} tags, etc.:
 #
 # Iterables of (ID, name, abbreviation for {for} tags, max version this version
-# group encompasses, and whether-this-version-should-show-in-the-menu) grouped
-# into optgroups by platform. To add the ability to sniff a new version of an
-# existing browser (assuming it doesn't change the user agent string too
-# radically), you should need only to add a line here; no JS required. Just be
-# wary of inexact floating point comparisons when setting max_version, which
-# should be read as "From the next smaller max_version up to but not including
-# version x.y".
+# group encompasses, whether-this-version-should-show-in-the-menu, and whether-
+# this-version-is-the-default-on-this-platform), grouped into optgroups by
+# platform. To add the ability to sniff a new version of an existing browser
+# (assuming it doesn't change the user agent string too radically), you should
+# need only to add a line here; no JS required. Just be wary of inexact
+# floating point comparisons when setting max_version, which should be read as
+# "From the next smaller max_version up to but not including version x.y".
 #
-# The first browser in each platform group will be considered the default one.
 # When a wiki page is being viewed in a desktop browser, the {for} sections for
 # the default mobile browser still show. The reverse is true when a page is
 # being viewed in a mobile browser.
@@ -87,7 +86,7 @@ VersionMetadata = namedtuple('VersionMetadata',
 GROUPED_FIREFOX_VERSIONS = (
     ((_lazy(u'Desktop:'), 'desktop'), (
         VersionMetadata(9, _lazy(u'Firefox 7'),
-                        _lazy(u'Firefox 7'), 'fx7', 7.9999, False, False),
+                        _lazy(u'Firefox 7'), 'fx7', 7.9999, True, False),
         VersionMetadata(6, _lazy(u'Firefox 6'),
                         _lazy(u'Firefox 6'), 'fx6', 6.9999, True, False),
         VersionMetadata(5, _lazy(u'Firefox 5'),
