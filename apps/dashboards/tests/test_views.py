d78cf07b4a137dc3b3c540a2f3ae61da8c670e4b
Tanay Gavankar <tgavankar@gmail.com>
2011-08-09 20:12:23 -0400

    [Bug 663244] Aggregate Metrics Dashboard

--- apps/dashboards/tests/test_views.py
+++ apps/dashboards/tests/test_views.py
@@ -1,12 +1,30 @@
+from datetime import timedelta
+import json
+
+from django.conf import settings
+
+from nose import SkipTest
 from nose.tools import eq_
 
+from dashboards.cron import cache_most_unhelpful_kb_articles
 from dashboards.readouts import CONTRIBUTOR_READOUTS
 from sumo.tests import TestCase
 from sumo.urlresolvers import reverse
+from sumo.utils import redis_client
 from users.tests import user, group
+from wiki.models import HelpfulVote
+from wiki.tests import revision
 
 
 class LocalizationDashTests(TestCase):
+    def setUp(self):
+        super(LocalizationDashTests, self).setUp()
+        try:
+            self.redis = redis_client('helpfulvotes')
+            self.redis.flushdb()
+        except (KeyError, AttributeError):
+            pass
+
     def test_redirect_to_contributor_dash(self):
         """Should redirect to Contributor Dash if the locale is the default"""
         response = self.client.get(reverse('dashboards.localization',
@@ -29,6 +47,63 @@ class ContributorDashTests(TestCase):
             args=[CONTRIBUTOR_READOUTS[CONTRIBUTOR_READOUTS.keys()[0]].slug],
             locale='en-US'))
         eq_(200, response.status_code)
+
+
+def _add_vote_in_past(rev, vote, days_back):
+    v = HelpfulVote(revision=rev, helpful=vote)
+    v.created = v.created - timedelta(days=days_back)
+    v.save()
+
+
+class HelpfulVotesGraphTests(TestCase):
+    def setUp(self):
+        super(HelpfulVotesGraphTests, self).setUp()
+        self.user = user(save=True)
+        self.client.login(username=self.user.username, password='testpass')
+        self.group = group(name='Contributors', save=True)
+        # Without this, there were unrelated failures with l10n dashboard
+        try:
+            self.redis = redis_client('helpfulvotes')
+            self.redis.flushdb()
+            self.REDIS_KEY = settings.HELPFULVOTES_UNHELPFUL_KEY
+        except (KeyError, AttributeError):
+            raise SkipTest
+
+    def tearDown(self):
+        try:
+            self.redis.flushdb()
+        except (KeyError, AttributeError):
+            raise SkipTest
+        super(HelpfulVotesGraphTests, self).tearDown()
+
+    def test_no_data(self):
+        response = self.client.get(
+                                  reverse('dashboards.get_helpful_graph_async',
+                                  locale='en-US'))
+        eq_(200, response.status_code)
+        result = json.loads(response.content)
+        eq_(0, len(result['data'][0]['data']))
+
+    def test_response_data(self):
+        r = revision(save=True)
+        r.created = r.created - timedelta(days=90)
+        r.save()
+
+        for x in range(0, 4):
+            _add_vote_in_past(r, 0, 15)
+
+        cache_most_unhelpful_kb_articles()
+
+        response = self.client.get(
+                                  reverse('dashboards.get_helpful_graph_async',
+                                  locale='en-US'))
+        eq_(200, response.status_code)
+        result = json.loads(response.content)
+
+        eq_(1, len(result['data'][0]['data']))
+        eq_(r.document.title, result['data'][0]['data'][0]['title'])
+        eq_(0.0, result['data'][0]['data'][0]['colorsize'])
+        eq_('0.00', result['data'][0]['data'][0]['currperc'])
 
 
 class DefaultDashboardRedirect(TestCase):
