c0921513ae90b0b28a61adb1e29f865ce9d1324d
Tanay Gavankar <tgavankar@gmail.com>
2011-08-18 13:37:38 -0400

    Fix migration 121

--- migrations/121-importantdate-permissions.sql
+++ migrations/121-importantdate-permissions.sql
@@ -1,7 +1,7 @@
-INSERT INTO django_content_type (`name`, `app_label`, `model`) VALUES
+INSERT IGNORE INTO django_content_type (`name`, `app_label`, `model`) VALUES
     ('important date', 'wiki', 'importantdate');
 
-SET @ct = LAST_INSERT_ID();
+SET @ct = (SELECT id FROM django_content_type WHERE model='importantdate');
 
 INSERT INTO auth_permission (`name`, `content_type_id`, `codename`) VALUES
     ('Can add important dates', @ct, 'add_importantdate'),
