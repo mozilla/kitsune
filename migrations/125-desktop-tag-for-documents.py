da0d12e94e1e901eba69e64077a4846263c1b6e0
Ricky Rosario <rickyrosario@gmail.com>
2011-08-24 10:24:50 -0400

    added IGNORE

--- migrations/125-desktop-tag-for-documents.py
+++ migrations/125-desktop-tag-for-documents.py
@@ -1,7 +1,7 @@
 SET @ct = (SELECT id from django_content_type WHERE name='document' AND app_label='wiki');
 SET @tag = (SELECT id FROM taggit_tag WHERE name = 'desktop');
 
-INSERT INTO taggit_taggeditem (tag_id, object_id, content_type_id)
+INSERT IGNORE INTO taggit_taggeditem (tag_id, object_id, content_type_id)
     SELECT DISTINCT @tag, document_id, @ct FROM wiki_firefoxversion WHERE item_id IN (1,2,3,5,6,9)
     UNION
     SELECT DISTINCT @tag, document_id, @ct FROM wiki_operatingsystem WHERE item_id IN (1,2,3);
