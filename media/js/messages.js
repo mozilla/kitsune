e6323bcd5e2af84a323546d31441fb5829c2e5b7
Tim Watts <tim@readevalprint.com>
2011-09-29 23:24:21 +0000

    [bug 663501] made texarea big on focus and added jquery.autoexpand

--- media/js/messages.js
+++ media/js/messages.js
@@ -1,16 +1,23 @@
 /*
+ * Make textarea in replies auto expanding.
  * Private messaging.
  */
 
-(function($){
+$(document).ready(function(){
+    // Hide reply button and shrink the textarea
+    var $area = $('#read-message textarea#id_message');
+    $area.attr('placeholder', gettext('Reply...'));
+    $('#read-message .editor-tools').hide();
+    $('#read-message input[type=submit]').hide();
 
-    function init() {
+    // Show the orginal button and expanding extarea
+    $area.one('focus',function(){
+        $area.autoResize();
+        $('#read-message .editor-tools').show();
+        $('#read-message input[type=submit]').show();
         Marky.createSimpleToolbar('.editor-tools', '#id_message');
         new k.AjaxPreview($('#preview-btn'), {
             changeHash: false
         });
-    }
-
-    $(document).ready(init);
-
-}(jQuery));
+    })
+});
