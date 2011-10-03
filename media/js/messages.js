53d73a773be171bbd6a6c3d95e7d10c1e6d2d33a
Tim Watts <tim@readevalprint.com>
2011-10-03 18:30:38 +0000

    fix for new message preview

--- media/js/messages.js
+++ media/js/messages.js
@@ -4,18 +4,24 @@
  */
 
 $(document).ready(function(){
-    // Hide reply button and shrink the textarea
+    // Show the ajax preview on the new message page.
+    Marky.createSimpleToolbar('#new-message .editor-tools', '#id_message');
+    new k.AjaxPreview($('#preview-btn'), {
+        changeHash: false
+    });
+
+    // Hide reply button and shrink the textarea.
     var $area = $('#read-message textarea#id_message');
     $area.attr('placeholder', gettext('Reply...'));
     $('#read-message .editor-tools').hide();
     $('#read-message input[type=submit]').hide();
 
-    // Show the orginal button and expanding extarea
+    // Show the orginal button and expanding textarea.
     $area.one('focus',function(){
         $area.autoResize();
         $('#read-message .editor-tools').show();
         $('#read-message input[type=submit]').show();
-        Marky.createSimpleToolbar('.editor-tools', '#id_message');
+        Marky.createSimpleToolbar('#read-message .editor-tools', '#id_message');
         new k.AjaxPreview($('#preview-btn'), {
             changeHash: false
         });
