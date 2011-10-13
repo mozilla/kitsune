17a7a9567033725b1cf202a8f7505ec03d33b946
Ricky Rosario <rickyrosario@gmail.com>
2011-10-13 10:17:35 -0400

    [bug 694263] Missing author_post_count on post preview.

--- apps/forums/views.py
+++ apps/forums/views.py
@@ -463,5 +463,6 @@ def post_preview_async(request):
     """Ajax preview of posts."""
     statsd.incr('forums.preview')
     post = Post(author=request.user, content=request.POST.get('content', ''))
+    post.author_post_count = post.author.post_set.count()
     return jingo.render(request, 'forums/includes/post_preview.html',
                         {'post_preview': post})
