a6fccd38fe00214d563f4189b55c24db95822cd9
Tim Watts <tim@readevalprint.com>
2011-10-14 22:55:15 +0000

    added js/css to wiki flagging

--- media/js/wiki.js
+++ media/js/wiki.js
@@ -35,6 +35,7 @@
 
         initDiffPicker();
         initDiffToggle();
+        initReportPost();
 
         Marky.createFullToolbar('.editor-tools', '#id_content');
 
@@ -401,6 +402,66 @@
                 .attr('value', referrer))
             .append($('<input type="hidden" name="query"/>')
                 .attr('value', query));
+    }
+
+    /*
+     * Initialize the 'Report Post' form (ajaxify) (copied from questions.js)
+     */
+    function initReportPost() {
+        $('form.report input[type="submit"]').click(function(ev){
+            ev.preventDefault();
+            var $form = $(this).closest('form');
+            $('div.report-article-box').remove();
+
+            var html = '<section class="report-article-box">' +
+                       '<ul class="wrap"></ul></section>';
+                $html = $(html),
+                $ul = $html.find('ul'),
+                kbox = new KBox($html, {
+                    title: gettext('Report this Article'),
+                    position: 'none',
+                    container: $form,
+                    closeOnOutClick: true
+                });
+
+            $form.find('select option').each(function(){
+                var $this = $(this),
+                    $li = $('<li><a href="#"></a></li>'),
+                    $a = $li.find('a');
+                $a.attr('data-val', $this.attr('value')).text($this.text());
+                $ul.append($li);
+            });
+            $ul.append('<li><input type="text" class="text" ' +
+                       'name="modal-other" /></li>');
+
+            $html.find('ul a').click(function(ev){
+                ev.preventDefault();
+                $form.find('select').val($(this).data('val'));
+                var other = $html.find('input[name="modal-other"]').val();
+                $form.find('input[name="other"]').val(other);
+                $.ajax({
+                    url: $form.attr('action'),
+                    type: 'POST',
+                    data: $form.serialize(),
+                    dataType: 'json',
+                    success: function(data) {
+                        var $msg = $('<div class="msg"></div>');
+                        $msg.text(data.message);
+                        $html.find('ul').replaceWith($msg);
+                    },
+                    error: function() {
+                        var message = gettext("There was an error."),
+                            $msg = $('<div class="msg"></div>');
+                        $msg.text(data.message);
+                        $html.find('ul').replaceWith($msg);
+                    }
+                });
+
+                return false;
+            });
+
+            kbox.open();
+        });
     }
 
     $(document).ready(init);
