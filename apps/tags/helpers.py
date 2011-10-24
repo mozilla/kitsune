08a76d51530ab4da0efd8532c738725eda319797
Ricky Rosario <rickyrosario@gmail.com>
2011-10-24 16:13:58 -0400

    wip ¯\_(ツ)_/¯

--- apps/tags/helpers.py
+++ apps/tags/helpers.py
@@ -1,7 +1,16 @@
+import json
+
 from jingo import register
+from taggit.models import Tag
 
 
 @register.function
 def tags_to_text(tags):
     """Converts a list of tag objects into a comma-separated slug list."""
     return ','.join([t.slug for t in tags])
+
+
+@register.function
+def tag_vocab():
+    """Returns the tag vocabulary as JSON array."""
+    return json.dumps(list(Tag.objects.values_list('name', flat=True)))
