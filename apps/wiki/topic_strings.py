6ee7758e8ef8bf4e84da00ea90da9f664efa67e9
Ricky Rosario <rickyrosario@gmail.com>
2011-10-31 11:20:08 -0400

    [bug 671581] Updated document topic strings for l10n.

--- apps/wiki/topic_strings.py
+++ apps/wiki/topic_strings.py
@@ -3,9 +3,12 @@
 ##########################################################
 from tower import ugettext as _
 
-_("""ui""", "KB Topic")
 _("""mobile""", "KB Topic")
-_("""Firefox 1.0""", "KB Topic")
-_("""addon""", "KB Topic")
-_("""Firefox 3.6.13""", "KB Topic")
-_("""Firefox 4.0""", "KB Topic")
+_("""desktop""", "KB Topic")
+_("""sync""", "KB Topic")
+_("""FxHome""", "KB Topic")
+_("""Firefox 6.0.2""", "KB Topic")
+_("""Firefox 5.0""", "KB Topic")
+_("""general""", "KB Topic")
+_("""download""", "KB Topic")
+_("""browserID""", "KB Topic")
