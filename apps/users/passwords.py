91013c518df7c0ce9cb90f9e811968adc1163af8
Tim Watts <tim@readevalprint.com>
2011-11-03 00:22:14 +0000

    [bug 682818] added basic filter for usernames

--- apps/users/passwords.py
+++ apps/users/passwords.py
@@ -1,14 +1,33 @@
+import string
+import re
+
 from django.core.cache import cache
 from django.conf import settings
 
 
-CACHE_KEY = 'password-blacklist'
+PASSWORD_CACHE_KEY = 'password-blacklist'
+USERNAME_CACHE_KEY = 'username-blacklist'
+
+
+def username_allowed(username):
+    """Returns True if the given username is not a blatent bad word."""
+    blacklist = cache.get(USERNAME_CACHE_KEY)
+    if blacklist is None:
+        f = open(settings.USERNAME_BLACKLIST, 'r')
+        blacklist = [w.strip() for w in f.readlines()]
+        cache.set(USERNAME_CACHE_KEY, blacklist)
+    username = username.lower()
+    usernames = set([username])
+    usernames |= set(''.join([o for o in username
+                           if not o in string.punctuation]).split())
+    usernames |= set(re.findall(r'\w+', username))
+    return not usernames.intersection(blacklist)
 
 
 def password_allowed(password):
-    blacklist = cache.get(CACHE_KEY)
+    blacklist = cache.get(PASSWORD_CACHE_KEY)
     if blacklist is None:
         f = open(settings.PASSWORD_BLACKLIST, 'r')
         blacklist = [w.strip() for w in f.readlines()]
-        cache.set(CACHE_KEY, blacklist)
+        cache.set(PASSWORD_CACHE_KEY, blacklist)
     return password not in blacklist
--- settings.py
+++ settings.py
@@ -212,6 +212,7 @@ GROUP_AVATAR_PATH = 'uploads/groupavatars/'
 ACCOUNT_ACTIVATION_DAYS = 30
 
 PASSWORD_BLACKLIST = path('configs/password-blacklist.txt')
+USERNAME_BLACKLIST = path('configs/username-blacklist.txt')
 
 ROOT_URLCONF = '%s.urls' % ROOT_PACKAGE
 
