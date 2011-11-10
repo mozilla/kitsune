65e5979b50c6779174b37a2a95b7e3a77f72e37c
James Socol <james@mozilla.com>
2011-11-10 09:58:32 -0500

    [bug 640753] Drop resurrected Tiki tables.

---
+++ migrations/133-drop-lazarus-tables.sql
@@ -0,0 +1,27 @@
+-- Drop Tiki tables resurrected during the move to PHX.
+
+DROP TABLE events_event;
+DROP TABLE events_signup;
+DROP TABLE galaxia_activities;
+DROP TABLE galaxia_activity_roles;
+DROP TABLE galaxia_instance_activities;
+DROP TABLE galaxia_instance_comments;
+DROP TABLE galaxia_instances;
+DROP TABLE galaxia_processes;
+DROP TABLE galaxia_roles;
+DROP TABLE galaxia_transitions;
+DROP TABLE galaxia_user_roles;
+DROP TABLE galaxia_workitems;
+DROP TABLE messu_archive;
+DROP TABLE messu_messages;
+DROP TABLE messu_sent;
+DROP TABLE metrics_assigned;
+DROP TABLE metrics_metric;
+DROP TABLE metrics_tab;
+DROP TABLE se_weights;
+DROP TABLE users_grouppermissions;
+DROP TABLE users_groups;
+DROP TABLE users_objectpermissions;
+DROP TABLE users_permissions;
+DROP TABLE users_usergroups;
+DROP TABLE users_users;
