a03c58ce6c818673e02dbf2bde1a7524609a0f6b
Tim Watts <tim@readevalprint.com>
2011-12-13 17:44:18 +0000

    works on new message, needs style and other apps

--- media/js/users.autocomplete.js
+++ media/js/users.autocomplete.js
@@ -6,8 +6,11 @@
 (function($) {
 
 "use strict";
-function extractLast(term) {
-    return term.split(/,\s*/).pop();
+function split( val ) {
+    return val.split( /,\s*/ );
+}
+function extractLast( term ) {
+    return split( term ).pop();
 }
 
 function init() {
@@ -20,17 +23,37 @@ function init() {
             },
             function(data){
                 console.log(data);
-                response(data.suggestions);
+                response(_.map(data.suggestions, function(user){
+                    return {
+                        'value': user.username,
+                        'label': user.username,
+                        'desc':  user.email
+                    }
+                }));
             });
         },
-        focus: function(event, ui) {
+        focus: function() {
+            // prevent value inserted on focus
             return false;
         },
-        select: function(event, ui) {
-            $(event.target).val(ui.item.username);
+        select: function( event, ui ) {
+            var terms = split( this.value );
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push( ui.item.value );
+            // add placeholder to get the comma-and-space at the end
+            terms.push( "" );
+            this.value = terms.join( ", " );
             return false;
         }
-    });
+    })
+    .data( "autocomplete" )._renderItem = function( ul, item ) {
+            return $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append( "<a>" + item.label + "<br>" + item.desc + "</a>" )
+                .appendTo( ul );
+        };
 }
 
 $(document).ready(init);
