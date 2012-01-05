242fd836dbc132ccf9422ebde81247bc24c77020
Erik Rose <erik@mozilla.com>
2012-01-05 11:40:04 -0800

    Make the rest of our ES tests use modelmakers rather than direct (noisier) calls to model constructors.

--- apps/questions/tests/test_es.py
+++ apps/questions/tests/test_es.py
@@ -1,100 +1,57 @@
-import uuid
-
 import elasticutils
 from nose.tools import eq_
 from waffle.models import Flag
 
-from questions.models import Question, Answer, question_searcher
+from questions.models import Question, question_searcher
 from questions.tests import question, answer, answer_vote
 from search.tests import dummy_request
 from sumo.tests import ElasticTestCase
-from users.tests import get_user
 
 
 class QuestionUpdateTests(ElasticTestCase):
     def test_added(self):
-        title = str(uuid.uuid4())
-        question = Question(title=title,
-                            content='Lorem Ipsum Dolor',
-                            creator=get_user())
+        q = question(save=True)
+        eq_(elasticutils.S(Question).count(), 1)
 
-        original_count = elasticutils.S(Question).count()
+        a = answer(question=q)
+        eq_(elasticutils.S(Question).count(), 1)
 
-        question.save()
-        self.refresh()
-
-        eq_(elasticutils.S(Question).count(), original_count + 1)
-
-        answer = Answer(content=u'Answer to ' + title,
-                        question=question,
-                        creator=get_user())
-
-        eq_(elasticutils.S(Question).count(), original_count + 1)
-
-        answer.save()
+        a.save()
         self.refresh()
 
         # Creating a new answer for a question doesn't create a new
-        # document in the index.  Therefore, the count remains
-        # original_count + 1.
+        # document in the index.  Therefore, the count remains 1.
         #
         # TODO: This is ambiguous: it's not clear whether we correctly
         # updated the document in the index or whether the post_save
         # hook didn't kick off.  Need a better test.
-        eq_(elasticutils.S(Question).count(), original_count + 1)
+        eq_(elasticutils.S(Question).count(), 1)
 
     def test_question_no_answers_deleted(self):
-        # Use a uuid since it's "unique" and makes sure we're not
-        # accidentally picking up a Post we don't want.
-        title = str(uuid.uuid4())
-
-        original_count = elasticutils.S(Question).count()
-
-        question = Question(title=title,
-                            content='Lorem Ipsum Dolor',
-                            creator=get_user())
-        question.save()
+        q = question(save=True)
         self.refresh()
+        eq_(elasticutils.S(Question).count(), 1)
 
-        eq_(elasticutils.S(Question).count(), original_count + 1)
-
-        question.delete()
+        q.delete()
         self.refresh()
-
-        eq_(elasticutils.S(Question).count(), original_count)
+        eq_(elasticutils.S(Question).count(), 0)
 
     def test_question_one_answer_deleted(self):
-        # Use a uuid since it's "unique" and makes sure we're not
-        # accidentally picking up a Post we don't want.
-        title = str(uuid.uuid4())
-
-        original_count = elasticutils.S(Question).count()
-
-        question = Question(title=title,
-                            content='Lorem Ipsum Dolor',
-                            creator=get_user())
-        question.save()
-
-        answer = Answer(content=u'Answer to ' + title,
-                        question=question,
-                        creator=get_user())
-
-        answer.save()
+        q = question(save=True)
+        a = answer(question=q, save=True)
         self.refresh()
 
         # Question and its answers are a single document--so the
-        # index count shouldn't change.
-        eq_(elasticutils.S(Question).count(), original_count + 1)
+        # index count should be only 1.
+        eq_(elasticutils.S(Question).count(), 1)
 
-        answer.delete()
+        a.delete()
         self.refresh()
+        eq_(elasticutils.S(Question).count(), 1)
 
-        eq_(elasticutils.S(Question).count(), original_count + 1)
-
-        question.delete()
+        q.delete()
         self.refresh()
-
-        eq_(elasticutils.S(Question).count(), original_count)
+        eq_(elasticutils.S(Question).count(), 0)
 
 
 class QuestionSearchTests(ElasticTestCase):
