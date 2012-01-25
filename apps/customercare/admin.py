4b86928a15714f4b38ab1a9318e9c62148a032fa
Tanay Gavankar <tgavankar@gmail.com>
2012-01-25 16:52:39 -0500

    [Bug 695104] Move AoA snippets to Verbatim

--- apps/customercare/admin.py
+++ apps/customercare/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from customercare.models import (Tweet, CannedCategory, CannedResponse,
-                                 CategoryMembership)
+from customercare.models import Tweet
 
 
 class TweetAdmin(admin.ModelAdmin):
@@ -10,28 +9,3 @@ class TweetAdmin(admin.ModelAdmin):
     list_filter = ('locale', 'hidden')
     search_fields = ('raw_json',)
 admin.site.register(Tweet, TweetAdmin)
-
-
-class MembershipInline(admin.StackedInline):
-    """Inline to show response/category relationships."""
-    extra = 1
-    model = CategoryMembership
-    raw_id_fields = ('response',)
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'locale', 'weight', 'response_count')
-    inlines = (MembershipInline,)
-
-    def response_count(self, obj):
-        return obj.responses.count()
-admin.site.register(CannedCategory, CategoryAdmin)
-
-
-class ResponseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'locale', 'category_count')
-    inlines = (MembershipInline,)
-
-    def category_count(self, obj):
-        return obj.categories.count()
-admin.site.register(CannedResponse, ResponseAdmin)
