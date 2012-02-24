5227550a6283ffe1695d0268b01116b7fad08635
Ricky Rosario <rickyrosario@gmail.com>
2012-02-24 14:08:07 -0500

    ocd

--- apps/kpi/cron.py
+++ apps/kpi/cron.py
@@ -13,18 +13,20 @@ def update_visitors_metric():
         # Get the latest metric value.
         last_metric = Metric.objects.filter(
             kind__code=VISITORS_METRIC_CODE).order_by('-start')[0]
-        # We will start updating the day after the last updated.
+        # Start updating the day after the last updated.
         start = last_metric.start + timedelta(days=1)
     except IndexError:
-        # There are no metrics yet. Let's start from 2011-01-01
+        # There are no metrics yet, start from 2011-01-01
         start = date(2011, 01, 01)
 
-    # We will collect up until yesterday
+    # Collect up until yesterday
     end = date.today() - timedelta(days=1)
 
+    # Get the visitor data from webtrends.
     visitors = Webtrends.visits(start, end)
-    metric_kind = MetricKind.objects.get(code=VISITORS_METRIC_CODE)
 
+    # Create the metrics.
+    metric_kind = MetricKind.objects.get(code=VISITORS_METRIC_CODE)
     for date_str, visits in visitors.items():
         day = datetime.strptime(date_str,"%Y-%m-%d").date()
         Metric.objects.create(
