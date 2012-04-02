42a43d1e216f2a5bbc09e7d7e28be125b3142704
Tanay Gavankar <tgavankar@gmail.com>
2012-04-02 13:10:26 -0400

    [Bug 706479] Fix in-page anchor links not working with lazyloading images

--- media/js/libs/jquery.lazyload.js
+++ media/js/libs/jquery.lazyload.js
@@ -16,15 +16,15 @@
             didScroll = true;
         });
 
-        loadAboveTheFoldImages(elements, opts);
+        loadAboveTheFoldImages(elements, opts, null);
 
         // Safari doesn't load images until scroll, sometimes
         $(window).trigger('scroll');
 
-        if(window.location.hash) {
-            // Reset scroll to anchor position if it exists
-            window.location.hash = window.location.hash;
-        }
+        var prevHash = window.location.hash;
+
+        // Anchor links should be preserved
+        resetHashPosition();
 
         var intv = setInterval(function() {
             if(loaded <= 0) {
@@ -34,7 +34,16 @@
             }
             if(didScroll) {
                 didScroll = false;
-                loaded -= loadAboveTheFoldImages(elements, opts);
+                if(prevHash != window.location.hash) {
+                    // In-page anchor link was clicked, preserve it
+                    assocAnchor = window.location.hash;
+                    prevHash = window.location.hash;
+                }
+                else {
+                    assocAnchor = null;
+                }
+                loadedCount = loadAboveTheFoldImages(elements, opts, assocAnchor);
+                loaded -= loadedCount;
             }
         }, 250);
         return this;
@@ -42,12 +51,22 @@
 
     $.fn.lazyload.defaults = {threshold: 750};
 
+    function resetHashPosition() {
+       if(window.location.hash) {
+            // Reset scroll to anchor position if it exists
+            window.location.hash = window.location.hash;
+        }
+    }
+
     function aboveTheFold(element, options){
         var fold = $(window).height() + $(window).scrollTop();
         return fold >= $(element).offset().top - (options['threshold']);
     };
 
-    $.fn.lazyload.loadOriginalImage = function(element){
+    $.fn.lazyload.loadOriginalImage = function(element, assocAnchor) {
+        if(assocAnchor != null) {
+            $(element).load(function() { resetHashPosition(); });
+        }
         $(element).attr('src', $(element).data('original-src')).removeData('original-src');
     };
 
@@ -59,16 +78,17 @@
         });
     }
 
-    function loadAboveTheFoldImages(elements, options){
+    function loadAboveTheFoldImages(elements, options, assocAnchor){
         var loaded = 0;
         elements.filter('.lazy').each(function(){
             if ($(this).hasClass('lazy') && aboveTheFold(this, options) &&
                 $(this).data('original-src') && $(this).is(":visible")) {
-                $.fn.lazyload.loadOriginalImage(this);
+                $.fn.lazyload.loadOriginalImage(this, assocAnchor);
                 $(this).removeClass('lazy');
                 loaded++;
             }
         });
+
         return loaded;
     };
 })(jQuery);
