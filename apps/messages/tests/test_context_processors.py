95e792b7416b47336bfa103a77ebd40e60550098
Ricky Rosario <rickyrosario@gmail.com>
2012-04-03 14:24:01 -0400

    [bug 741796] Remove fixture usage in messages/tests.

--- apps/messages/tests/test_context_processors.py
+++ apps/messages/tests/test_context_processors.py
@@ -3,18 +3,15 @@ from django.contrib.auth.models import AnonymousUser
 import mock
 from nose.tools import eq_
 from test_utils import RequestFactory
-import waffle
 
 import messages
 from messages.context_processors import unread_message_count
 from sumo.tests import TestCase
-from users.tests import get_user
+from users.tests import user
 
 
 class UnreadCountTests(TestCase):
     """Tests for unread_message_count."""
-
-    fixtures = ['users.json']
 
     @mock.patch.object(messages, 'unread_count_for')
     def test_anonymous(self, unread_count_for):
@@ -32,6 +29,6 @@ class UnreadCountTests(TestCase):
         unread_count_for.return_value = 3
         rf = RequestFactory()
         request = rf.get('/')
-        request.user = get_user('rrosario')
+        request.user = user(save=True)
         eq_(3, unread_message_count(request)['unread_message_count'])
         assert unread_count_for.called
