2b72ca5a602e22bada673a964f6dfb6a80b0a485
Will Kahn-Greene <willkg@mozilla.com>
2012-04-05 18:13:23 -0400

    [bug 740169] ES search admin page updates
    * removes waffle-on-finish stuff
    * adds elasticsearch waffle status and link to waffle flag admin page
    * breaks everything into separate sections for easier readability
    * fixes some borked html

--- apps/search/tasks.py
+++ apps/search/tasks.py
@@ -6,23 +6,20 @@ from django.conf import settings
 from django.core.cache import cache
 
 from celery.decorators import task
-from waffle.models import Flag
 
 from search.es_utils import es_reindex_with_progress
 
 
-# This is present in memcached when reindexing is in progress and holds a float
-# value between 0 and 100. When reindexing is complete (even if it crashes),
-# the token is removed.
+# This is present in memcached when reindexing is in progress and
+# holds a float value between 0 and 100. When reindexing is complete
+# (even if it crashes), the token is removed.
 ES_REINDEX_PROGRESS = 'sumo:search:es_reindex_progress'
-# Present iff ES should be waffled back on when indexing is complete:
-ES_WAFFLE_WHEN_DONE = 'sumo:search:es_reindex_will_waffle'
 
 log = logging.getLogger('k.task')
 
 
 @task
-def reindex_with_progress(waffle_when_done=False):
+def reindex_with_progress():
     """Rebuild elasticsearch index while updating progress bar for admins."""
     # Need to import Record here to prevent circular import
     from search.models import Record
@@ -35,11 +32,6 @@ def reindex_with_progress(waffle_when_done=False):
         # Init progress bar stuff:
         cache.set(ES_REINDEX_PROGRESS, 0.001)  # An iota so it tests true in
                                                # the template
-        if waffle_when_done:
-            cache.set(ES_WAFFLE_WHEN_DONE, True)
-        else:
-            # Clear it in case there was already one there somehow:
-            cache.delete(ES_WAFFLE_WHEN_DONE)
 
         # Reindex:
         start = time()
@@ -52,17 +44,10 @@ def reindex_with_progress(waffle_when_done=False):
                 # to be understood by JS but makes me nervous:
                 cache.set(ES_REINDEX_PROGRESS, '%.5f' % ratio)
 
-        if cache.get(ES_WAFFLE_WHEN_DONE):
-            # Just go ahead and crash if the flag isn't there.
-            flag = Flag.objects.get(name='elasticsearch')
-            flag.everyone = True
-            flag.save()
-
         rec.endtime = datetime.datetime.now()
         rec.save()
     finally:
         cache.delete(ES_REINDEX_PROGRESS)
-        cache.delete(ES_WAFFLE_WHEN_DONE)
 
 
 @task
