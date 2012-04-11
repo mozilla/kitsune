b3445cbb5251741fcdb5fc25c6422abff28950a3
Will Kahn-Greene <willkg@mozilla.com>
2012-04-11 16:59:04 -0400

    [bug 712069] Add logarithmic retry to indexing
    This adds logarithmic retry to index/unindex_task. If they fail, they'll
    get retried periodically rather than just fail and die like they do now.

--- apps/search/tasks.py
+++ apps/search/tasks.py
@@ -50,15 +50,39 @@ def reindex_with_progress():
         cache.delete(ES_REINDEX_PROGRESS)
 
 
+# Note: If you reduce the length of RETRY_TIMES, it affects all tasks
+# currently in the celery queue---they'll throw an IndexError.
+RETRY_TIMES = (
+    60,           # 1 minute
+    5 * 60,       # 5 minutes
+    10 * 60,      # 10 minutes
+    30 * 60,      # 30 minutes
+    60 * 60,      # 60 minutes
+    2 * 60 * 60,  # 2 hours
+    6 * 60 * 60   # 6 hours
+    )
+MAX_RETRIES = len(RETRY_TIMES)
+
+
 @task
 def index_task(cls, ids, **kw):
     """Index documents specified by cls and ids"""
-    for id in cls.uncached.filter(id__in=ids).values_list('id', flat=True):
-        cls.index(cls.extract_document(id), refresh=True)
+    try:
+        for id in cls.uncached.filter(id__in=ids).values_list('id', flat=True):
+            cls.index(cls.extract_document(id), refresh=True)
+    except Exception, exc:
+        retries = index_task.request.retries
+        index_task.retry(exc=exc, max_retries=MAX_RETRIES - 1,
+                         countdown=RETRY_TIMES[retries])
 
 
 @task
 def unindex_task(cls, ids, **kw):
     """Unindex documents specified by cls and ids"""
-    for id in ids:
-        cls.unindex(id)
+    try:
+        for id in ids:
+            cls.unindex(id)
+    except Exception, exc:
+        retries = unindex_task.request.retries
+        unindex_task.retry(exc=exc, max_retries=MAX_RETRIES - 1,
+                           countdown=RETRY_TIMES[retries])
