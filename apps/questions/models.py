a75bb9896f9dd89482a651b82ddb0211a16e5189
Rehan Dalal <rehandalal@gmail.com>
2012-05-28 01:59:08 -0400

    [bug 757173] 3 days unanswered filter should discount Closed Threads
    Make the counter and answered question bar discount closed threads,
    as these should be considered answered.

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -398,10 +398,13 @@ class Question(ModelBase, BigVocabTaggableMixin, SearchMixin):
         return qs.count()
 
     @classmethod
-    def recent_answered_count(cls):
-        """Returns the number of questions replied to in the last 72 hours."""
+    def recent_unanswered_count(cls):
+        """Returns the number of questions that have not been answered in the
+        last 72 hours
+        """
         start = datetime.now() - timedelta(hours=72)
-        qs = cls.objects.filter(created__gt=start, num_answers__gt=0)
+        qs = cls.objects.filter(
+            num_answers=0, created__gt=start, is_locked=False)
         return qs.count()
 
 
