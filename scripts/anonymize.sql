adcc0e3abf1d39e91b398e3ac7a44f9a538db112
Rehan Dalal <rehandalal@gmail.com>
2012-05-31 10:23:23 -0400

    Fix anonymization script
    Stop checking foreign keys when truncating tables to prevent foreign key constraint errors on MySQL >= 5.5

--- scripts/anonymize.sql
+++ scripts/anonymize.sql
@@ -7,6 +7,8 @@ UPDATE auth_user SET
     email = CONCAT('user',id,'@example.com'),
     password = 'sha256$f538347e82$5098e89186fd307d4bb6fe29ac476e72cf96175617fa933a9bd6b3d89a8b0946'; -- 'testpass'
 
+SET SESSION FOREIGN_KEY_CHECKS = 0;
+
 TRUNCATE tidings_watchfilter;
 
 TRUNCATE tidings_watch;
@@ -18,6 +20,8 @@ TRUNCATE messages_inboxmessage;
 TRUNCATE messages_outboxmessage_to;
 
 TRUNCATE messages_outboxmessage;
+
+SET SESSION FOREIGN_KEY_CHECKS = 1;
 
 UPDATE django_site SET
     domain = 'support-local.allizom.org',
