36b72e0b93303da130bf3542975e8f57d0b332a1
Will Kahn-Greene <willkg@mozilla.com>
2012-06-01 16:27:52 -0400

    [bug 755926] Add --delete to reindex cmd
    By default, it **won't** wipe the index. It now only wipes and rebuilds
    the index from scratch if you tell it to.

--- apps/search/management/commands/esreindex.py
+++ apps/search/management/commands/esreindex.py
@@ -10,11 +10,15 @@ class Command(BaseCommand):
     help = 'Reindex the database for Elastic.'
     option_list = BaseCommand.option_list + (
         make_option('--percent', type='int', dest='percent', default=100,
-                    help='Reindex a percentage of things'),)
+                    help='Reindex a percentage of things'),
+        make_option('--delete', action='store_true', dest='delete',
+                    help='Wipes index before reindexing')
+        )
 
     def handle(self, *args, **options):
         logging.basicConfig(level=logging.INFO)
         percent = options['percent']
+        delete = options['delete']
         if not 1 <= percent <= 100:
             raise CommandError('percent should be between 1 and 100')
-        es_reindex_cmd(percent)
+        es_reindex_cmd(percent, delete)
