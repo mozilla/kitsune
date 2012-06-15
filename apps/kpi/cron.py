7040f945bbc4758d8e572030c3213e537e7a8dfa
Ricky Rosario <rickyrosario@gmail.com>
2012-06-14 21:39:14 -0400

    [bug 757028] Add daily calculation of contributor metrics.
    New cron job that updates the number of contributors on a daily
    basis. Graphing this in stockcharts, now we have the ability to see
    daily, weekly and monthly groupings of the data.

--- apps/kpi/cron.py
+++ apps/kpi/cron.py
@@ -1,26 +1,34 @@
 from datetime import datetime, date, timedelta
 
+from django.db.models import Count, F
+
 import cronjobs
 
+from customercare.models import Reply
 from dashboards import LAST_90_DAYS
 from dashboards.models import WikiDocumentVisits
-from kpi.models import (Metric, MetricKind, VISITORS_METRIC_CODE,
-                        L10N_METRIC_CODE)
+from kpi.models import (Metric, MetricKind,
+                        AOA_CONTRIBUTORS_METRIC_CODE,
+                        KB_ENUS_CONTRIBUTORS_METRIC_CODE,
+                        KB_L10N_CONTRIBUTORS_METRIC_CODE,
+                        L10N_METRIC_CODE,
+                        SUPPORT_FORUM_CONTRIBUTORS_METRIC_CODE,
+                        VISITORS_METRIC_CODE)
+from questions.models import Answer
 from sumo.webtrends import Webtrends
+from wiki.models import Revision
 
 
 @cronjobs.register
 def update_visitors_metric():
     """Get new visitor data from webtrends and save."""
-    try:
-        # Get the latest metric value.
-        last_metric = Metric.objects.filter(
-            kind__code=VISITORS_METRIC_CODE).order_by('-start')[0]
-        # Start updating the day after the last updated.
-        start = last_metric.start + timedelta(days=1)
-    except IndexError:
-        # There are no metrics yet, start from 2011-01-01
-        start = date(2011, 01, 01)
+    # Start updating the day after the last updated.
+    latest_metric = _get_latest_metric(VISITORS_METRIC_CODE)
+    if latest_metric:
+        latest_metric_date = latest_metric.start
+    else:
+        latest_metric_date = date(2011, 01, 01)
+    start = latest_metric_date + timedelta(days=1)
 
     # Collect up until yesterday
     end = date.today() - timedelta(days=1)
@@ -104,6 +112,179 @@ def update_l10n_metric():
         start=day,
         end=day + timedelta(days=1),
         value=int(coverage * 100))  # Store as a % int.
+
+
+@cronjobs.register
+def update_contributor_metrics(day=None):
+    """Calculate and save contributor metrics."""
+    update_support_forum_contributors_metric(day)
+    update_kb_contributors_metric(day)
+    update_aoa_contributors_metric(day)
+
+
+def update_support_forum_contributors_metric(day=None):
+    """Calculate and save the support forum contributor counts.
+
+    An support forum contributor is a user that has replied 10 times
+    in the past 30 days to questions that aren't his/her own.
+    """
+    if day:
+        start = end = day
+    else:
+        latest_metric = _get_latest_metric(
+            SUPPORT_FORUM_CONTRIBUTORS_METRIC_CODE)
+        if latest_metric:
+            # Start updating the day after the last updated.
+            start = latest_metric.end + timedelta(days=1)
+        else:
+            start = date(2011, 01, 01)
+
+        # Update until yesterday.
+        end = date.today() - timedelta(days=1)
+
+    # Loop through all the days from start to end, calculating and saving.
+    day = start
+    while day <= end:
+        # Figure out the number of contributors from the last 30 days.
+        thirty_days_back = day - timedelta(days=30)
+        contributors = (Answer.objects
+            .exclude(creator=F('question__creator'))
+            .filter(
+                created__gte=thirty_days_back,
+                created__lt=day)
+            .values('creator')
+            .annotate(count=Count('creator'))
+            .filter(count__gte=10))
+        count = contributors.count()
+
+        # Save the value to Metric table.
+        metric_kind = MetricKind.objects.get(
+            code=SUPPORT_FORUM_CONTRIBUTORS_METRIC_CODE)
+        Metric.objects.create(
+            kind=metric_kind,
+            start=thirty_days_back,
+            end=day,
+            value=count)
+
+        day = day + timedelta(days=1)
+
+
+def update_kb_contributors_metric(day=None):
+    """Calculate and save the KB (en-US and L10n) contributor counts.
+
+    A KB contributor is a user that has edited or reviewed a Revision
+    in the last 30 days.
+    """
+    if day:
+        start = end = day
+    else:
+        latest_metric = _get_latest_metric(KB_ENUS_CONTRIBUTORS_METRIC_CODE)
+        if latest_metric:
+            # Start updating the day after the last updated.
+            start = latest_metric.end + timedelta(days=1)
+        else:
+            start = date(2011, 01, 01)
+
+        # Update until yesterday.
+        end = date.today() - timedelta(days=1)
+
+    # Loop through all the days from start to end, calculating and saving.
+    day = start
+    while day <= end:
+        # Figure out the number of contributors from the last 30 days.
+        thirty_days_back = day - timedelta(days=30)
+        editors = (Revision.objects
+            .filter(
+                created__gte=thirty_days_back,
+                created__lt=day)
+            .values_list('creator', flat=True).distinct())
+        reviewers = (Revision.objects
+            .filter(
+                reviewed__gte=thirty_days_back,
+                reviewed__lt=day)
+            .values_list('reviewer', flat=True).distinct())
+
+        en_us_count = len(set(
+            list(editors.filter(document__locale='en-US')) +
+            list(reviewers.filter(document__locale='en-US'))
+        ))
+        l10n_count = len(set(
+            list(editors.exclude(document__locale='en-US')) +
+            list(reviewers.exclude(document__locale='en-US'))
+        ))
+
+        # Save the values to Metric table.
+        metric_kind = MetricKind.objects.get(
+            code=KB_ENUS_CONTRIBUTORS_METRIC_CODE)
+        Metric.objects.create(
+            kind=metric_kind,
+            start=thirty_days_back,
+            end=day,
+            value=en_us_count)
+
+        metric_kind = MetricKind.objects.get(
+            code=KB_L10N_CONTRIBUTORS_METRIC_CODE)
+        Metric.objects.create(
+            kind=metric_kind,
+            start=thirty_days_back,
+            end=day,
+            value=l10n_count)
+
+        day = day + timedelta(days=1)
+
+
+def update_aoa_contributors_metric(day=None):
+    """Calculate and save the AoA contributor counts.
+
+    An AoA contributor is a user that has replied in the last 30 days.
+    """
+    if day:
+        start = end = day
+    else:
+        latest_metric = _get_latest_metric(AOA_CONTRIBUTORS_METRIC_CODE)
+        if latest_metric:
+            # Start updating the day after the last updated.
+            start = latest_metric.end + timedelta(days=1)
+        else:
+            # Start updating 30 days after the first reply we have.
+            first_reply = Reply.objects.order_by('created')[0]
+            start = first_reply.created.date() + timedelta(days=30)
+
+        # Update until yesterday.
+        end = date.today() - timedelta(days=1)
+
+    # Loop through all the days from start to end, calculating and saving.
+    day = start
+    while day <= end:
+        # Figure out the number of contributors from the last 30 days.
+        thirty_days_back = day - timedelta(days=30)
+        contributors = (Reply.objects
+            .filter(
+                created__gte=thirty_days_back,
+                created__lt=day)
+            .values_list('twitter_username').distinct())
+        count = contributors.count()
+
+        # Save the value to Metric table.
+        metric_kind = MetricKind.objects.get(code=AOA_CONTRIBUTORS_METRIC_CODE)
+        Metric.objects.create(
+            kind=metric_kind,
+            start=thirty_days_back,
+            end=day,
+            value=count)
+
+        day = day + timedelta(days=1)
+
+
+def _get_latest_metric(metric_code):
+    """Returns the date of the latest metric value."""
+    try:
+        # Get the latest metric value and return the date.
+        last_metric = Metric.objects.filter(
+            kind__code=metric_code).order_by('-start')[0]
+        return last_metric
+    except IndexError:
+        return None
 
 
 def _get_top_docs(count):
