d48e6c1b7851cff628512ee9da9db08d7cf2c2ab
Rehan Dalal <rehandalal@gmail.com>
2012-06-21 11:26:30 -0400

    [bug 765882] Only show canned reponses on forum

--- media/js/forums.js
+++ media/js/forums.js
@@ -7,7 +7,7 @@
 (function($){
 
     function init() {
-        Marky.createSimpleToolbar('.editor-tools', '#reply-content, #id_content');
+        Marky.createSimpleToolbar('.editor-tools', '#reply-content, #id_content', true);
         new k.AjaxPreview($('#preview'));
 
         $('span.post-action a.reply').click(function() {
