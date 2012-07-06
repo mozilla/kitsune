dace5d0bfc2e7d90aa8927c374c9c0704573f598
Tobbi <tobbi.bugs@googlemail.com>
2012-07-06 16:28:16 +0200

    [bug 670952] Add Quote button to private message editor

--- media/js/messages.js
+++ media/js/messages.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         $area.autoResize({minHeight: 150}).addClass('focused');
         $('#read-message .editor-tools').show();
         $('#read-message input[type=submit]').show();
-        Marky.createSimpleToolbar('#read-message .editor-tools', '#id_message');
+        Marky.createSimpleToolbar('#read-message .editor-tools', '#id_message', {privateMessaging: true});
         new k.AjaxPreview($('#preview-btn'), {
             changeHash: false
         });
