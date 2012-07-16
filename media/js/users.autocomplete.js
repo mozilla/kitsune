15ca549c4b390e3a02ad608a8e0cfac9ff2616ae
James Socol <james@mozilla.com>
2012-07-16 10:43:05 -0400

    [fix bug 772998] Don't simply concat JS strings.
    - Add a k.safeString function similar to Python's markupsafe.escape.
    - Add a k.safeInterpolate function that uses k.safeString.
    - Use interpolation instead of concatenation in user autocomplete.

--- media/js/users.autocomplete.js
+++ media/js/users.autocomplete.js
@@ -37,10 +37,9 @@ function init() {
         resultsFormatter: function(item){
             var term = $("#token-input-id_to").val();
             if (item.display_name) {
-                return ("<li><div class='name_search'>" +
-                        wrapTerm(item.display_name, term) + " [" +item.username +  "]</div></div></li>");
+                return k.safeInterpolate('<li><div class="name_search">%(display_name)s [%(username)s]</div></li>', item, true);
             }
-            return ("<li><div class='name_search'>" + item.username + "</div></li>");
+            return k.safeInterpolate('<li><div class="name_search">%(username)s</div></li>', item, true);
         },
         onAdd: function (item) {
             $(this).closest('.single').closest('form').submit();
