a91b6b95e2f1897a344a26f6727c114914188e08
Ricky Rosario <rickyrosario@gmail.com>
2012-08-05 18:15:11 -0400

    [bug 778160] Topic page

---
+++ apps/topics/tests/test_templates.py
@@ -0,0 +1,65 @@
+import mock
+import waffle
+from nose.tools import eq_
+from pyquery import PyQuery as pq
+
+from products.tests import product
+from search.tests.test_es import ElasticTestCase
+from sumo.helpers import urlparams
+from sumo.urlresolvers import reverse
+from topics.tests import topic
+from wiki.tests import revision
+
+
+class TopicViewsTestCase(ElasticTestCase):
+    @mock.patch.object(waffle, 'flag_is_active')
+    def test_topic_select_product(self, flag_is_active):
+        """Verify that /topics/<slug>?selectproduct=1 renders products."""
+        flag_is_active.return_value = True
+
+        # Create a topic
+        t = topic(save=True)
+
+        # Create 3 products
+        prods = []
+        for i in range(3):
+            prods.append(product(save=True))
+
+        # Create a document and assign the topic and two products.
+        doc = revision(is_approved=True, save=True).document
+        doc.topics.add(t)
+        doc.products.add(prods[0])
+        doc.products.add(prods[1])
+
+        self.refresh()
+
+        # GET the topic page and verify the content
+        url = reverse('topics.topic', args=[t.slug])
+        url = urlparams(url, selectproduct=1)
+        r = self.client.get(url, follow=True)
+        eq_(200, r.status_code)
+        doc = pq(r.content)
+        eq_(2, len(doc('#products-and-services li')))
+
+    @mock.patch.object(waffle, 'flag_is_active')
+    def test_topic_document_listing(self, flag_is_active):
+        """Verify that /topics/<slug> renders articles."""
+        flag_is_active.return_value = True
+
+        # Create a topic
+        t = topic(save=True)
+
+        # Create 3 documents with the topic and one without
+        for i in range(3):
+            doc = revision(is_approved=True, save=True).document
+            doc.topics.add(t)
+        doc = revision(is_approved=True, save=True).document
+
+        self.refresh()
+
+        # GET the topic page and verify the content
+        url = reverse('topics.topic', args=[t.slug])
+        r = self.client.get(url, follow=True)
+        eq_(200, r.status_code)
+        doc = pq(r.content)
+        eq_(3, len(doc('#topic-documents li')))
---
+++ apps/topics/views.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+import jingo
+
+from landings.utils import show_ia
+from products.models import Product
+from sumo.urlresolvers import reverse
+from topics.models import Topic
+from wiki.facets import products_for, documents_for
+from wiki.models import Document
+
+
+def topic_landing(request, slug):
+    """The topic landing page.
+
+    If `selectproduct=1` query param is passed, shows the product picker.
+    Else, shows the list of articles.
+    """
+    if not show_ia(request):
+        # User should only be able to get here in new IA.
+        # Redirect to home page
+        return HttpResponseRedirect(reverse('home'))
+
+    topic = get_object_or_404(Topic, slug=slug)
+
+    data = dict(topic=topic)
+    if request.GET.get('selectproduct') == '1':
+        data.update(products=products_for(topics=[topic]))
+    else: 
+        data.update(documents=documents_for(
+            locale=request.locale, topics=[topic]))
+
+    return jingo.render(request, 'topics/topic.html', data)
