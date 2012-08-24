6d3703a749e9dd4ccab6e514731b61c3f83cd614
Ricky Rosario <rickyrosario@gmail.com>
2012-08-24 09:41:28 -0400

    [bug 781402] Nix caching in products_for and topics_for.
    * Cache machine is already caching the queries.
    * For whatever reason, this unstucks the tests on jenkins.

--- apps/wiki/facets.py
+++ apps/wiki/facets.py
@@ -15,17 +15,12 @@ def products_for(topics):
 
     :arg topics: a list of Topic instances
     """
-    # First try to get the results from the cache
-    products = cache.get(_products_for_cache_key(topics))
-    if products:
-        statsd.incr('wiki.facets.products_for.cache')
-        return products
-
-    products = _db_products_for(topics)
-    cache.add(_products_for_cache_key(topics), products)
     statsd.incr('wiki.facets.products_for.db')
 
-    return products
+    docs = Document.objects
+    for topic in topics:
+        docs = docs.filter(topics=topic)
+    return Product.objects.filter(document__in=docs).distinct()
 
 
 def topics_for(products):
@@ -33,17 +28,12 @@ def topics_for(products):
 
     :arg products: a list of Product instances
     """
-    # First try to get the results from the cache
-    topics = cache.get(_topics_for_cache_key(products))
-    if topics:
-        statsd.incr('wiki.facets.topics_for.cache')
-        return topics
-
-    topics = _db_topics_for(products)
-    cache.add(_topics_for_cache_key(products), topics)
     statsd.incr('wiki.facets.topics_for.db')
 
-    return topics
+    docs = Document.objects
+    for product in products:
+        docs = docs.filter(products=product)
+    return Topic.objects.filter(document__in=docs).distinct()
 
 
 def documents_for(locale, topics, products=None):
@@ -82,41 +72,6 @@ def documents_for(locale, topics, products=None):
     return documents
 
 
-def md5_result(func):
-    """A decorator that md5's the returned value."""
-    def _md5(*args, **kwargs):
-        m = hashlib.md5()
-        m.update(func(*args, **kwargs))
-        return '%s:%s' % (func.__name__, m.hexdigest())
-    return _md5
-
-
-def _db_products_for(topics):
-    """DB implementation of products_for."""
-    docs = Document.objects
-    for topic in topics:
-        docs = docs.filter(topics=topic)
-    return Product.objects.filter(document__in=docs).distinct()
-
-
-@md5_result
-def _products_for_cache_key(topics):
-    return ','.join(sorted([t.slug for t in topics]))
-
-
-def _db_topics_for(products):
-    """DB implementation of topics_for."""
-    docs = Document.objects
-    for product in products:
-        docs = docs.filter(products=product)
-    return Topic.objects.filter(document__in=docs).distinct()
-
-
-@md5_result
-def _topics_for_cache_key(products):
-    return ','.join(sorted([p.slug for p in products]))
-
-
 def _es_documents_for(locale, topics, products):
     """ES implementation of documents_for."""
     s = Document.search().values_dict(
@@ -145,9 +100,10 @@ def _db_documents_for(locale, topics, products=None):
     return doc_dicts
 
 
-@md5_result
 def _documents_for_cache_key(locale, topics, products):
-    return '{locale}:{topics}:{products}'.format(
+    m = hashlib.md5()
+    m.update('{locale}:{topics}:{products}'.format(
         locale=locale,
         topics=','.join(sorted([t.slug for t in topics])),
-        products=','.join(sorted([p.slug for p in products or []])))
+        products=','.join(sorted([p.slug for p in products or []]))))
+    return 'documents_for:%s' % m.hexdigest()
