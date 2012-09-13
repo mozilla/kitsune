62bb14914fc9c72125169415b4c63d7d5e84874c
Ricky Rosario <rickyrosario@gmail.com>
2012-09-13 17:34:14 -0400

    Hot fix: pass request personal_tabs.html.

--- apps/dashboards/helpers.py
+++ apps/dashboards/helpers.py
@@ -10,6 +10,7 @@ def personal_dashboard_tabs(context, active_tab):
     """Render the tabs for the user/group dashboard."""
     c = {'dashboards': personal_dashboards(context['request']),
          'user': context['request'].user,
-         'active_tab': active_tab}
+         'active_tab': active_tab,
+         'request': context['request']}
     t = env.get_template('dashboards/includes/personal_tabs.html').render(c)
     return jinja2.Markup(t)
