bc7caa0ee3cbf6e0c7159842b15f9408126b8250
Ricky Rosario <rickyrosario@gmail.com>
2012-10-10 08:26:59 -0400

    Revert "Revert "[bug 793701] Redirect /sync to /products/firefox/sync""
    This reverts commit a728f26550de56cb7ae725e73f76fc7b4c322317.

--- apps/landings/views.py
+++ apps/landings/views.py
@@ -103,7 +103,11 @@ def mobile(request, template=None):
 @mobile_template('landings/{mobile/}sync.html')
 def sync(request, template=None):
     if not request.MOBILE:
-        return redirect_to(request, 'home', permanent=False)
+        return redirect_to(
+            request, 'products.documents',
+            product_slug='firefox',
+            topic_slug='sync',
+            permanent=False)
 
     docs = SYNC_DOCS_FOR_MOBILE
     return jingo.render(request, template,
