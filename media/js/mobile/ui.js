0530cb1363df69e5ce9d6261863013db2f8969a7
Rehan Dalal <rehandalal@gmail.com>
2012-10-16 17:11:13 -0400

    [bug 795870] Mobile redesign base styles

---
+++ media/js/mobile/ui.js
@@ -0,0 +1,39 @@
+(function($) {
+    $(function() {
+        // Menu toggling
+        $('#menu-button').on('click', function() {
+            $page = $('#page');
+            $nav = $('body > nav');
+
+            var animation = {};
+
+            if ($('body').data('orientation') === 'right') {
+                if ($page.css('right') === '0px') {
+                    animation.right = $nav.outerWidth();
+                } else {
+                    animation.right = 0;
+                }
+            } else {
+                if ($page.css('left') === '0px') {
+                    animation.left = $nav.outerWidth();
+                } else {
+                    animation.left = 0;
+                }
+            }
+
+            $page.animate(animation);
+        });
+
+        // iOS Standalone Web App Fix
+        if (("standalone" in window.navigator) && window.navigator.standalone) {
+            $(document).on('click', 'a', function(event) {
+                var href = $(event.target).attr('href');
+
+                if (href) {
+                    event.preventDefault();
+                    location.href = href;
+                }
+            });
+        }
+    });
+})(jQuery)
