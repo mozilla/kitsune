f6e2a6ae8a8b333c64dbf50fe808734442d7ba1c
Rehan Dalal <rehandalal@gmail.com>
2012-10-17 15:46:24 -0400

    Use CSS for exposing menu

--- media/js/mobile/ui.js
+++ media/js/mobile/ui.js
@@ -2,26 +2,7 @@
     $(function() {
         // Menu toggling
         $('#menu-button').on('click', function() {
-            var $page = $('#page');
-            var $nav = $('body > nav');
-
-            var animation = {};
-
-            if ($('body').data('orientation') === 'right') {
-                if ($page.css('right') === '0px') {
-                    animation.right = $nav.outerWidth();
-                } else {
-                    animation.right = 0;
-                }
-            } else {
-                if ($page.css('left') === '0px') {
-                    animation.left = $nav.outerWidth();
-                } else {
-                    animation.left = 0;
-                }
-            }
-
-            $page.animate(animation);
+            $('#page').toggleClass('exposed');
         });
 
         $('.overlay > header').on('click', function() {
