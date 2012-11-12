9b58986eac790889f9ab8f0747c2485d77d18e39
Mike Cooper <mythmon@gmail.com>
2012-11-12 10:21:55 -0800

    Increase zIndex for survey.

--- media/js/surveygizmo.js
+++ media/js/surveygizmo.js
@@ -8,7 +8,19 @@
         register_survey: function() {
             // This ugly pile is generated code from Survey Gizmo. Bug 809433
             var sg_div = document.createElement("div");
-            sg_div.innerHTML = "<h1>You have been selected for a survey</h1><p>We appreciate your feedback!</p><p><a href=\"http://www.surveygizmo.com/s3/popup/1076431/ba695bdef132\">Please click here start it now.</a> </p> <a href=\"#\" onclick=\"document.getElementById('sg-popup').style.display = 'none';return false;\">No, thank you.</a> "; sg_div.id = "sg-popup";sg_div.style.position = "absolute";sg_div.style.width = "500px";sg_div.style.top = "100px";sg_div.style.left = "400px";sg_div.style.backgroundColor = "#ffffff";sg_div.style.borderColor = "#000000";sg_div.style.borderStyle = "solid";sg_div.style.padding = "20px";sg_div.style.fontSize = "16px";document.body.appendChild(sg_div);
+            sg_div.innerHTML = "<h1>You have been selected for a survey</h1><p>We appreciate your feedback!</p><p><a href=\"http://www.surveygizmo.com/s3/popup/1076431/ba695bdef132\">Please click here start it now.</a> </p> <a href=\"#\" onclick=\"document.getElementById('sg-popup').style.display = 'none';return false;\">No, thank you.</a> ";
+            sg_div.id = "sg-popup";
+            sg_div.style.position = "absolute";
+            sg_div.style.width = "500px";
+            sg_div.style.top = "100px";
+            sg_div.style.left = "400px";
+            sg_div.style.backgroundColor = "#ffffff";
+            sg_div.style.borderColor = "#000000";
+            sg_div.style.borderStyle = "solid";
+            sg_div.style.padding = "20px";
+            sg_div.style.fontSize = "16px";
+            sg_div.style.zIndex = "1000";
+            document.body.appendChild(sg_div);
         }
     };
 
