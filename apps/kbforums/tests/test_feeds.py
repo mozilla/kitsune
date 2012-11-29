1a488a896833f1e6b9a413dfbf1cea640721bf63
Berker Peksag <berker.peksag@gmail.com>
2012-11-29 03:46:28 +0200

    [bug 741781] Convert kbforums/tests to use model makers

--- apps/kbforums/tests/test_feeds.py
+++ apps/kbforums/tests/test_feeds.py
@@ -1,31 +1,38 @@
+import time
+
 from nose.tools import eq_
 from pyquery import PyQuery as pq
 
 from kbforums.feeds import ThreadsFeed, PostsFeed
-from kbforums.models import Thread
-from kbforums.tests import KBForumTestCase, get
-from wiki.models import Document
+from kbforums.tests import KBForumTestCase, get, thread
+from wiki.tests import document
 
 
 class FeedSortingTestCase(KBForumTestCase):
 
     def test_threads_sort(self):
         """Ensure that threads are being sorted properly by date/time."""
-        f = Document.objects.get(pk=1)
-        given_ = ThreadsFeed().items(f)[0].id
-        exp_ = 4L
-        eq_(exp_, given_)
+        d = document(save=True)
+        t = thread(document=d, save=True)
+        t.new_post(creator=t.creator, content='foo')
+        time.sleep(1)
+        t2 = thread(document=d, save=True)
+        t2.new_post(creator=t2.creator, content='foo')
+        given_ = ThreadsFeed().items(d)[0].id
+        eq_(t2.id, given_)
 
     def test_posts_sort(self):
         """Ensure that posts are being sorted properly by date/time."""
-        t = Thread.objects.get(pk=1)
+        t = thread(save=True)
+        t.new_post(creator=t.creator, content='foo')
+        time.sleep(1)
+        p2 = t.new_post(creator=t.creator, content='foo')
         given_ = PostsFeed().items(t)[0].id
-        exp_ = 24L
-        eq_(exp_, given_)
+        eq_(p2.id, given_)
 
     def test_multi_feed_titling(self):
         """Ensure that titles are being applied properly to feeds."""
-        d = Document.objects.filter()[0]
+        d = document(save=True)
         response = get(self.client, 'wiki.discuss.threads', args=[d.slug])
         doc = pq(response.content)
         given_ = doc('link[type="application/atom+xml"]')[0].attrib['title']
