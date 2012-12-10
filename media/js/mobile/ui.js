9ff9557eb718cbddebbfd4768be0a8c885c0659a
Rehan Dalal <rehandalal@gmail.com>
2012-12-10 18:24:59 -0500

    [bug 816072] Add reply for to question page

--- media/js/mobile/ui.js
+++ media/js/mobile/ui.js
@@ -20,6 +20,18 @@
             }
         });
 
+        $(document).on('click', '[data-show]', function() {
+            $('#' + $(this).data('show')).css('display', '');
+        });
+
+        $(document).on('click', '[data-hide]', function() {
+            $('#' + $(this).data('hide')).hide();
+        });
+
+        $(document).on('click', '[data-expand]', function() {
+            $('#' + $(this).data('expand')).addClass('expanded');
+        });
+
         $(document).on('click', '.collapsable .toggle', function() {
             $(this).closest('.collapsable').toggleClass('expanded');
         });
