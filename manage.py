84c5ac764b6ca2f452db2f387d0f838d395e31fa
Berker Peksag <berker.peksag@gmail.com>
2012-12-27 14:13:02 +0200

    Remove duplicate import of "sys" module.

--- manage.py
+++ manage.py
@@ -34,7 +34,6 @@ except ImportError:
     try:
         import settings  # Assumed to be in the same directory.
     except ImportError:
-        import sys
         sys.stderr.write(
             "Error: Tried importing 'settings_local.py' and 'settings.py' "
             "but neither could be found (or they're throwing an ImportError)."
