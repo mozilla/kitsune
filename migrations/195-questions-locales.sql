5ec3633af91a593d7fc6d6d76056660fe8a775b9
Mike Cooper <mythmon@gmail.com>
2013-01-11 09:29:24 -0800

    Add Products and Topics FKs too.
    Topics and Products are M2M right now mainly because adding FKs is so
    hard. We aren't using this in the code yet, but if we are going to go
    through the process of doing an alter table, we might as well add these
    anyways.
    
    TODO: Eventually convert the code to use this too.

--- migrations/195-questions-locales.sql
+++ migrations/195-questions-locales.sql
@@ -3,3 +3,9 @@
 ALTER TABLE `questions_question` ADD COLUMN `locale_id` integer NULL;
 ALTER TABLE `questions_question` ADD CONSTRAINT `locale_id_refs_id_643d2ff3` FOREIGN KEY (`locale_id`) REFERENCES `wiki_locale` (`id`);
 
+ALTER TABLE `questions_question` ADD COLUMN `product_id` integer NULL;
+ALTER TABLE `questions_question` ADD CONSTRAINT `product_id_refs_id_9659be74` FOREIGN KEY (`locale_id`) REFERENCES `products_product` (`id`);
+
+ALTER TABLE `questions_question` ADD COLUMN `topic_id` integer NULL;
+ALTER TABLE `questions_question` ADD CONSTRAINT `topic_id_refs_id_b4f2f293` FOREIGN KEY (`locale_id`) REFERENCES `topics_topic` (`id`);
+
