a0643031d12fa20fcec8286e279213cea9048115
Mike Cooper <mythmon@gmail.com>
2013-01-11 14:46:50 -0800

    Doh.

--- migrations/195-questions-locales.sql
+++ migrations/195-questions-locales.sql
@@ -5,8 +5,8 @@ ALTER TABLE `questions_question`
     ADD CONSTRAINT `locale_id_refs_id_643d2ff3` FOREIGN KEY (`locale_id`) REFERENCES `wiki_locale` (`id`),
 
     ADD COLUMN `product_id` integer NULL,
-    ADD CONSTRAINT `product_id_refs_id_9659be74` FOREIGN KEY (`locale_id`) REFERENCES `products_product` (`id`),
+    ADD CONSTRAINT `product_id_refs_id_9659be74` FOREIGN KEY (`product_id`) REFERENCES `products_product` (`id`),
 
     ADD COLUMN `topic_id` integer NULL,
-    ADD CONSTRAINT `topic_id_refs_id_b4f2f293` FOREIGN KEY (`locale_id`) REFERENCES `topics_topic` (`id`);
+    ADD CONSTRAINT `topic_id_refs_id_b4f2f293` FOREIGN KEY (`topic_id`) REFERENCES `topics_topic` (`id`);
 
