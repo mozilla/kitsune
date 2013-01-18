087e0e46436ce4c17f1f0f75a55554aba7e9fa9a
Ricky Rosario <rickyrosario@gmail.com>
2013-01-18 17:46:20 -0500

    Remove no longer used landing page views.

--- apps/landings/views.py
+++ apps/landings/views.py
@@ -1,8 +1,6 @@
 from django.views.decorators.cache import never_cache
 
 import jingo
-import waffle
-from mobility.decorators import mobile_template
 
 from products.models import Product
 from sumo.parser import get_object_fallback
@@ -10,40 +8,6 @@ from sumo.views import redirect_to
 from topics.models import Topic, HOT_TOPIC_SLUG
 from wiki.facets import documents_for
 from wiki.models import Document
-from wiki.views import showfor_data
-
-
-# Docs for the mobile optimized templates:
-HOME_DOCS_FOR_MOBILE = {
-    'common': 'Desktop home for mobile - Common Questions',
-    'top': 'Home page - Top'}
-MOBILE_DOCS_FOR_MOBILE = {
-    'common': 'Mobile home for mobile - Common Questions',
-    'top': 'Mobile home - Top'}
-SYNC_DOCS_FOR_MOBILE = {
-    'common': 'Sync home for mobile - Common Questions',
-    'top': 'Sync home - Top'}
-MARKETPLACE_DOCS_FOR_MOBILE = {
-    'common': 'Marketplace home for mobile - Common Questions',
-    'top': 'Marketplace home - Top'}
-FIREFOX_DOCS_FOR_MOBILE = {
-    'common': 'Firefox home for mobile - Common Questions',
-    'top': 'Firefox home - Top'}
-PRODUCTS_DOCS_FOR_MOBILE = {
-    'common': 'Products home for mobile - Common Questions',
-    'top': 'Products home - Top'}
-KB_DOCS_FOR_MOBILE = {
-    'common': 'KB home for mobile - Common Questions',
-    'top': 'KB home - Top'}
-ASK_DOCS_FOR_MOBILE = {
-    'common': 'Ask home for mobile - Common Questions',
-    'top': 'Ask home - Top'}
-PARTICIPATE_DOCS_FOR_MOBILE = {
-    'common': 'Participate home for mobile - Common Questions',
-    'top': 'Participate home - Top'}
-FEEDBACK_DOCS_FOR_MOBILE = {
-    'common': 'Feedback home for mobile - Common Questions',
-    'top': 'Feedback home - Top'}
 
 
 # Docs for the new IA:
@@ -84,123 +48,5 @@ def home(request):
         'moz_news': moz_news})
 
 
-@mobile_template('landings/{mobile/}mobile.html')
-def mobile(request, template=None):
-    return redirect_to(
-        request, 'products.product', slug='mobile', permanent=False)
-
-
-@mobile_template('landings/{mobile/}sync.html')
-def sync(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(
-            request, 'products.documents',
-            product_slug='firefox',
-            topic_slug='sync',
-            permanent=False)
-
-    docs = SYNC_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
-@mobile_template('landings/{mobile/}marketplace.html')
-def marketplace(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(request, 'home', permanent=False)
-
-    docs = MARKETPLACE_DOCS_FOR_MOBILE
-    # Marketplace search results should only be kb (zendesk is being
-    # used for questions).
-    return jingo.render(request, template,
-                        _data(docs, request.locale, only_kb=True))
-
-
-@mobile_template('landings/{mobile/}firefox.html')
-def firefox(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(
-            request, 'products.product', slug='firefox', permanent=False)
-
-    docs = FIREFOX_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale, 'firefox', 'desktop'))
-
-
-@mobile_template('landings/{mobile/}products.html')
-def old_products(request, template=None):
-    docs = PRODUCTS_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
-@mobile_template('landings/{mobile/}kb.html')
-def old_kb(request, template=None):
-    docs = KB_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
-@mobile_template('landings/{mobile/}ask.html')
-def ask(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(
-            request,
-            'wiki.document',
-            document_slug='get-community-support',
-            permanent=False)
-
-    docs = ASK_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
-@mobile_template('landings/{mobile/}participate.html')
-def participate(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(
-            request,
-            'wiki.document',
-            document_slug='superheroes-wanted',
-            permanent=False)
-
-    docs = PARTICIPATE_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
-@mobile_template('landings/{mobile/}feedback.html')
-def feedback(request, template=None):
-    if not request.MOBILE:
-        return redirect_to(
-            request,
-            'wiki.document',
-            document_slug='suggestion-box',
-            permanent=False)
-
-    docs = FEEDBACK_DOCS_FOR_MOBILE
-    return jingo.render(request, template,
-                        _data(docs, request.locale))
-
-
 def integrity_check(request):
     return jingo.render(request, 'landings/integrity-check.html')
-
-
-def _data(docs, locale, product=None, q_tags=None, only_kb=False):
-    """Add the documents and showfor data to the context data."""
-    data = {}
-    for side, title in docs.iteritems():
-        data[side] = get_object_fallback(Document, title, locale)
-
-    data.update(showfor_data())
-
-    if product:
-        data.update(search_params={'product': product})
-
-    if only_kb:
-        data.setdefault('search_params', {}).update({'w': 1})
-    elif q_tags:
-        data['search_params'].update(q_tags=q_tags)
-
-    return data
