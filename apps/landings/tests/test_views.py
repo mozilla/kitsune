3545a99d14a021fb790e9006dd4aa8a43eade038
Ricky Rosario <rickyrosario@gmail.com>
2013-01-18 17:46:20 -0500

    Remove no longer used landing page views.

--- apps/landings/tests/test_views.py
+++ apps/landings/tests/test_views.py
@@ -1,38 +1,7 @@
 from nose.tools import eq_
-from pyquery import PyQuery as pq
 
 from sumo.tests import MobileTestCase, TestCase
 from sumo.urlresolvers import reverse
-
-
-class MobileHomeTests(MobileTestCase):
-    def test_sync_home_for_mobile(self):
-        self._check_template('home.sync', 'landings/mobile/sync.html')
-
-    def test_marketplace_home_for_mobile(self):
-        r = self._check_template(
-            'home.marketplace', 'landings/mobile/marketplace.html')
-        doc = pq(r.content)
-        eq_('1', doc('#search input[name="w"]')[0].attrib['value'])
-
-    def test_firefox_home_for_mobile(self):
-        self._check_template('home.firefox', 'landings/mobile/firefox.html')
-
-    def test_ask_home_for_mobile(self):
-        self._check_template('home.ask', 'landings/mobile/ask.html')
-
-    def test_participate_home_for_mobile(self):
-        self._check_template(
-            'home.participate', 'landings/mobile/participate.html')
-
-    def test_feedback_home_for_mobile(self):
-        self._check_template('home.feedback', 'landings/mobile/feedback.html')
-
-    def _check_template(self, url_name, template):
-        r = self.client.get(reverse(url_name), follow=True)
-        eq_(r.status_code, 200)
-        self.assertTemplateUsed(r, template)
-        return r
 
 
 class RootRedirectTests(TestCase):
--- apps/landings/urls.py
+++ apps/landings/urls.py
@@ -1,20 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
-from sumo.views import redirect_to
-
 
 urlpatterns = patterns('landings.views',
     url(r'^$', 'desktop_or_mobile', name='home.default'),
     url(r'^home$', 'home', name='home'),
-    url(r'^mobile$', 'mobile', name='home.mobile'),
-    url(r'^sync$', 'sync', name='home.sync'),
-    url(r'^firefox-home$', redirect_to, {'url': 'home'},
-        name='home.fxhome'),
-    url(r'^marketplace$', 'marketplace', name='home.marketplace'),
-    url(r'^firefox$', 'firefox', name='home.firefox'),
-    url(r'^ask$', 'ask', name='home.ask'),
-    url(r'^participate$', 'participate', name='home.participate'),
-    url(r'^feedback$', 'feedback', name='home.feedback'),
 
     # A static page for downloading FirefoxIntegrityCheck.exe
     url(r'^download-firefox-integrity-check$', 'integrity_check',
