51f890453c12a80a40f93bc32691a1decf46141b
Ricky Rosario <rickyrosario@gmail.com>
2013-01-22 19:06:49 -0500

    Redirect from /mobile to /products/mobile

--- apps/landings/urls.py
+++ apps/landings/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import patterns, url
 
+from sumo.views import redirect_to
+
 
 urlpatterns = patterns('landings.views',
     url(r'^$', 'desktop_or_mobile', name='home.default'),
     url(r'^home$', 'home', name='home'),
+
+    # Redirect from old mobile URL to new one.
+    url(r'^mobile$', redirect_to,
+        {'url': 'products.product', 'slug': 'mobile'}, name='home.mobile'),
 
     # A static page for downloading FirefoxIntegrityCheck.exe
     url(r'^download-firefox-integrity-check$', 'integrity_check',
