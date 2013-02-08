0b86d8efcb935ea3871c5e4e494345219f9d495c
Michal Frontczak <michaljev@o2.pl>
2013-02-08 23:06:56 +0100

    [bug 839417] Replace custom js with input type="number" pattern="\d*".

--- media/js/search.js
+++ media/js/search.js
@@ -14,9 +14,6 @@ $(document).ready(function() {
     $('.datepicker').datepicker();
     $('.datepicker').attr('readonly', 'readonly').css('background', '#ddd');
 
-    // Force numeric input for num_votes
-    $('input.numeric').numericInput();
-
     $('select', cache_search_date).change(function () {
         if ($(this).val() == 0) {
             $('input', $(this).parent()).hide();
@@ -37,28 +34,3 @@ $(document).ready(function() {
             tabs.tabs('select', 0);
     }
 });
-
-/**
- * Accept only numeric keystrokes.
- *
- * Based on http://snipt.net/GerryEng/jquery-making-textfield-only-accept-numeric-values
- */
-jQuery.fn.numericInput = function (options) {
-    // Only works on <input/>
-    if (!this.is('input')) {
-        return this;
-    }
-
-    this.keydown(function(event) {
-        // Allow only backspace and delete
-        if ( event.keyCode == 46 || event.keyCode == 8 ) {
-            // let it happen, don't do anything
-        } else if (event.shiftKey || event.keyCode < 48 || event.keyCode > 57) {
-            // Ensure that it is a number and stop the keypress
-            event.preventDefault();
-        }
-    });
-
-
-    return this;
-};
