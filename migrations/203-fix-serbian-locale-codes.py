ac0b3bd6a7ac0bb03709e7b99c59cc3e42a72b2b
Ricky Rosario <rickyrosario@gmail.com>
2013-04-05 10:15:49 -0400

    [bug 851207] Fix locale code casing of sr-Latn and sr-Cyrl.

---
+++ migrations/203-fix-serbian-locale-codes.py
@@ -0,0 +1,18 @@
+from gallery.models import Image, Video
+from users.models import Profile
+from wiki.models import Document, Locale
+
+
+def run():
+    """Fix casing in locale code for sr-Cyrl and sr-Latn."""
+    Document.objects.filter(locale='sr-CYRL').update(locale='sr-Cyrl')
+    Locale.objects.filter(locale='sr-CYRL').update(locale='sr-Cyrl')
+    Image.objects.filter(locale='sr-CYRL').update(locale='sr-Cyrl')
+    Video.objects.filter(locale='sr-CYRL').update(locale='sr-Cyrl')
+    Profile.objects.filter(locale='sr-CYRL').update(locale='sr-Cyrl')
+
+    Document.objects.filter(locale='sr-LATN').update(locale='sr-Latn')
+    Locale.objects.filter(locale='sr-LATN').update(locale='sr-Latn')
+    Image.objects.filter(locale='sr-LATN').update(locale='sr-Latn')
+    Video.objects.filter(locale='sr-LATN').update(locale='sr-Latn')
+    Profile.objects.filter(locale='sr-LATN').update(locale='sr-Latn')
