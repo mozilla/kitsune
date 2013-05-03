48ee34da7fb1a2c5ae0e5822cf5b90b501aba28e
Ricky Rosario <rickyrosario@gmail.com>
2013-05-03 15:41:01 -0400

    [bug 868208] Remove focus and refine and improve subtopics.
    Yay, removed the code that does focus and refine. Tweaked the subtopics
    code and added a way to get back to parent topic.

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 
 from mobility.decorators import mobile_template
@@ -43,27 +42,16 @@ def document_listing(request, template, product_slug, topic_slug):
     """The document listing page for a product + topic."""
     product = get_object_or_404(Product, slug=product_slug)
     topic = get_object_or_404(Topic, slug=topic_slug)
-    refine_slug = request.GET.get('refine')
-    if refine_slug:
-        refine = get_object_or_404(Topic, slug=refine_slug)
-        topics = [topic, refine]
-    else:
-        refine = None
-        topics = [topic]
     documents, fallback_documents = documents_for(
-        locale=request.LANGUAGE_CODE, products=[product], topics=topics)
+        locale=request.LANGUAGE_CODE, products=[product], topics=[topic])
 
-    subtopics = (Topic.objects
-                      .filter(parent=topic)
-                      .annotate(num_docs=Count('document')))
+    subtopics = Topic.objects.filter(parent=topic)
 
     return render(request, template, {
         'product': product,
         'topic': topic,
         'topics': topics_for(products=[product]),
         'subtopics': subtopics,
-        'refine': refine,
-        'refine_topics': topics_for(products=[product], topics=[topic]),
         'documents': documents,
         'fallback_documents': fallback_documents,
         'search_params': {'product': product_slug}})
