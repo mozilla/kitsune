8ab9975b64013beef679fa9aaf0ee5808a18d500
Ricky Rosario <rickyrosario@gmail.com>
2013-06-05 11:34:14 -0400

    add comment

--- apps/sumo/static_finders.py
+++ apps/sumo/static_finders.py
@@ -3,7 +3,13 @@ from django.contrib.staticfiles.storage import StaticFilesStorage
 
 
 class WTFinder(BaseStorageFinder):
-    """A staticfiles finder that looks in STATIC_ROOT."""
+    """A staticfiles finder that looks in STATIC_ROOT.
+
+    This is super lame!
+
+    It is specifically for when DEBUG = True because jingo-minify puts
+    compiled files in STATIC_ROOT. gah!
+    """
     storage = StaticFilesStorage
 
     def list(self, ignore_patterns):
