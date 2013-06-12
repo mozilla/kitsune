69791579ef442ddf352d646469ad9bb4b282a080
Mike Cooper <mythmon@gmail.com>
2013-06-12 14:52:38 -0700

    [Bug 881476] Add selenium testcase and a test.

---
+++ kitsune/sumo/tests/test_ui.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from kitsune.sumo.urlresolvers import reverse
+from kitsune.sumo.tests import SeleniumTestCase
+
+from nose.tools import eq_
+
+
+class TestLanguageSwitch(SeleniumTestCase):
+
+    def test_english_to_spanish(self):
+        url = self.live_server_url + reverse('home', locale='en-US')
+        self.webdriver.get(url)
+
+        picker = self.webdriver.find_element_by_css_selector('.locale-picker')
+        eq_(picker.text, 'ENGLISH')
+        picker.click()
+
+        self.webdriver.find_element_by_css_selector('a[lang="es"]').click()
+
+        url = self.live_server_url + reverse('home', locale='es')
+        eq_(self.webdriver.current_url, url)
+
+        picker = self.webdriver.find_element_by_css_selector('.locale-picker')
+        eq_(picker.text, u'ESPAÑOL')
