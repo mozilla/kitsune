866eb51133cd04633f09c271c508dcafaf54e045
Shuhao Wu <shuhao@shuhaowu.com>
2013-06-21 17:07:59 -0700

    Removed another bogus import

--- kitsune/offline/urls.py
+++ kitsune/offline/urls.py
@@ -4,5 +4,4 @@ from django.conf.urls import patterns, url
 # http://<base>/offline/get-bundles ... etc.
 urlpatterns = patterns('kitsune.offline.views',
     url(r'^/get-bundles$', 'get_bundles', name='offline.get_bundles'),
-    url(r'^/get-languages$', 'get_languages', name='offline.get_languages')
 )
