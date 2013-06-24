d84cf52bce23b19dae31a14d744754a0562195e7
Ricky Rosario <rickyrosario@gmail.com>
2013-06-24 09:44:26 -0400

    Update anonymize script to blank out new topic images.

--- scripts/anonymize.sql
+++ scripts/anonymize.sql
@@ -30,6 +30,8 @@ UPDATE django_site SET
 -- We don't pull images from production, clearing out the fields
 -- let's us see the default placeholders.
 UPDATE topics_topic SET
-	image = NULL;
+    image = NULL;
 UPDATE products_product SET
-	image = NULL;
+    image = NULL;
+UPDATE products_topic SET
+    image = NULL;
