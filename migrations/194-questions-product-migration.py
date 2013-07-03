00a7f018b80d7ddf9cad775ee39d90813879253b
Gaurav Dadhania <gaurav.dadhania@gmail.com>
2013-07-03 15:33:19 +1000

    Fix wrong module name in migrations

--- migrations/194-questions-product-migration.py
+++ migrations/194-questions-product-migration.py
@@ -1,7 +1,7 @@
 from django.utils.encoding import smart_str
 
 from kitsune.products.models import Product
-from kitsune.taggit.models import Tag
+from taggit.models import Tag
 from kitsune.questions.models import Question
 
 tags_to_migrate = {
