065e50069ae8149922024af38001060432c710e8
Mike Cooper <mythmon@gmail.com>
2013-07-22 16:28:45 -0700

    Now with more speed *AND* correct logic!

--- migrations/226-retopic-questions.py
+++ migrations/226-retopic-questions.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from random import random
-
 from django.conf import settings
 
 from kitsune.products.models import Product, Topic
@@ -10,6 +8,7 @@ from kitsune.questions.models import Question
 
 
 def run():
+    # Make sure all topics listed in kitsune.questions.question_config exist.
     for prod_desc in question_config.products.values():
         for product_slug in prod_desc.get('products', []):
             product = Product.objects.get(slug=product_slug)
@@ -25,29 +24,12 @@ def run():
                 if created:
                     print ('Created missing topic %s/%s'
                            % (product_slug, topic_desc['topic']))
-    # Get all the old topics.
-    old_topics = OldTopic.objects.all().order_by('parent')
 
-    # For each of the old topics...
-    for old_topic in old_topics:
-        # Get the questions they apply to.
-        questions = Question.objects.filter(old_topics=old_topic)
-
-        # Get the products that apply to those documents.
-        products = Product.objects.filter(question__in=questions).distinct()
-
-        # For each product, create the topic if it doesn't exist and
-        # assign it to the documents.
-        for product in products:
-            print '%s / %s (%s questions)' % (product, old_topic.title, len(questions))
-            try:
-                topic = Topic.uncached.get(slug=old_topic.slug, product=product)
-
-                # Add the new topic to the appropriate docs.
-                for q in questions:
-                    q.topics.add(topic)
-            except Topic.DoesNotExist:
-                # This is rare, leave these questions without a topic.
-                print ('WARNING - No topic matching %s / %s, '
-                       'leaving %d questions possibly without a topic.'
-                       % (product.title, old_topic.title, len(questions)))
+    # Assign all the right new topics to the right old topics.
+    for product in Product.objects.all().select_related('questions'):
+        topics = Topic.objects.filter(product=product)
+        for topic in topics:
+            questions = Question.objects.filter(old_topics__slug=topic.slug)
+            print '%s / %s (%d questions)' % (product.title, topic.title, len(questions))
+            for q in questions:
+                q.topics.add(topic)
