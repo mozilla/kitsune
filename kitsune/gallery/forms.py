94fa5fec5935d8f4b96b5a65258acbac5c399ec2
Ricky Rosario <rickyrosario@gmail.com>
2013-07-24 15:01:17 -0400

    [bug 894947] Remove video upload code.

--- kitsune/gallery/forms.py
+++ kitsune/gallery/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 from tower import ugettext_lazy as _lazy, ugettext as _
 
-from kitsune.gallery.models import Image, Video
+from kitsune.gallery.models import Image
 from kitsune.lib.sumo_locales import LOCALES
 from kitsune.sumo.form_fields import StrippedCharField
 from kitsune.upload.forms import clean_image_extension
@@ -26,32 +26,14 @@ MSG_IMAGE_REQUIRED = _lazy(u'You have not selected an image to upload.')
 MSG_IMAGE_LONG = _lazy(
     u'Please keep the length of your image filename to %(max)s '
      'characters or less. It is currently %(length)s characters.')
-MSG_WEBM_LONG = _lazy(
-    u'Please keep the length of your webm filename to %(max)s '
-     'characters or less. It is currently %(length)s characters.')
-MSG_OGV_LONG = _lazy(
-    u'Please keep the length of your ogv filename to %(max)s '
-     'characters or less. It is currently %(length)s characters.')
-MSG_FLV_LONG = _lazy(
-    u'Please keep the length of your flv filename to %(max)s '
-     'characters or less. It is currently %(length)s characters.')
-MSG_VID_REQUIRED = _lazy(
-    u'The video has no files associated with it. You must upload one of the '
-     'following extensions: webm, ogv, flv.')
 MSG_TITLE_DRAFT = _lazy(u'Please select a different title.')
 
 TITLE_HELP_TEXT = _lazy(u'Include this in wiki syntax with [[%(type)s:title]]')
 DESCRIPTION_HELP_TEXT = _lazy(u'Provide a brief description of this media.')
 
 
-class UploadTypeForm(forms.Form):
-    type = forms.ChoiceField(
-        label=_lazy(u'Upload'), initial='image', widget=forms.RadioSelect,
-        choices=(('image', _lazy(u'Image')), ('video', _lazy('Video'))))
-
-
 class MediaForm(forms.ModelForm):
-    """Common abstractions for Image and Video forms."""
+    """Common abstractions for Image form."""
     locale = forms.ChoiceField(
                     required=False,
                     label=_lazy(u'Locale'),
@@ -109,52 +91,6 @@ class ImageForm(MediaForm):
     class Meta:
         model = Image
         fields = ('file', 'locale', 'title', 'description')
-
-
-class VideoForm(MediaForm):
-    """Video form."""
-    flv = forms.FileField(
-        required=False, error_messages={'max_length': MSG_FLV_LONG},
-        max_length=settings.MAX_FILENAME_LENGTH, label=_lazy('Video (flv)'))
-    ogv = forms.FileField(required=False, label=_lazy('Video (ogv)'),
-                          error_messages={'max_length': MSG_OGV_LONG},
-                          max_length=settings.MAX_FILENAME_LENGTH)
-    webm = forms.FileField(required=False, label=_lazy('Video (webm)'),
-                           error_messages={'max_length': MSG_WEBM_LONG},
-                           max_length=settings.MAX_FILENAME_LENGTH)
-    thumbnail = forms.ImageField(
-        label=_lazy('Thumbnail'),
-        required=False, error_messages={'max_length': MSG_IMAGE_LONG},
-        max_length=settings.MAX_FILENAME_LENGTH)
-
-    def __init__(self, *args, **kwargs):
-        super(VideoForm, self).__init__(*args, **kwargs)
-        self.fields['flv'].help_text = _(
-            'Select a Flash video (FLV) to upload.')
-        self.fields['ogv'].help_text = _('Select an Ogv video to upload.')
-        self.fields['webm'].help_text = _('Select a WebM video to upload.')
-        self.fields['thumbnail'].help_text = _(
-            'Suggested size: {width}x{height}px').format(
-                width=settings.THUMBNAIL_SIZE, height=settings.THUMBNAIL_SIZE)
-
-    def clean(self):
-        """Ensure one of the supported file formats has been uploaded"""
-        c = super(VideoForm, self).clean()
-        if not ('webm' in c and c['webm'] and
-                    c['webm'].name.endswith('.webm') or
-                'ogv' in c and c['ogv'] and
-                    (c['ogv'].name.endswith('.ogv') or
-                     c['ogv'].name.endswith('.ogg')) or
-                'flv' in c and c['flv'] and c['flv'].name.endswith('.flv') or
-                'thumbnail' in c and c['thumbnail']):
-            raise ValidationError(MSG_VID_REQUIRED)
-        clean_image_extension(c.get('thumbnail'))
-        return self.cleaned_data
-
-    class Meta:
-        model = Video
-        fields = ('webm', 'ogv', 'flv', 'thumbnail', 'locale',
-                  'title', 'description')
 
 
 def save_form(form, update_user=None, is_draft=True, **kwargs):
--- kitsune/gallery/utils.py
+++ kitsune/gallery/utils.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.core.files import File
 
-from kitsune.gallery.forms import ImageForm, VideoForm
-from kitsune.gallery.models import Image, Video
+from kitsune.gallery.forms import ImageForm
+from kitsune.gallery.models import Image
 from kitsune.sumo.urlresolvers import reverse
 from kitsune.upload.utils import _image_to_png, upload_media, check_file_size
 from kitsune.upload.tasks import _scale_dimensions
@@ -46,50 +46,6 @@ def create_image(files, user):
 def upload_image(request):
     """Uploads an image from the request."""
     return upload_media(request, ImageForm, create_image)
-
-
-def create_video(files, user):
-    """Given an uploaded file, a user, and other data, it creates a Video"""
-    try:
-        vid = Video.objects.filter(creator=user, is_draft=True)
-        # Delete other drafts, if any:
-        vid.exclude(pk=vid[0].pk).delete()
-        vid = vid[0]
-    except IndexError:  # No drafts, create one
-        vid = Video(creator=user, is_draft=True)
-    # Async uploads fallback to these defaults.
-    vid.title = get_draft_title(user)
-    vid.description = u'Autosaved draft.'
-    vid.locale = settings.WIKI_DEFAULT_LANGUAGE
-
-    for name in files:
-        up_file = files[name]
-        check_file_size(up_file, settings.VIDEO_MAX_FILESIZE)
-        # name is in (webm, ogv, flv) sent from upload_video(), below
-        getattr(vid, name).save(up_file.name, up_file, save=False)
-
-        if 'thumbnail' == name:
-            # Save poster too, we shrink it later.
-            vid.poster.save(up_file.name, up_file, save=False)
-
-    vid.save()
-    if 'thumbnail' in files:
-        thumb = vid.thumbnail
-        (width, height) = _scale_dimensions(thumb.width, thumb.height)
-    else:
-        width = settings.THUMBNAIL_PROGRESS_WIDTH
-        height = settings.THUMBNAIL_PROGRESS_HEIGHT
-    delete_url = reverse('gallery.delete_media', args=['video', vid.id])
-    return {'name': up_file.name, 'url': vid.get_absolute_url(),
-            'thumbnail_url': vid.thumbnail_url_if_set(),
-            'width': width,
-            'height': height,
-            'delete_url': delete_url}
-
-
-def upload_video(request):
-    """Uploads a video from the request; accepts multiple submitted formats"""
-    return upload_media(request, VideoForm, create_video)
 
 
 def check_media_permissions(media, user, perm_type):
