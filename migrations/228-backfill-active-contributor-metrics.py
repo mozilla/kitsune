57b989c592ad47ae7e478013ee79fbf691486749
Ricky Rosario <rickyrosario@gmail.com>
2013-07-31 14:04:58 -0400

    last 12 months

--- migrations/228-backfill-active-contributor-metrics.py
+++ migrations/228-backfill-active-contributor-metrics.py
@@ -5,13 +5,19 @@ from kitsune.dashboards.cron import update_l10n_contributor_metrics
 
 
 def run():
-    # Run for ever first of every month this year.
+    # Run for every first of every month for the past year.
     today = date.today()
-    year = today.year
-    month = today.month
-    i = 1
+    current_year = today.year
+    current_month = today.month
 
-    while i <= month:
-        day = date(year, i, 1)
+    year = current_year - 1
+    month = current_month - 1
+
+    while year < current_year or month <= current_month:
+        day = date(year, month, 1)
         update_l10n_contributor_metrics(day)
-        i += 1
+
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
