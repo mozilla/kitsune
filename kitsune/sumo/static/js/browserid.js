9947493e8bc85372a13ce11a99984b3882c63f5e
Rehan Dalal <rehandalal@gmail.com>
2013-08-23 13:16:49 -0400

    [bug 905118] Redirect correctly on sign in with Persona

--- kitsune/sumo/static/js/browserid.js
+++ kitsune/sumo/static/js/browserid.js
@@ -2,24 +2,27 @@
     $(function() {
         $(document).on('click', '.browserid-login', function(e) {
             e.preventDefault();
+
+            var $this = $(this);
+
+            var $form;
+            if ($this.data('form')) {
+                $form = $($this.data('form'));
+            } else {
+                $form = $('#browserid-form');
+            }
+
+            if ($this.data('next')) {
+                $form.find('input[name="next"]').val($this.data('next'));
+            }
+
             navigator.id.get(function(assertion) {
                 if (assertion) {
-                    var $e = $('#browserid-form input[name="assertion"]');
+                    var $e = $form.find('input[name="assertion"]');
                     $e.val(assertion.toString());
-                    $e.parent().submit();
+                    $form.submit();
                 }
             });
         });
-
-        $(document).on('click', '#aaq-browserid-login', function(e) {
-            e.preventDefault();
-            navigator.id.get(function(assertion) {
-                if (assertion) {
-                    var $e = $('#aaq-browserid-form input[name="assertion"]');
-                    $e.val(assertion.toString());
-                    $e.parent().submit();
-                }
-            });
-        })
     });
 })(jQuery);
