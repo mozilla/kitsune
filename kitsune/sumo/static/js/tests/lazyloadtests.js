f15295be389a38df8fe03ada644675e3629b52a4
Ricky Rosario <rickyrosario@gmail.com>
2013-09-19 11:34:58 -0400

    Stop causing stacktraces from image requests.

--- kitsune/sumo/static/js/tests/lazyloadtests.js
+++ kitsune/sumo/static/js/tests/lazyloadtests.js
@@ -7,9 +7,9 @@ module('lazyload');
 test('load original image', function() {
     var img = new Image();
     $(img).addClass("lazy");
-    $(img).data("original-src", "test.jpg");
+    $(img).data("original-src", "http://example.com/test.jpg");
     $.fn.lazyload.loadOriginalImage($(img));
-    equals($(img).attr("src"), 'test.jpg', 'src attribute set correctly');
+    equals($(img).attr("src"), 'http://example.com/test.jpg', 'src attribute set correctly');
     equals($(img).data("original-src"), undefined, 'original-src data attribute cleared correctly');
 });
 });
