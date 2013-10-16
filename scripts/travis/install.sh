ba0a6a29c2fd698bad083efcc521d87cf0c1eb70
Mike Cooper <mythmon@gmail.com>
2013-10-16 13:14:35 -0700

    Make test setup and install quieter.

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 # pwd is the git repo.
 set -e
-uname -a
-date
 
-echo "Fixing path issues"
+echo "Fix path issues"
 sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so ~/virtualenv/python2.6/lib/
 sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so ~/virtualenv/python2.6/lib/
 sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so ~/virtualenv/python2.6/lib/
 
 echo "Install Python dependencies"
-pip install -r requirements/compiled.txt
-pip install nosenicedots
+pip install -r requirements/compiled.txt > /dev/null
+pip install nosenicedots > /dev/null
+pip freeze
+echo
 
 echo "Installing Node.js dependencies"
-npm install
+npm install > /dev/null 2> /dev/null
+npm list
+echo
 
-echo "Extracting ElasticSearch"
-tar xzvf vendor/tarballs/elasticsearch-0.20.5.tar.gz
+echo "Installing ElasticSearch"
+tar xzvf vendor/tarballs/elasticsearch-0.20.5.tar.gz > /dev/null
 
-echo "Building Redis"
-tar xzvf vendor/tarballs/redis-2.4.11.tar.gz
+echo "Installing Redis"
+tar xzvf vendor/tarballs/redis-2.4.11.tar.gz > /dev/null
 pushd redis-2.4.11
-  make
+  make > /dev/null 2> /dev/null
 popd
