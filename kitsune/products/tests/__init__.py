e448f0c1737634f13812ad2cfe490b706e9be749
Mike Cooper <mythmon@gmail.com>
2013-11-12 17:35:59 -0800

    Add db stuff for versions and platforms.

--- kitsune/products/tests/__init__.py
+++ kitsune/products/tests/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from random import randint
 
 from django.template.defaultfilters import slugify
 
-from kitsune.products.models import Product, Topic
+from kitsune.products.models import Product, Topic, Version
 from kitsune.sumo.tests import with_save
 
 
@@ -36,3 +37,40 @@ def topic(**kwargs):
         defaults['product'] = product(save=True)
 
     return Topic(**defaults)
+
+
+@with_save
+def version(**kwargs):
+    """Create and return a version."""
+    v = randint(0, 1000)
+
+    defaults = {
+        'name': 'Version %d.0' % v,
+        'slug': 'v%d' % v,
+        'min_version': v,
+        'max_version': v + 1,
+        'visible': True,
+        'default': False,
+    }
+    defaults.update(kwargs)
+
+    if 'product' not in kwargs:
+        defaults['product'] = product(save=True)
+
+    return Version(**defaults)
+
+
+@with_save
+def platform(**kwargs):
+    """Create and return a platform."""
+    v = randint(0, 1000)
+
+    defaults = {
+        'name': 'Platform %d' % v,
+        'slug': 'platform%d' % v,
+        'visible': True,
+        'display_order': 0,
+    }
+    defaults.update(kwargs)
+
+    return Version(**defaults)
