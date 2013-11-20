94aeaca9d405ce3cd7f398654ee6702c05354896
Rehan Dalal <rehandalal@gmail.com>
2013-11-20 01:42:55 -0500

    oops

--- kitsune/sumo/static/js/products.js
+++ kitsune/sumo/static/js/products.js
@@ -1,7 +1,8 @@
 (function($, BD) {
     $(function() {
         $('.download-firefox .download-button').on('click', function(ev) {
-            if (BD.version == $(this).data('latest-version')) {
+            var latest_version = $(this).data('latest-version');
+            if ((BD.version == latest_version) && (BD.browser == 'fx')) {
                 ev.stopPropagation();
                 $(this).siblings('.help-bubble').show();
             }
