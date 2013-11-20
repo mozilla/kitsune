2f5e86f6af866c516eb1f3df8b70ac757a217c8d
Rehan Dalal <rehandalal@gmail.com>
2013-11-20 11:11:58 -0500

    fix

--- kitsune/sumo/static/js/products.js
+++ kitsune/sumo/static/js/products.js
@@ -4,6 +4,7 @@
             var latest_version = $(this).data('latest-version');
             if ((BD.version == latest_version) && (BD.browser == 'fx')) {
                 ev.stopPropagation();
+                ev.preventDefault();
                 $(this).siblings('.help-bubble').show();
             }
         });
