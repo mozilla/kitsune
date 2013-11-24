15e7afe50595e65ea24dbf142bfacc65bbdc820b
Rehan Dalal <rehandalal@gmail.com>
2013-11-24 00:49:16 -0500

    [bug 923732] Create a badge list page

--- kitsune/kbadge/urls.py
+++ kitsune/kbadge/urls.py
@@ -6,7 +6,7 @@ badger.autodiscover()
 
 urlpatterns = patterns(
     'badger.views',
-
+    url(r'^$', 'badges_list', name='badger.badges_list'),
     url(r'^awards/?$', 'awards_list', name='badger.awards_list'),
     url(r'^badge/(?P<slug>[^/]+)/awards/(?P<id>\d+)/?$', 'award_detail',
         name='badger.award_detail'),
