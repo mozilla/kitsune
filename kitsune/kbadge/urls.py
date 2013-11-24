6b0f9b37a8e3fc390d83d604ab0f1f15c6c54e78
Rehan Dalal <rehandalal@gmail.com>
2013-11-24 04:46:25 -0500

    fixup

--- kitsune/kbadge/urls.py
+++ kitsune/kbadge/urls.py
@@ -4,8 +4,7 @@ import badger
 badger.autodiscover()
 
 
-urlpatterns = patterns(
-    'badger.views',
+urlpatterns = patterns('badger.views',
     url(r'^$', 'badges_list', name='badger.badges_list'),
     url(r'^awards/?$', 'awards_list', name='badger.awards_list'),
     url(r'^badge/(?P<slug>[^/]+)/awards/(?P<id>\d+)/?$', 'award_detail',
