7cd95bf449a6a5448d8e8dec661e69b1da525036
Rehan Dalal <rehandalal@gmail.com>
2013-11-25 17:46:40 -0500

    [bug 931748] Fix question traffic stats cron

--- kitsune/sumo/googleanalytics.py
+++ kitsune/sumo/googleanalytics.py
@@ -164,37 +164,50 @@ def pageviews_by_question(start_date, end_date):
     start_index = 1
     max_results = 10000
 
-    while True:  # To deal with pagination
+    end_date_step = end_date
 
-        @retry_503
-        def _make_request():
-            return request.get(
-                ids='ga:' + profile_id,
-                start_date=str(start_date),
-                end_date=str(end_date),
-                metrics='ga:pageviews',
-                dimensions='ga:pagePath',
-                filters='ga:pagePathLevel2==/questions/',
-                max_results=max_results,
-                start_index=start_index).execute()
+    while True: # To reduce the size of result set request 3 months at a time
+        start_date_step = end_date_step - timedelta(90)
 
-        results = _make_request()
+        if start_date_step < start_date:
+            start_date_step = start_date
 
-        for result in results['rows']:
-            path = result[0]
-            pageviews = int(result[1])
-            question_id = Question.from_url(path, id_only=True)
-            if not question_id:
-                continue
+        while True:  # To deal with pagination
 
-            # The same question can appear multiple times due to url params
-            # and locale.
-            counts[question_id] = counts.get(question_id, 0) + pageviews
+            @retry_503
+            def _make_request():
+                return request.get(
+                    ids='ga:' + profile_id,
+                    start_date=str(start_date_step),
+                    end_date=str(end_date_step),
+                    metrics='ga:pageviews',
+                    dimensions='ga:pagePath',
+                    filters='ga:pagePathLevel2==/questions/',
+                    max_results=max_results,
+                    start_index=start_index).execute()
 
-        # Move to next page of results.
-        start_index += max_results
-        if start_index > results['totalResults']:
+            results = _make_request()
+
+            for result in results['rows']:
+                path = result[0]
+                pageviews = int(result[1])
+                question_id = Question.from_url(path, id_only=True)
+                if not question_id:
+                    continue
+
+                # The same question can appear multiple times due to url params
+                # and locale.
+                counts[question_id] = counts.get(question_id, 0) + pageviews
+
+            # Move to next page of results.
+            start_index += max_results
+            if start_index > results['totalResults']:
+                break
+
+        if start_date_step == start_date:
             break
+
+        end_date_step = end_date_step - timedelta(91)
 
     return counts
 
