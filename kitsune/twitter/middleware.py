5ee044f64e6af79c5c9a885b3226f0a9d2e7007e
Will Kahn-Greene <willkg@mozilla.com>
2013-12-18 11:14:18 -0500

    [bug 951702] Handle IOErrors in twitter middleware
    Pretty sure this doesn't affect users, but it is a source of errors in
    Sentry and it's easy to fix.
    
    In order to fix it, I had to tweak the structure of the code from one big
    if-statement into smaller blocks that exit out when they're done.

--- kitsune/twitter/middleware.py
+++ kitsune/twitter/middleware.py
@@ -29,81 +29,89 @@ class SessionMiddleware(object):
 
         ssl_url = url(request, {'scheme': 'https' if is_secure else 'http'})
 
-        if request.REQUEST.get('twitter_delete_auth'):
-            request.twitter = Session()
-            return http.HttpResponseRedirect(url(request))
+        try:
+            if request.REQUEST.get('twitter_delete_auth'):
+                request.twitter = Session()
+                return http.HttpResponseRedirect(url(request))
+        except IOError:
+            # Django throws an IOError if there's a bad read when parsing
+            # the request data. If it throws an error, this is not a good
+            # request and there's certainly no twitter stuff here.
+            return
 
-        elif request.twitter.authed:
+        if request.twitter.authed:
             request.twitter.api = Twython(settings.TWITTER_CONSUMER_KEY,
                                           settings.TWITTER_CONSUMER_SECRET,
                                           request.twitter.key,
                                           request.twitter.secret,
                                           ssl_verify=True)
-        else:
-            verifier = request.GET.get('oauth_verifier')
-            if verifier:
-                # We are completing an OAuth login
+            return
 
-                request_key = request.COOKIES.get(REQUEST_KEY_NAME)
-                request_secret = request.COOKIES.get(REQUEST_SECRET_NAME)
 
-                if (validate_token(request_key) and
-                        validate_token(request_secret)):
+        verifier = request.GET.get('oauth_verifier')
+        if verifier:
+            # We are completing an OAuth login
 
-                    t = Twython(settings.TWITTER_CONSUMER_KEY,
-                                settings.TWITTER_CONSUMER_SECRET,
-                                request_key,
-                                request_secret,
-                                ssl_verify=True)
+            request_key = request.COOKIES.get(REQUEST_KEY_NAME)
+            request_secret = request.COOKIES.get(REQUEST_SECRET_NAME)
 
-                    try:
-                        tokens = t.get_authorized_tokens(verifier)
-                    except (TwythonError, TwythonAuthError):
-                        log.warning('Twython Error with verifier token')
-                        pass
-                    else:
-                        # Override path to drop query string.
-                        ssl_url = url(
-                            request,
-                            {'scheme': 'https' if is_secure else 'http',
-                             'path': request.path})
-                        response = http.HttpResponseRedirect(ssl_url)
+            if (validate_token(request_key) and
+                    validate_token(request_secret)):
 
-                        request.twitter.screen_name = tokens['screen_name']
-
-                        Session(
-                            tokens['oauth_token'],
-                            tokens['oauth_token_secret']).save(request,
-                                                               response)
-
-                        return response
-                else:
-                    # request tokens didn't validate
-                    log.warning("Twitter Oauth request tokens didn't validate")
-
-            elif request.REQUEST.get('twitter_auth_request'):
-                # We are requesting Twitter auth
                 t = Twython(settings.TWITTER_CONSUMER_KEY,
                             settings.TWITTER_CONSUMER_SECRET,
+                            request_key,
+                            request_secret,
                             ssl_verify=True)
+
                 try:
-                    auth_props = t.get_authentication_tokens(
-                        callback_url=ssl_url)
+                    tokens = t.get_authorized_tokens(verifier)
                 except (TwythonError, TwythonAuthError):
-                    log.warning('Twython error while getting authorization '
-                                'url')
+                    log.warning('Twython Error with verifier token')
+                    pass
                 else:
-                    response = http.HttpResponseRedirect(
-                        auth_props['auth_url'])
-                    response.set_cookie(
-                        REQUEST_KEY_NAME,
-                        auth_props['oauth_token'],
-                        secure=is_secure)
-                    response.set_cookie(
-                        REQUEST_SECRET_NAME,
-                        auth_props['oauth_token_secret'],
-                        secure=is_secure)
+                    # Override path to drop query string.
+                    ssl_url = url(
+                        request,
+                        {'scheme': 'https' if is_secure else 'http',
+                         'path': request.path})
+                    response = http.HttpResponseRedirect(ssl_url)
+
+                    request.twitter.screen_name = tokens['screen_name']
+
+                    Session(
+                        tokens['oauth_token'],
+                        tokens['oauth_token_secret']).save(request,
+                                                           response)
+
                     return response
+            else:
+                # request tokens didn't validate
+                log.warning("Twitter Oauth request tokens didn't validate")
+
+        elif request.REQUEST.get('twitter_auth_request'):
+            # We are requesting Twitter auth
+            t = Twython(settings.TWITTER_CONSUMER_KEY,
+                        settings.TWITTER_CONSUMER_SECRET,
+                        ssl_verify=True)
+            try:
+                auth_props = t.get_authentication_tokens(
+                    callback_url=ssl_url)
+            except (TwythonError, TwythonAuthError):
+                log.warning('Twython error while getting authorization '
+                            'url')
+            else:
+                response = http.HttpResponseRedirect(
+                    auth_props['auth_url'])
+                response.set_cookie(
+                    REQUEST_KEY_NAME,
+                    auth_props['oauth_token'],
+                    secure=is_secure)
+                response.set_cookie(
+                    REQUEST_SECRET_NAME,
+                    auth_props['oauth_token_secret'],
+                    secure=is_secure)
+                return response
 
     def process_response(self, request, response):
         if getattr(request, 'twitter', False):
