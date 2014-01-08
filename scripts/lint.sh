e11b3b2e401012fffd57ce37a33d2eeb652c7530
Mike Cooper <mythmon@gmail.com>
2014-01-08 11:34:44 -0800

    Add lint checks to travis.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -22,17 +22,22 @@ FLAKE8_IGNORE=(
 # Files to lint is either the list of arguments to this script, or the
 # kitsune directory.
 FILES="$@"
-FLAKE8_FILES=()
 
+FLAKE8_FILES=()
 for f in $FILES; do
     # If it is a directory, or if it is a python file, lint it with flake8.
     if [[ -d $f ]] || echo $f | grep -E '.py$' > /dev/null; then
         FLAKE8_FILES+=($f)
     fi
 done
-
 FLAKE8_IGNORE=$(IFS=,; echo "${FLAKE8_IGNORE[*]}")
-flake8 --exclude=$FLAKE8_IGNORE ${FLAKE8_FILES[*]}
-FLAKE_RETURN=$?
+
+# If there are files to run through flake8, do it.
+if [[ -n ${FLAKE8_FILES[*]} ]]; then
+  flake8 --exclude=$FLAKE8_IGNORE ${FLAKE8_FILES[*]}
+  FLAKE_RETURN=$?
+else
+  FLAKE_RETURN=0
+fi
 
 exit $FLAKE_RETURN
