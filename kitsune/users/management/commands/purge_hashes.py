2b625b73e6bcc4069c5ab632dd92fbad5d5cfd16
Ricky Rosario <rickyrosario@gmail.com>
2014-04-16 15:08:33 -0400

    [bug 994070] Update purge_hashes for django 1.6

--- kitsune/users/management/commands/purge_hashes.py
+++ kitsune/users/management/commands/purge_hashes.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import UNUSABLE_PASSWORD
+from django.contrib.auth.hashers import UNUSABLE_PASSWORD_PREFIX
 from django.core.management.base import BaseCommand
 
 
@@ -11,6 +11,9 @@ class Command(BaseCommand):
     def handle(self, *args, **kw):
         old = datetime.now() - timedelta(365)
         users = User.objects.filter(last_login__lt=old)
-        users = users.exclude(password=UNUSABLE_PASSWORD)
-        num = users.update(password=UNUSABLE_PASSWORD)
-        print 'Cleared %d passwords.' % num
+        users = users.exclude(password__startswith=UNUSABLE_PASSWORD_PREFIX)
+        for user in users:
+            user.set_unusable_password()
+            user.save()
+
+        print 'Cleared %d passwords.' % len(users)
