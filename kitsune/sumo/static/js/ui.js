mozilla/kitsune

commit dda371eaf0f3424ce76e4c39d532e9cb4376ea7c

[bug 939803] Make mobile banner remember closed state

kitsune/sumo/static/js/ui.js
@@ -19,8 +19,22 @@
       var $this = $(this);
       if ($this.data('close-id')) {
         $('#' + $this.data('close-id')).hide();
+        if ($this.data('close-memory') == 'remember') {
+          if (Modernizr.localstorage) {
+            localStorage.setItem($this.data('close-id') + '.closed', true);
+          }
+        }
       } else {
         $this.parent().hide();
+      }
+    });
+
+    $('[data-close-memory="remember"]').each(function() {
+      var id = $this.data('close-id');
+      if (id) {
+        if (localStorage.getItem(id + '.closed') === true) {
+          $('#' + id).hide();
+        }
       }
     });
 

kitsune/sumo/templates/base.html
@@ -103,8 +103,8 @@
     <a/>
   </div>
 {% else %}
-  <div class="mobile-banner">
-    <div class="close-button">X</div>
+  <div class="mobile-banner" id="mobile-warning">
+    <div class="close-button" data-close-id="mobile-warning" data-close-memory="remember">X</div>
     <a href="?{{ request.QUERY_STRING }}&amp;mobile=1">
       {{ _('Tap here to go to the mobile version of the site.') }}
     </a>

commit 07359c95390096c2df5de1e41da9381c91881b81

Fix bad variable name in ui.js
This was a bug introduced in dda371ea.

kitsune/sumo/static/js/ui.js
@@ -30,7 +30,7 @@
     });
 
     $('[data-close-memory="remember"]').each(function() {
-      var id = $this.data('close-id');
+      var id = $(this).data('close-id');
       if (id) {
         if (localStorage.getItem(id + '.closed') === true) {
           $('#' + id).hide();