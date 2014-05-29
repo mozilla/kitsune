a201ddf5edaadd9c0eefc7d95c4d3293797b3a1d
Mike Cooper <mythmon@gmail.com>
2014-05-29 15:20:28 -0700

    Test and fix LocaleNegotiationMixin.

--- kitsune/sumo/api.py
+++ kitsune/sumo/api.py
@@ -22,17 +22,23 @@ class LocaleNegotiationMixin(object):
     """A mixin for CBV to select a locale based on Accept-Language headers."""
 
     def get_locale(self):
-        acceptable_locales = self.request.META.get('HTTP_ACCEPT_LANGUAGE', 'en-US')
+        acceptable_locales = self.request.META.get('HTTP_ACCEPT_LANGUAGE',
+                                                   'en-US')
         acceptable_locales = acceptable_locales.split(',')
         acceptable_locales = [l.split(';')[0] for l in acceptable_locales]
 
-        for locale in acceptable_locales:
-            if locale in settings.SUMO_LANGUAGES:
-                return locale
-            if locale in settings.NON_SUPPORTED_LOCALES:
-                locale = settings.NON_SUPPORTED_LOCALES
-                if locale is None:
-                    locale = 'en-US'
-                return locale
+        sumo_languages = dict((l.lower(), l) for l in settings.SUMO_LANGUAGES)
+        non_supported_locales = dict((k.lower(), v)
+                                     for k, v
+                                     in settings.NON_SUPPORTED_LOCALES.items())
 
-        return 'en-US'
+        for locale in acceptable_locales:
+            locale = locale.lower()
+            if locale in sumo_languages:
+                return sumo_languages[locale]
+            if locale in non_supported_locales:
+                locale = non_supported_locales[locale]
+                if locale is not None:
+                    return locale
+
+        return settings.WIKI_DEFAULT_LANGUAGE
