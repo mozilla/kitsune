772bbcc4c3846f810aa1a081c7b5b06b4aa88db9
Dean Johnson <deanjohnson222@gmail.com>
2014-06-20 10:52:29 -0700

    [bug 905834] Changed sys.path arrangement
    By moving the items to the back of the path, instead of the front,
    the lookup times for /vendor packages is halved. In the vagrant env
    we went from 1 minute 56 seconds load time for ./manage.py runserver to
    54 seconds.

--- manage.py
+++ manage.py
@@ -2,6 +2,21 @@
 import os
 import sys
 
+ROOT = os.path.dirname(os.path.abspath(__file__))
+path = lambda *a: os.path.join(ROOT, *a)
+
+prev_sys_path = list(sys.path)
+
+site.addsitedir(path('vendor'))
+
+# Move the new items to the front of sys.path.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
 # Now we can import from third-party libraries.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitsune.settings_local')
