5f897f7cce9802469f4f5c836ac4d32c5817ba5c
Bogdan Ștefan <bogdan.stefan3@gmail.com>
2014-07-01 18:27:40 +0300

    [Bug 1031906] Update error message in users/forms.py

--- kitsune/users/forms.py
+++ kitsune/users/forms.py
@@ -23,7 +23,7 @@ from kitsune.users.widgets import FacebookURLWidget, TwitterURLWidget
 
 
 USERNAME_INVALID = _lazy(u'Username may contain only English letters, '
-                         'numbers and ./+/-/_ characters.')
+                         'numbers and ./-/_ characters.')
 USERNAME_REQUIRED = _lazy(u'Username is required.')
 USERNAME_SHORT = _lazy(u'Username is too short (%(show_value)s characters). '
                        'It must be at least %(limit_value)s characters.')
@@ -86,7 +86,7 @@ class RegisterForm(forms.ModelForm):
         label=_lazy(u'Username:'), max_length=30, min_length=4,
         regex=r'^[\w.-]+$',
         help_text=_lazy(u'Required. 30 characters or fewer. Letters, digits '
-                        u'and ./+/- only.'),
+                        u'and ./- only.'),
         error_messages={'invalid': USERNAME_INVALID,
                         'required': USERNAME_REQUIRED,
                         'min_length': USERNAME_SHORT,
