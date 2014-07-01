c9fb9f1b0916f549b5f18125993a7fa0543e6bff
Rehan Dalal <rehandalal@gmail.com>
2014-07-01 07:12:47 -0400

    [bug 970214] Send a weekly "Ready for review" digest

--- kitsune/wiki/cron.py
+++ kitsune/wiki/cron.py
@@ -1,9 +1,18 @@
 import cronjobs
 import waffle
 
+from itertools import chain
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models import F, Q, ObjectDoesNotExist
+
+from tower import ugettext as _
+
 from kitsune.search.tasks import index_task
+from kitsune.sumo import email_utils
 from kitsune.wiki import tasks
-from kitsune.wiki.models import DocumentMappingType
+from kitsune.wiki.models import Document, DocumentMappingType, Revision, Locale
 
 
 @cronjobs.register
@@ -19,3 +28,48 @@ def rebuild_kb():
 def reindex_kb():
     """Reindex wiki_document."""
     index_task.delay(DocumentMappingType, DocumentMappingType.get_indexable())
+
+
+@cronjobs.register
+def send_weekly_ready_for_review_digest():
+    """Sends out the weekly "Ready for review" digest email."""
+
+    @email_utils.safe_translation
+    def _send_mail(locale, user, context):
+        subject = _('[Reviews Pending] SUMO needs your help!')
+
+        mail = email_utils.make_mail(
+            subject=subject,
+            text_template='wiki/email/ready_for_review_weekly_digest.ltxt',
+            html_template='wiki/email/ready_for_review_weekly_digest.html',
+            context_vars=context,
+            from_email=settings.TIDINGS_FROM_ADDRESS,
+            to_email=user.email)
+
+        email_utils.send_messages([mail])
+
+    # Get the list of revisions ready for review
+    revs = Revision.objects.filter(reviewed=None).filter(
+        Q(document__current_revision_id__lt=F('id')) |
+        Q(document__current_revision_id=None))
+
+    locales = revs.values_list('document__locale', flat=True).distinct()
+
+    for l in locales:
+        docs = revs.filter(document__locale=l).values_list(
+            'document', flat=True).distinct()
+        docs = Document.objects.filter(id__in=docs)
+
+        try:
+            leaders = Locale.objects.get(locale=l).leaders.all()
+            reviewers = Locale.objects.get(locale=l).reviewers.all()
+            users = list(chain(leaders, reviewers))
+        except ObjectDoesNotExist:
+            # Locale does not exist, so skip to the next locale
+            continue
+
+        for u in users:
+            _send_mail(l, u, {
+                'recipient': u,
+                'docs': docs,
+            })
