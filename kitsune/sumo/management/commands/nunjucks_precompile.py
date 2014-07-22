8311c86d44cc1d89b3fc79b2cd91e9a6017be037
Rehan Dalal <rehandalal@gmail.com>
2014-07-22 07:19:02 -0400

    [bug 1039523] Infrastructure for instant search

---
+++ kitsune/sumo/management/commands/nunjucks_precompile.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+ROOT = os.path.realpath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+path = lambda *a: os.path.join(ROOT, *a)
+
+
+class Command(BaseCommand):
+    help = 'Precompiles nunjuck templates'
+
+    def handle(self, *args, **kwargs):
+        files = os.listdir(path('static/tpl'))
+
+        for f in files:
+            if f.endswith('.html'):
+                tpl = f[:-5]
+                cmd = '%s %s > %s' % (
+                    settings.NUNJUCKS_PRECOMPILE_BIN,
+                    path('static/tpl'),
+                    path('static/js/templates/%s.js' % tpl))
+                subprocess.call(cmd, shell=True)
