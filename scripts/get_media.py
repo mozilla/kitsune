e83eda810eb793bde2905a5f501742c6728e0c4d
Mike Cooper <mythmon@gmail.com>
2014-08-25 13:26:29 -0700

    Safely iterate through missing Image queries.

--- scripts/get_media.py
+++ scripts/get_media.py
@@ -18,13 +18,18 @@ BASE_URL = 'https://support.cdn.mozilla.net'
 
 
 def run():
-    images = list(Image.objects.all())
+    # Fetching things from the DB can error, which is sad.
+    images = list(safe_exhaust_queryset(Image.objects.all()))
     progress = Progress(len(images), 100)
     progress.draw()
 
     for img in images:
         url = BASE_URL + img.file.url
         path = img.file.url[1:]  # Remove leading '/'
+
+        if os.path.exists(path):
+            progress.tick()
+            continue
 
         try:
             os.makedirs(os.path.dirname(path))
@@ -36,5 +41,22 @@ def run():
             urllib.urlretrieve(url, path)
         except urllib.ContentTooShortError:
             print "Couldn't download", path
+            try:
+                os.unlink(path)
+            except IOError:
+                pass
         progress.tick()
     print '\nDone'
+
+
+def safe_exhaust(it):
+    """Safely exhaust an iterable, skipping any items that raise IOErrors."""
+    it = iter(it)
+    while True:
+        try:
+            yield next(it)
+        except StopIteration:
+            break
+        except IOError as e:
+            if e.errno != 2:
+                raise e
