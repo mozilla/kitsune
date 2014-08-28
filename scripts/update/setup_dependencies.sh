063bf311a6088152428d4d4e344df03fce5bdbc3
dean <deanjohnson222@gmail.com>
2014-08-28 13:51:59 -0700

    [bug 1035319] Stop checking for virtualenv dir.

--- scripts/update/setup_dependencies.sh
+++ scripts/update/setup_dependencies.sh
@@ -3,9 +3,8 @@
 # This creates a virtualenv and installs our requirements into it.
 # CWD should be the project folder, i.e. kitsune/.
 
-if [ ! -d "virtualenv" ]; then
-    virtualenv --no-site-packages --verbose virtualenv
-fi
+virtualenv --no-site-packages --verbose virtualenv
+
 source virtualenv/bin/activate
 python scripts/peep.py install -r requirements/requirements_src.txt
 python scripts/peep.py install -r requirements/requirements_packages.txt
