6c419a8cc57701d534455981ecd337127f287684
dean <deanjohnson222@gmail.com>
2014-08-28 13:38:19 -0700

    [bug 1035319] Made virtualenv installs explicit.

--- scripts/update/setup_dependencies.sh
+++ scripts/update/setup_dependencies.sh
@@ -4,11 +4,11 @@
 # CWD should be the project folder, i.e. kitsune/.
 
 if [ ! -d "virtualenv" ]; then
-    virtualenv --system-site-packages --verbose virtualenv
+    virtualenv --no-site-packages --verbose virtualenv
 fi
 source virtualenv/bin/activate
 python scripts/peep.py install -r requirements/requirements_src.txt
 python scripts/peep.py install -r requirements/requirements_packages.txt
 
 # Make sure all files in virtualenv can be rsynced.
-virtualenv --relocatable virtualenv
+virtualenv --system-site-packages --relocatable virtualenv
