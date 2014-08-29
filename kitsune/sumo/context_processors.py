20efa51a7c206c7f295342fd45188c0ec1248ea5
Mike Cooper <mythmon@gmail.com>
2014-08-29 15:51:32 -0700

    Only show to en-US.

--- kitsune/sumo/context_processors.py
+++ kitsune/sumo/context_processors.py
@@ -17,5 +17,6 @@ def geoip_cache_detector(request):
     cookies = getattr(request, 'COOKIES', {})
     has_geoip = ('geoip_country_name' in cookies and
                  'geoip_country_code' in cookies)
+    is_en_us = getattr(request, 'LANGUAGE_CODE', 'en-US') == 'en-US'
 
-    return {'include_geoip': not has_geoip}
+    return {'include_geoip': is_en_us and not has_geoip}
