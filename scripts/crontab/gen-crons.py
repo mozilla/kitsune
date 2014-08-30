606028998eac31031d8e9f250b1f268bc66777b3
Mike Cooper <mythmon@gmail.com>
2014-08-29 17:19:00 -0700

    Change default python in gen-cron.py.

--- scripts/crontab/gen-crons.py
+++ scripts/crontab/gen-crons.py
@@ -15,7 +15,7 @@ def main():
     parser.add_option("-u", "--user",
                       help=("Prefix cron with this user. "
                            "Only define for cron.d style crontabs"))
-    parser.add_option("-p", "--python", default="/usr/bin/python2.6",
+    parser.add_option("-p", "--python", default="python",
                       help="Python interpreter to use")
 
     (opts, args) = parser.parse_args()
@@ -27,9 +27,9 @@ def main():
     python_path = 'PYTHONPATH+=:/usr/local/lib64/python2.6/site-packages'
 
     ctx = {
-        'django': 'cd %s; source virtualenv/bin/activate; %s python -W ignore::DeprecationWarning manage.py' % (
+        'django': 'cd %s; source virtualenv/bin/activate; %s %s -W ignore::DeprecationWarning manage.py' % (
             opts.kitsune, python_path, opts.python),
-        'scripts': 'cd %s; source virtualenv/bin/activate; %s python' % (
+        'scripts': 'cd %s; source virtualenv/bin/activate; %s %s' % (
             opts.kitsune, python_path, opts.python),
     }
     ctx['cron'] = '%s cron' % ctx['django']
