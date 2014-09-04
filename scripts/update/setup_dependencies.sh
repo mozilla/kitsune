4ade857f5c190d00221bae78e563dc4c1c8a3aa7
dean <deanjohnson222@gmail.com>
2014-09-04 16:02:38 -0700

    [bug 1035319] Abort on shell script failures.

--- scripts/update/setup_dependencies.sh
+++ scripts/update/setup_dependencies.sh
@@ -3,6 +3,8 @@
 # This creates a virtualenv and installs our requirements into it.
 # CWD should be the project folder, i.e. kitsune/.
 
+set -e -u
+
 virtualenv --system-site-packages virtualenv
 
 source virtualenv/bin/activate
