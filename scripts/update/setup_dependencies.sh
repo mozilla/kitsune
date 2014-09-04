dccbb3ad68c4368cd8097da1266804d19104638a
dean <deanjohnson222@gmail.com>
2014-09-04 11:22:24 -0700

    [bug 1035319] Fixed virtualenv deployment script.

--- scripts/update/setup_dependencies.sh
+++ scripts/update/setup_dependencies.sh
@@ -3,11 +3,11 @@
 # This creates a virtualenv and installs our requirements into it.
 # CWD should be the project folder, i.e. kitsune/.
 
-virtualenv --no-site-packages --verbose virtualenv
+virtualenv --system-site-packages virtualenv
 
 source virtualenv/bin/activate
 python scripts/peep.py install -r requirements/requirements_src.txt
 python scripts/peep.py install -r requirements/requirements_packages.txt
 
-# Make sure all files in virtualenv can be rsynced.
-virtualenv --system-site-packages --relocatable virtualenv
+# Fix path issues for when we rsync..
+virtualenv --relocatable virtualenv
