58265fe67de6c1afd782f90f0d6480fbb5e0674f
Mike Cooper <mythmon@gmail.com>
2014-09-17 12:55:18 -0700

    Remove code that shouldn't have been included in the first place.

--- kitsune/questions/urls_api.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-
-from kitsune.questions.api import QuestionViewSet
-
-router = routers.SimpleRouter()
-router.register(r'question', QuestionViewSet)
-urlpatterns = router.urls
