38b45a4489e605d6346574edd02361d5fc0b6ced
Ricky Rosario <rickyrosario@gmail.com>
2014-09-23 16:23:09 -0400

    [bug 1058649] Use mysql instead of ES for contribution numbers.
    * We think mysql will be faster than ES for this.

--- kitsune/questions/tests/test__utils.py
+++ kitsune/questions/tests/test__utils.py
@@ -2,11 +2,11 @@ from nose.tools import eq_
 
 from kitsune.questions.tests import question, answer
 from kitsune.questions.utils import num_questions, num_answers, num_solutions
-from kitsune.search.tests.test_es import ElasticTestCase
+from kitsune.sumo.tests import TestCase
 from kitsune.users.tests import user
 
 
-class ContributionCountTestCase(ElasticTestCase):
+class ContributionCountTestCase(TestCase):
     def test_num_questions(self):
         """Answers are counted correctly on a user."""
         u = user(save=True)
@@ -27,23 +27,18 @@ class ContributionCountTestCase(ElasticTestCase):
     def test_num_answers(self):
         u = user(save=True)
         q = question(save=True)
-        self.refresh()
         eq_(num_answers(u), 0)
 
         a1 = answer(creator=u, question=q, save=True)
-        self.refresh()
         eq_(num_answers(u), 1)
 
         a2 = answer(creator=u, question=q, save=True)
-        self.refresh()
         eq_(num_answers(u), 2)
 
         a1.delete()
-        self.refresh()
         eq_(num_answers(u), 1)
 
         a2.delete()
-        self.refresh()
         eq_(num_answers(u), 0)
 
     def test_num_solutions(self):
@@ -52,24 +47,19 @@ class ContributionCountTestCase(ElasticTestCase):
         q2 = question(save=True)
         a1 = answer(creator=u, question=q1, save=True)
         a2 = answer(creator=u, question=q2, save=True)
-        self.refresh()
         eq_(num_solutions(u), 0)
 
         q1.solution = a1
         q1.save()
-        self.refresh()
         eq_(num_solutions(u), 1)
 
         q2.solution = a2
         q2.save()
-        self.refresh()
         eq_(num_solutions(u), 2)
 
         q1.solution = None
         q1.save()
-        self.refresh()
         eq_(num_solutions(u), 1)
 
         a2.delete()
-        self.refresh()
         eq_(num_solutions(u), 0)
--- kitsune/questions/utils.py
+++ kitsune/questions/utils.py
@@ -16,40 +16,10 @@ def num_questions(user):
 
 
 def num_answers(user):
-    """Returns the number of answers a user has.
-
-    Tries to get the data from ES first.
-    """
-    try:
-        count = (AnswerMetricsMappingType
-            .search()
-            .filter(creator_id=user.id).count())
-
-        statsd.incr('questions.utils.num_answers.es')
-        return count
-
-    except ES_EXCEPTIONS as exc:
-        log.exception('num_answers in ES failed.')
-
-    statsd.incr('questions.utils.num_answers.db')
+    """Returns the number of answers a user has."""
     return Answer.objects.filter(creator=user).count()
 
 
 def num_solutions(user):
-    """Returns the number of solutions a user has.
-
-    Tries to get the data from ES first.
-    """
-    try:
-        count = (AnswerMetricsMappingType
-            .search()
-            .filter(creator_id=user.id, is_solution=True).count())
-
-        statsd.incr('questions.utils.num_solutions.es')
-        return count
-
-    except ES_EXCEPTIONS as exc:
-        log.exception('num_solutions in ES failed.')
-
-    statsd.incr('questions.utils.num_solutions.db')
+    """Returns the number of solutions a user has."""
     return Question.objects.filter(solution__creator=user).count()
