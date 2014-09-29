a5dbbd53da8efd87257b9db4d499de4b78848f41
Ricky Rosario <rickyrosario@gmail.com>
2014-09-29 11:38:20 -0400

    Firefox 31 is ESR.

--- kitsune/sumo/static/js/ui.js
+++ kitsune/sumo/static/js/ui.js
@@ -247,7 +247,7 @@
       closed = localStorage.getItem('announcement-outdated.closed') === 'true';
     }
 
-    show = (b == 'fx') && (v <= 23 || (v >= 25 && v <= 31)) && (!closed);
+    show = (b == 'fx') && (v <= 23 || (v >= 25 && v <= 30)) && (!closed);
     if (show) {
       $('#announce-outdated').show();
     } else {
