9e0a2894ee70da2d1c5d38130c9ba2986cd15b72
Mike Cooper <mythmon@gmail.com>
2014-10-08 14:57:26 -0700

    [Bug 1074943] User create, detail, and create APIs

--- kitsune/users/api.py
+++ kitsune/users/api.py
@@ -7,14 +7,14 @@ from django.views.decorators.http import require_GET
 import waffle
 from statsd import statsd
 
+from rest_framework import viewsets, serializers, mixins, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes, api_view
 
-
 from kitsune.access.decorators import login_required
 from kitsune.sumo.decorators import json_view
-from kitsune.users.models import Profile
+from kitsune.users.models import Profile, RegistrationProfile
 
 
 def display_name_or_none(user):
@@ -63,3 +63,87 @@ def test_auth(request):
         'username': request.user.username,
         'authorized': True,
     })
+
+
+class ProfileShortSerializer(serializers.ModelSerializer):
+    username = serializers.WritableField(source='user.username')
+    display_name = serializers.WritableField(source='name', required=False)
+    date_joined = serializers.Field(source='user.date_joined')
+    email = serializers.WritableField(
+        source='user.email', write_only=True, required=False)
+    # This is a write only field. It is very very important it stays that way!
+    password = serializers.WritableField(
+        source='user.password', write_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'username',
+            'display_name',
+            'date_joined',
+            'email',
+            # Password is here so it can be involved in write operations.
+            # It is marked as write-only above, so it will not be visible.
+            'password',
+        ]
+
+    def restore_object(self, attrs, instance=None):
+        """
+        Override the default behavior to make a user if one doesn't exist.
+
+        This user may not be saved here, but will be saved if/when the .save()
+        method of the serializer is called.
+        """
+        instance = (super(ProfileShortSerializer, self)
+                    .restore_object(attrs, instance))
+        if instance.user_id is None:
+            # The Profile doesn't have a user, so create one. If an email is
+            # specified, the user will be inactive until the email is
+            # confirmed. Otherwise the user can be created immediately.
+            if 'user.email' in attrs:
+                u = RegistrationProfile.objects.create_inactive_user(
+                    attrs['user.username'],
+                    attrs['user.password'],
+                    attrs['user.email'])
+            else:
+                u = User(username=attrs['user.username'])
+                u.set_password(attrs['user.password'])
+            instance._nested_forward_relations['user'] = u
+        return instance
+
+    def validate_display_name(self, attrs, source):
+        if attrs.get('name') is None:
+            attrs['name'] = attrs['user.username']
+        return attrs
+
+    def validate_email(self, attrs, source):
+        email = attrs.get('user.email')
+        if email and User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('A user with that email address '
+                                              'already exists.')
+        return attrs
+
+
+class ProfileViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
+    model = Profile
+    serializer_class = ProfileShortSerializer
+    paginate_by = 20
+    # User usernames instead of ids in urls.
+    lookup_field = 'user__username'
+    filter_backends = [
+        filters.DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    filter_fields = [
+        'date_joined',
+    ]
+    ordering_fields = [
+        'username',
+        'display_name',
+        'date_joined',
+    ]
+    # Default, if not overwritten
+    ordering = ('-date_joined',)
--- kitsune/users/tests/test_api.py
+++ kitsune/users/tests/test_api.py
@@ -1,10 +1,15 @@
 import json
 
-from nose.tools import eq_
+import mock
+from django.contrib.auth.models import User
+from django.core import mail
+from nose.tools import eq_, ok_
 
 from kitsune.sumo.helpers import urlparams
 from kitsune.sumo.tests import TestCase
 from kitsune.sumo.urlresolvers import reverse
+from kitsune.users import api
+from kitsune.users.models import Profile
 from kitsune.users.tests import user
 
 
@@ -44,3 +49,65 @@ class UsernamesTests(TestCase):
         self.client.logout()
         res = self.client.get(self.url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         eq_(403, res.status_code)
+
+
+class TestQuestionSerializer(TestCase):
+
+    def setUp(self):
+        self.request = mock.Mock()
+        self.data = {
+            'username': 'bob',
+            'display_name': 'Bobert the Seventh',
+            'password': 'testpass',
+            'email': 'bob@example.com',
+        }
+
+    def test_user_created(self):
+        # There is at least one user in existence due to migrations
+        number_users = User.objects.count()
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        serializer.save()
+        eq_(User.objects.count(), number_users + 1)
+        eq_(Profile.objects.count(), 1)
+
+    def test_password(self):
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        serializer.save()
+        assert serializer.object.user.password != 'testpass'
+        ok_(serializer.object.user.check_password('testpass'))
+
+    def test_automatic_display_name(self):
+        del self.data['display_name']
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        eq_(serializer.object.name, 'bob')
+
+    def test_no_duplicate_emails(self):
+        user(email=self.data['email'], save=True)
+        serializer = api.ProfileShortSerializer(data=self.data)
+        eq_(serializer.errors, {
+            'email': ['A user with that email address already exists.'],
+        })
+        ok_(not serializer.is_valid())
+
+    def test_users_without_emails_are_active(self):
+        del self.data['email']
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        serializer.save()
+        eq_(serializer.object.user.is_active, True)
+
+    def test_users_with_emails_are_inactive(self):
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        serializer.save()
+        eq_(serializer.object.user.is_active, False)
+
+    def test_users_with_emails_get_confirmation_email(self):
+        serializer = api.ProfileShortSerializer(data=self.data)
+        ok_(serializer.is_valid())
+        serializer.save()
+        eq_(len(mail.outbox), 1)
+        eq_(mail.outbox[0].subject, 'Please confirm your email address')
