f750b3b5ef501bf19e3947ed4e371692c074a7c2
Ricky Rosario <rickyrosario@gmail.com>
2014-10-15 16:28:11 -0400

    [bug 1081652] Only update session timezone if editing own profile.

--- kitsune/users/views.py
+++ kitsune/users/views.py
@@ -475,7 +475,8 @@ def edit_profile(request, username=None, template=None):
         if form.is_valid():
             user_profile = form.save()
             new_timezone = user_profile.timezone
-            if request.session.get('timezone', None) != new_timezone:
+            tz_changed = request.session.get('timezone', None) != new_timezone
+            if tz_changed and user == request.user:
                 request.session['timezone'] = new_timezone
             return HttpResponseRedirect(reverse('users.profile',
                                                 args=[user.username]))
