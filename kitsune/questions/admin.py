ecc7915042f3f4757b877f81972f21c0b19ea2a0
Rehan Dalal <rehandalal@gmail.com>
2014-10-23 11:42:16 -0400

    Migration to populate QuestionLocale products

--- kitsune/questions/admin.py
+++ kitsune/questions/admin.py
@@ -6,5 +6,6 @@ from kitsune.questions.models import QuestionLocale
 class QuestionLocaleAdmin(admin.ModelAdmin):
     list_display = ('locale',)
     ordering = ('locale',)
+    filter_horizontal = ('products',)
 
 admin.site.register(QuestionLocale, QuestionLocaleAdmin)
