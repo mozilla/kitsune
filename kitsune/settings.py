1265a4f0677f793d7d03819d68fa3ad8d2dcc706
Ricky Rosario <rickyrosario@gmail.com>
2014-12-16 15:55:59 -0500

    [bug 1113162] Remove django-cache-machine part I.
    Stop using the magical caching manager.

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -559,19 +559,6 @@ def JINJA_CONFIG():
                              'jinja2.ext.do'],
               'finalize': lambda x: x if x is not None else ''}
 
-    if not settings.DEBUG:
-        import jinja2
-        from caching.base import cache
-        if hasattr(cache, 'scheme') and 'memcached' in cache.scheme:
-            # We're passing the _cache object directly to jinja because
-            # Django can't store binary directly; it enforces unicode on it.
-            # Details:
-            #     http://jinja.pocoo.org/2/documentation/api#bytecode-cache
-            # and in the errors you get when you try it the other way.
-            bc = jinja2.MemcachedBytecodeCache(cache._cache,
-                                               "%sj2:" % settings.CACHE_PREFIX)
-            config['cache_size'] = -1  # Never clear the cache
-            config['bytecode_cache'] = bc
     return config
 
 # Let Tower know about our additional keywords.
