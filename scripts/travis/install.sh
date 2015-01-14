18462fe4e503131a46a093aa833beeb168341d32
Mike Cooper <mythmon@gmail.com>
2015-01-14 10:20:53 -0800

    Run tests on Python 2.7 with allowed failures.

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -9,17 +9,32 @@ ln -s /usr/lib/`uname -i`-linux-gnu/libz.so ~/virtualenv/python2.6/lib/
 
 echo "Install Python dependencies"
 python scripts/peep.py install \
-    -r requirements/py26.txt \
-    -r requirements/dev.txt \
-    --no-use-wheel
+  -r requirements/dev.txt \
+  --no-use-wheel
+
+# Optimization: None of the rest is needed for lint tests.
+if [[ $TEST_SUITE == "lint" ]]; then
+  exit 0
+fi
+
+if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then
+  MAIN_REQ_FILE="requirements/py26.txt"
+else
+  MAIN_REQ_FILE="requirements/default.txt"
+fi
+python scripts/peep.py install \
+  -r $MAIN_REQ_FILE \
+  --no-use-wheel
 # Print the installed packages for the world to see.
 pip freeze
 echo
+
 
 echo "Installing Node.js dependencies"
 npm install > /dev/null 2> /dev/null
 npm list
 echo
+
 
 echo "Installing ElasticSearch"
 tar xzvf vendor/tarballs/elasticsearch-0.90.10.tar.gz > /dev/null
