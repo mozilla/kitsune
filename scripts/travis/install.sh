9a57a18c78982812cbc3c5dc8314366e15897e2f
Mike Cooper <mythmon@gmail.com>
2015-01-14 15:26:33 -0800

    Tailor test set up to current env.

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -2,16 +2,26 @@
 # pwd is the git repo.
 set -e
 
+# None of this is needed for lint tests.
+if [[ $TEST_SUITE == "lint" ]]; then
+  return 0
+fi
+
 echo "Fix path issues"
 ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so ~/virtualenv/python2.6/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so ~/virtualenv/python2.6/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libz.so ~/virtualenv/python2.6/lib/
 
 echo "Install Python dependencies"
+if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then
+  MAIN_REQ_FILE="requirements/py26.txt"
+else
+  MAIN_REQ_FILE="requirements/py26.txt"
+fi
 python scripts/peep.py install \
-    -r requirements/py26.txt \
-    -r requirements/dev.txt \
-    --no-use-wheel
+  -r $MAIN_REQ_FILE \
+  -r requirements/dev.txt \
+  --no-use-wheel
 # Print the installed packages for the world to see.
 pip freeze
 echo
