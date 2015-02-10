d37c030622a586697e1f74c474226f54dd71d447
Ricky Rosario <rickyrosario@gmail.com>
2015-02-10 16:43:10 -0500

    [bug 1085456] Support Forum metrics email report.
    This sends a daily email to users in the Support Forum Metrics group.

--- kitsune/questions/cron.py
+++ kitsune/questions/cron.py
@@ -1,15 +1,18 @@
 import logging
+import textwrap
 import time
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 from django.conf import settings
+from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 from django.db import connection, transaction
 
 import cronjobs
 
 from kitsune.questions import config
 from kitsune.questions.models import (
-    Question, QuestionVote, QuestionMappingType, QuestionVisits)
+    Question, QuestionVote, QuestionMappingType, QuestionVisits, Answer)
 from kitsune.questions.tasks import (
     escalate_question, update_question_vote_chunk)
 from kitsune.search.es_utils import ES_EXCEPTIONS, get_documents
@@ -151,3 +154,57 @@ def escalate_questions():
         escalate_question.delay(question.id)
 
     return len(qs_no_replies_yet)
+
+
+@cronjobs.register
+def report_employee_answers():
+    """Send an email about employee answered questions.
+
+    We report on the users in the "Support Forum Tracked" group.
+    We send the email to the users in the "Support Forum Metrics" group.
+    """
+    tracked_group = Group.objects.get(name='Support Forum Tracked')
+    report_group = Group.objects.get(name='Support Forum Metrics')
+
+    tracked_users = tracked_group.user_set.all()
+    report_recipients = report_group.user_set.all()
+
+    if len(tracked_users) == 0 or len(report_recipients) == 0:
+        return
+
+    yesterday = date.today() - timedelta(days=1)
+    day_before_yesterday = yesterday - timedelta(days=1)
+
+    # Total number of questions asked the day before yesterday
+    questions = Question.objects.filter(
+        creator__is_active=True,
+        created__gte=day_before_yesterday,
+        created__lt=yesterday)
+    num_questions = questions.count()
+
+    # Total number of answered questions day before yesterday
+    num_answered = questions.filter(num_answers__gt=0).count()
+
+    # Total number of questions answered by user in tracked_group
+    num_answered_by_tracked = Answer.objects.filter(
+        question__in=questions,
+        creator__in=tracked_users).values_list('question_id').distinct().count()
+
+    email_subject = 'Support Forum answered report for {date}'.format(date=day_before_yesterday)
+
+    email_body_tmpl = textwrap.dedent("""\
+        Date: {date}
+        Number of questions asked: {num_questions}
+        Number of questions answered: {num_answered}
+        Number of questions with answer from 'Support Forum Tracked' users: {num_by_tracked}
+        """)
+    email_body = email_body_tmpl.format(
+        date=day_before_yesterday,
+        num_questions=num_questions,
+        num_answered=num_answered,
+        num_by_tracked=num_answered_by_tracked)
+
+    email_addresses = [u.email for u in report_recipients]
+
+    send_mail(email_subject, email_body, settings.TIDINGS_FROM_ADDRESS, email_addresses,
+              fail_silently=False)
