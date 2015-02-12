0dcd55cb47180b71530bc6adae5b872a99a8ce5c
Mike Cooper <mythmon@gmail.com>
2015-02-12 13:12:46 -0800

    [Bug 1132544] Don't use bulk create for notifications.
    Is there a way to make bulk creating these an error?

--- kitsune/notifications/tasks.py
+++ kitsune/notifications/tasks.py
@@ -40,8 +40,9 @@ def add_notification_for_action(action_id):
     # assocated with those Follow objects, and fire off a  notification to
     # every one of them. Use a set to only notify each user once.
     users_to_notify = set(f.user for f in Follow.objects.filter(query))
-    notifications = [Notification(owner=u, action=action) for u in users_to_notify]
-    Notification.objects.bulk_create(notifications)
+    # Don't use bulk save since that wouldn't trigger signal handlers
+    for u in users_to_notify:
+        Notification.objects.create(owner=u, action=action)
 
 
 @task(ignore_result=True)
