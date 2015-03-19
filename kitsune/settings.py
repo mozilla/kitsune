226331e36c6a5052b46b54130ebbb6d3062640fe
Ricky Rosario <rickyrosario@gmail.com>
2015-03-19 10:07:40 -0400

    [bug 1121588] Update django 1.7.7

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -504,10 +504,8 @@ TEMPLATE_DIRS = (
 # TODO: Figure out why changing the order of apps (for example, moving
 # taggit higher in the list) breaks tests.
 INSTALLED_APPS = (
-    # south needs to come early so tests don't fail
-    'south',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.auth',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
