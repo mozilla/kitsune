40f1e743649c256e2ac604d86f8d65a4f3d256da
Ed Morley <emorley@mozilla.com>
2015-04-17 11:26:12 +0100

    [Bug 1155599] Remove blacklisting of pip 6.1 from peep.sh
    Now that we've updated peep, it is no longer incompatible with pip 6.1*,
    so we don't need to error out.

--- peep.sh
+++ peep.sh
@@ -14,17 +14,10 @@ echo "peep.sh: Using pip $PIPVER"
 # Note: If we encounter other versions of pip here that require other
 # things, we can toss them in.
 case $PIPVER in
-    1.5*|1.6*|6.0*)
+    1.5*|1.6*|6.*)
         echo "peep.sh: Wheel-using pip detected, so passing --no-use-wheel."
         ARGS="--no-use-wheel"
         ;;
-    6.1*)
-        echo
-        echo "Error: peep does not support pip >= 6.1."
-        echo "Please downgrade your version of pip by running:"
-        echo ""
-        echo "    pip install --upgrade 'pip<6.1'"
-        exit 1
 esac
 
 # Execute peep with the command line args plus the additional args
