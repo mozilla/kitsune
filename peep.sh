88bc01ec314b97c7e3a6e8b249dd700773d7117b
Ed Morley <emorley@mozilla.com>
2015-04-17 11:31:22 +0100

    [Bug 1155599] Clarify explanation for --no-use-wheel in peep.sh
    I've also removed references to pip v1.6 (since it does not exist) and
    cleaned up the comments that wrapped unnecessarily.

--- peep.sh
+++ peep.sh
@@ -8,20 +8,19 @@ ARGS=""
 
 echo "peep.sh: Using pip $PIPVER"
 
-# If we're using pip 1.5, 1.6 or 6.0, then toss on the --no-use-wheel
-# argument.
-#
-# Note: If we encounter other versions of pip here that require other
-# things, we can toss them in.
+# Add pip arguments that vary according to the version of pip, here:
 case $PIPVER in
-    1.5*|1.6*|6.*)
+    1.5*|6.*)
+        # Pip uses the wheel format packages by default in pip 1.5+.
+        # However in pip < 6.0+ wheel support is broken, and even with pip 6.0+
+        # we intentionally don't use the wheel packages, since otherwise each
+        # package in the requirements files would need multiple hashes.
         echo "peep.sh: Wheel-using pip detected, so passing --no-use-wheel."
         ARGS="--no-use-wheel"
         ;;
 esac
 
-# Execute peep with the command line args plus the additional args
-# if any.
+# Add the version specific arguments to those passed on the command line.
 python ./scripts/peep.py "$@" $ARGS
 
 echo "peep.sh: Done!"
