07189f976b250bfef5ff6c057986847b63eb24e3
Rehan Dalal <rehandalal@gmail.com>
2015-04-20 17:12:33 -0400

    fix tests

--- kitsune/products/tests/test_templates.py
+++ kitsune/products/tests/test_templates.py
@@ -93,10 +93,9 @@ class ProductViewsTestCase(ElasticTestCase):
             doc.products.add(p)
             docs.append(doc)
 
-        # Add a helpful vote to the second document. It should be first now.
-        rev = docs[1].current_revision
-        helpful_vote(revision=rev, helpful=True, save=True)
-        docs[1].save()  # Votes don't trigger a reindex.
+        # Add a lower display order to the second document. It should be first now.
+        docs[1].display_order = 0
+        docs[1].save()
         self.refresh()
         url = reverse('products.documents', args=[p.slug, t.slug])
         r = self.client.get(url, follow=True)
@@ -105,18 +104,30 @@ class ProductViewsTestCase(ElasticTestCase):
         eq_(doc('#document-list > ul > li:first-child > a').text(),
             docs[1].title)
 
-        # Add 2 helpful votes the third document. It should be first now.
+        # Add a helpful vote to the third document. It should be second now.
         rev = docs[2].current_revision
         helpful_vote(revision=rev, helpful=True, save=True)
-        helpful_vote(revision=rev, helpful=True, save=True)
         docs[2].save()  # Votes don't trigger a reindex.
+        self.refresh()
+        url = reverse('products.documents', args=[p.slug, t.slug])
+        r = self.client.get(url, follow=True)
+        eq_(200, r.status_code)
+        doc = pq(r.content)
+        eq_(doc('#document-list > ul > li:nth-child(2) > a').text(),
+            docs[2].title)
+
+        # Add 2 helpful votes the first document. It should be second now.
+        rev = docs[0].current_revision
+        helpful_vote(revision=rev, helpful=True, save=True)
+        helpful_vote(revision=rev, helpful=True, save=True)
+        docs[0].save()  # Votes don't trigger a reindex.
         self.refresh()
         cache.clear()  # documents_for() is cached
         r = self.client.get(url, follow=True)
         eq_(200, r.status_code)
         doc = pq(r.content)
-        eq_(doc('#document-list > ul > li:first-child > a').text(),
-            docs[2].title)
+        eq_(doc('#document-list > ul > li:nth-child(2) > a').text(),
+            docs[0].title)
 
     def test_subtopics(self):
         """Verifies subtopics appear on document listing page."""
