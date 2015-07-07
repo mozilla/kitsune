819b44da65e6cbaa53700e49bfeddb74c3480293
Mike Cooper <mythmon@gmail.com>
2015-07-07 13:09:11 -0700

    Add initilization code for SurveyGizmo Beacon.

--- kitsune/sumo/static/sumo/js/surveygizmo.js
+++ kitsune/sumo/static/sumo/js/surveygizmo.js
@@ -4,14 +4,20 @@
  * This code was derived from the minified version of code posted in bug 1175880
  */
 (function() {
+    // set up temp SG reciever, until script loads
     window.SurveyGizmoBeacon = 'sg_beacon';
     window.sg_beacon = window.sg_beacon || function() {
         window.sg_beacon.q = window.sg_beacon.q || [];
         window.sg_beacon.q.push(arguments);
     };
+
+    // load SG beacon script
     var beaconScript = document.createElement('script');
     beaconScript.async = 1;
     beaconScript.src = '//d2bnxibecyz4h5.cloudfront.net/runtimejs/intercept/intercept.js';
     var lastScriptTag = document.getElementsByTagName('script')[0];
     lastScriptTag.parentNode.insertBefore(beaconScript, lastScriptTag);
+
+    // Initialize
+    window.sg_beacon('init', 'MjgwNDktQUYyRDQ3ODk0MjY1NEVFNUIwNTI3MjhFMDk2QTE3RDU=');
 })();
