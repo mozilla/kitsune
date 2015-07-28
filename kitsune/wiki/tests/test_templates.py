dfb691f5757d4e2ef49c670b52e386e8b5110113
Tim P <pollti@gmx.net>
2015-07-28 13:48:23 +0200

    divided test_document_fallback_no_translation into two tests

--- kitsune/wiki/tests/test_templates.py
+++ kitsune/wiki/tests/test_templates.py
@@ -192,8 +192,8 @@ class DocumentTests(TestCaseBase):
 
     def test_document_fallback_no_translation(self):
         """The document template falls back to English if no translation
-        exists. The fallback message is only shown when there is any revision
-        ready for localization"""
+        exists. The fallback message is not shown if there is no revision
+        ready for localization."""
         r = revision(save=True, content='Some text.', is_approved=True)
         url = reverse('wiki.document', args=[r.document.slug], locale='fr')
         response = self.client.get(url)
@@ -206,22 +206,25 @@ class DocumentTests(TestCaseBase):
         # Included content is English.
         eq_(pq(r.document.html)('div').text(), doc('#doc-content div').text())
 
-        # Marking the document and the revision ready for localization
-        r2 = revision(save=True, document=r.document, content='Some text.',
-                      is_approved=True, is_ready_for_localization=True)
-
+    def test_document_fallback_no_translation_ready_for_l10n(self):
+        """The document template falls back to English if no translation
+        exists. The fallback message is shown if there is any revision
+        ready for localization."""
+        # Creating a revision ready for localization
+        r = revision(save=True, content='Some text.', is_approved=True,
+                     is_ready_for_localization=True)
         url = reverse('wiki.document', args=[r.document.slug], locale='de')
         response = self.client.get(url)
         doc = pq(response.content)
 
-        eq_(r2.document.title, doc('article h1.title').text())
+        eq_(r.document.title, doc('article h1.title').text())
         # Fallback message is shown.
         eq_(1, len(doc('#doc-pending-fallback')))
         # Removing this as it shows up in text(), and we don't want to depend
         # on its localization.
         doc('#doc-pending-fallback').remove()
         # Included content is English.
-        eq_(pq(r2.document.html)('div').text(), doc('#doc-content div').text())
+        eq_(pq(r.document.html)('div').text(), doc('#doc-content div').text())
 
     def test_redirect(self):
         """Make sure documents with REDIRECT directives redirect properly.
