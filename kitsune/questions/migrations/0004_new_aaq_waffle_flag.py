aa18fecfb54ed2bb784bfdd882220e5f116fd56f
Rehan Dalal <rehandalal@gmail.com>
2015-08-05 18:08:14 -0400

    [bug 1183422] Add waffle switch for new AAQ

---
+++ kitsune/questions/migrations/0004_new_aaq_waffle_flag.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def create_waffle_flag(apps, schema_editor):
+    Flag = apps.get_model('waffle', 'Flag')
+    f = Flag(name='new_aaq', everyone=False, superusers=False, staff=False,
+             authenticated=False, rollout=False, testing=False)
+    f.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('waffle', '0001_initial'),
+        ('questions', '0003_auto_20150430_1304'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_waffle_flag),
+    ]
