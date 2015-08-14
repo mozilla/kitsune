2fbf949ac8233cf1e6831e4d9cad1c2706845b55
Mike Cooper <mythmon@gmail.com>
2015-08-13 17:41:25 -0700

    [Bug 1183428] Add about:support troubleshooting data to new AAQ.

--- kitsune/questions/static/questions/js/constants/AAQConstants.es6.js
+++ kitsune/questions/static/questions/js/constants/AAQConstants.es6.js
@@ -9,6 +9,9 @@ export const actionTypes = constantMap([
   'QUESTION_SUBMIT_OPTIMISTIC',
   'QUESTION_SUBMIT_SUCCESS',
   'QUESTION_SUBMIT_FAILURE',
+  'TROUBLESHOOTING_OPT_IN',
+  'TROUBLESHOOTING_OPT_OUT',
+  'TROUBLESHOOTING_SET_DATA',
 ]);
 
 export const questionEditState = constantMap([
