6f6a123612da04084cca700a97d6aea4a0e7b7d7
Adrian Gaudebert <adrian@gaudebert.fr>
2015-09-02 18:36:58 +0200

    Fixed unit tests.

--- kitsune/coolsearch/urls.py
+++ kitsune/coolsearch/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'kitsune.coolsearch.views',
-    url(r'^$', 'search', name='search'),
+    url(r'^$', 'search', name='coolsearch.search'),
 )
