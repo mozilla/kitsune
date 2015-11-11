1e791f242df84b49d372ea3216b70a608134ef95
Mike Cooper <mythmon@gmail.com>
2015-11-11 08:32:07 -0800

    Revert "Merge pull request #2705 from rehandalal/upgrade-nunjucks"
    This reverts commit 68daba37eae4eceaffcb1ca978c783955b42d2eb, reversing
    changes made to 8557f6a115388b7fa1b357147d1954331194d162.

--- kitsune/sumo/static/sumo/js/nunjucks.js
+++ kitsune/sumo/static/sumo/js/nunjucks.js
@@ -7,7 +7,10 @@
   env.addGlobal('_', gettext);
   env.addGlobal('ngettext', window.ngettext);
 
-  env.addFilter('f', function(fmt, obj, named) {
+  // TODO: Get rid of these and replace filters with functions in templates
+  env.addFilter('gettext', gettext);
+  env.addFilter('ngettext', ngettext);
+  env.addFilter('interpolate', function(fmt, obj, named) {
     var keys = Object.keys(obj);
     var escape = env.getFilter('escape');
 
