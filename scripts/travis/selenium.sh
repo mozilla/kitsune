a94e55866eb39d18abefe53dc732d4b35efb876a
Safwan Rahman <safwan.rahman15@gmail.com>
2015-11-17 02:13:22 +0600

    update screen size

--- scripts/travis/selenium.sh
+++ scripts/travis/selenium.sh
@@ -13,5 +13,5 @@ sleep 3
 echo 'Running Smoke tests'
 source selenium/bin/activate
 cd smoketests
-xvfb-run --server-args="-screen 0, 1024x768x24" py.test --driver=firefox --baseurl=http://localhost:8000 tests
+xvfb-run --server-args="-screen 0, 1280x1024x16" py.test --driver=firefox --baseurl=http://localhost:8000 tests
 echo 'Booyahkasha!'
