fa28dacb607b74847821c02bfba0928ac63c5dbf
Giorgos Logiotatidis <seadog@sealabs.net>
2018-02-14 14:27:05 +0200

    Make top_contributors functions db only.
    Since the ESv2 upgrade the top_contributors_* functions were using a hack to
    workaround deprecated facets. The hack required custom ES configuration. This
    patch completes the move to db-only for those queries.
    
    We 're protected from overloading the db from the already existing cache
    mechanism. We 're also avoided loading results into python lists which improves
    execution speed and reduces memory requirements.
    
    The top_contributors_aoa function has been set to return empty set since we're
    in the process to remove AoA.

--- kitsune/community/utils.py
+++ kitsune/community/utils.py
@@ -3,27 +3,18 @@ import hashlib
 from datetime import datetime, date, timedelta
 from django.conf import settings
 from django.core.cache import cache
-from django.db.models import Count
+from django.db.models import Count, F
 
-from kitsune.customercare.models import ReplyMetricsMappingType
 from kitsune.products.models import Product
-from kitsune.questions.models import AnswerMetricsMappingType
-from kitsune.search.es_utils import F
+from kitsune.questions.models import Answer
 from kitsune.users.models import User
 from kitsune.users.templatetags.jinja_helpers import profile_avatar
-from kitsune.wiki.models import RevisionMetricsMappingType
-
-
-# This should be higher than the max number of questions, revisions or tweets.
-# There isn't a way to tell ES to just return everything.
-HUGE_NUMBER = 1500000
+from kitsune.wiki.models import Revision
 
 
 def top_contributors_questions(start=None, end=None, locale=None, product=None,
                                count=10, page=1, use_cache=True):
     """Get the top Support Forum contributors."""
-    # Get the user ids and contribution count of the top contributors.
-
     if use_cache:
         cache_key = u'{}_{}_{}_{}_{}_{}'.format(start, end, locale, product, count, page)
         cache_key = hashlib.sha1(cache_key.encode('utf-8')).hexdigest()
@@ -32,19 +23,32 @@ def top_contributors_questions(start=None, end=None, locale=None, product=None,
         if cached:
             return cached
 
-    query = AnswerMetricsMappingType.search()
+    answers = (Answer.objects
+               .exclude(is_spam=True)
+               .exclude(question__is_spam=True)
+               # Adding answer to your own question, isn't a contribution.
+               .exclude(creator_id=F('question__creator_id')))
 
-    # Adding answer to your own question, isn't a contribution.
-    query = query.filter(by_asker=False)
-    query = _apply_filters(query, start, end, locale, product)
+    if start is None:
+        # By default we go back 90 days.
+        start = date.today() - timedelta(days=90)
+        answers = answers.filter(created__gte=start)
+    if end:
+        # If no end is specified, we don't need to filter by it.
+        answers = answers.filter(created__lt=end)
+    if locale:
+        answers = answers.filter(question__locale=locale)
+    if product:
+        if isinstance(product, Product):
+            product = product.slug
+        answers = answers.filter(question__product__slug=product)
 
-    answers = [q.id for q in query.all()[:HUGE_NUMBER]]
     users = (User.objects
              .filter(answers__in=answers)
              .annotate(query_count=Count('answers'))
              .order_by('-query_count'))
-
     counts = _get_creator_counts(users, count, page)
+
     if use_cache:
         cache.set(cache_key, counts, 60*15)  # 15 minutes
     return counts
@@ -59,7 +63,6 @@ def top_contributors_kb(start=None, end=None, product=None, count=10, page=1, us
 def top_contributors_l10n(start=None, end=None, locale=None, product=None,
                           count=10, page=1, use_cache=True):
     """Get the top l10n contributors for the KB."""
-
     if use_cache:
         cache_key = u'{}_{}_{}_{}_{}_{}'.format(start, end, locale, product, count, page)
         cache_key = hashlib.sha1(cache_key.encode('utf-8')).hexdigest()
@@ -69,21 +72,31 @@ def top_contributors_l10n(start=None, end=None, locale=None, product=None,
             return cached
 
     # Get the user ids and contribution count of the top contributors.
-    query = RevisionMetricsMappingType.search()
-
+    revisions = Revision.objects.all()
     if locale is None:
         # If there is no locale specified, exclude en-US only. The rest are
         # l10n.
-        query = query.filter(~F(locale=settings.WIKI_DEFAULT_LANGUAGE))
+        revisions = revisions.exclude(document__locale=settings.WIKI_DEFAULT_LANGUAGE)
+    if start is None:
+        # By default we go back 90 days.
+        start = date.today() - timedelta(days=90)
+        revisions = revisions.filter(created__gte=start)
+    if end:
+        # If no end is specified, we don't need to filter by it.
+        revisions = revisions.filter(created__lt=end)
+    if locale:
+        revisions = revisions.filter(document__locale=locale)
+    if product:
+        if isinstance(product, Product):
+            product = product.slug
+        revisions = revisions.filter(document__products__slug=product)
 
-    query = _apply_filters(query, start, end, locale, product)
-    revisions = [q.id for q in query.all()[:HUGE_NUMBER]]
     users = (User.objects
              .filter(created_revisions__in=revisions)
              .annotate(query_count=Count('created_revisions'))
              .order_by('-query_count'))
-
     counts = _get_creator_counts(users, count, page)
+
     if use_cache:
         cache.set(cache_key, counts, 60*15)  # 15 minutes
     return counts
@@ -91,55 +104,8 @@ def top_contributors_l10n(start=None, end=None, locale=None, product=None,
 
 def top_contributors_aoa(start=None, end=None, locale=None, count=10, page=1, use_cache=True):
     """Get the top Army of Awesome contributors."""
-
-    if use_cache:
-        cache_key = u'{}_{}_{}_{}_{}'.format(start, end, locale, count, page)
-        cache_key = hashlib.sha1(cache_key.encode('utf-8')).hexdigest()
-        cache_key = u'top_contributors_aoa_{}'.format(cache_key)
-        cached = cache.get(cache_key, None)
-
-        if cached:
-            return cached
-
-    # Get the user ids and contribution count of the top contributors.
-    query = ReplyMetricsMappingType.search()
-
-    # twitter only does language
-    locale = locale.split('-')[0] if locale else None
-
-    query = _apply_filters(query, start, end, locale)
-    tweets = [q.id for q in query.all()[:HUGE_NUMBER]]
-    users = (User.objects
-             .filter(tweet_replies__in=tweets)
-             .annotate(query_count=Count('tweet_replies'))
-             .order_by('-query_count'))
-
-    counts = _get_creator_counts(users, count, page)
-    if use_cache:
-        cache.set(cache_key, counts, 60*15)  # 15 minutes
-    return counts
-
-
-def _apply_filters(query, start, end, locale=None, product=None):
-    """Apply the date and locale filters to the EU query."""
-    if start is None:
-        # By default we go back 90 days.
-        start = date.today() - timedelta(days=90)
-    query = query.filter(created__gte=start)
-
-    if end:
-        # If no end is specified, we don't need to filter by it.
-        query = query.filter(created__lt=end)
-
-    if locale:
-        query = query.filter(locale=locale)
-
-    if product:
-        if isinstance(product, Product):
-            product = product.slug
-        query = query.filter(product=product)
-
-    return query
+    # AoA is deprecated, return 0 until we remove all related code.
+    return ([], 0)
 
 
 def _get_creator_counts(query, count, page):
