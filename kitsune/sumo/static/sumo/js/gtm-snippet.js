7ae95c422c481f990f56810332337c97f1651d34
Benjamin Sternthal <ben@devpatch.com>
2018-02-15 12:26:34 -0800

    Preserved legacy trackEvent function, added analytics.js back to bundle
    Added ga to env file, replaced all refs to data-ga-click for a and button.
    
    Set gtag and fixed formatting
    
    Added tracking according to spec. Waiting on GA Push legacy implementation.
    
    Removed usage of data-ga-push.
    
    Tidyed up analytics. Made sure both mobile and desktop versions are consistent.
    
    Fixed typo.
    
    Fixed debug typo.
    
    Fixed issues due to bad AP spec ¯\_(ツ)_/¯
    
    Minor tweaks from PR review.
    
    Fixed pep8 failures
    
    Fixed pep8 failures

--- kitsune/sumo/static/sumo/js/gtm-snippet.js
+++ kitsune/sumo/static/sumo/js/gtm-snippet.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- (function(w) {
+(function(w) {
   'use strict';
 
   var GTM_CONTAINER_ID = document.getElementsByTagName('html')[0].getAttribute('data-gtm-container-id');
@@ -15,14 +15,13 @@
   w.gtag = function() {
     w.dataLayer.push(arguments)
   }
-
   // If doNotTrack is not enabled, it is ok to add GTM
   // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1217896 for more details
   if (typeof _dntEnabled === 'function' && !_dntEnabled() && GTM_CONTAINER_ID) {
     (function(w,d,s,l,i,j,f,dl,k,q){
       w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});f=d.getElementsByTagName(s)[0];
       k=i.length;q='//www.googletagmanager.com/gtag/js?id=@&l='+(l||'dataLayer');
-      while(k--){j=d.createElement(s);j.async=!0;j.src=q.replace('@',i[k]);f.parentNode.insertBefore(j,f);}
+      while (k--) {j=d.createElement(s);j.async=!0;j.src=q.replace('@',i[k]);f.parentNode.insertBefore(j,f);}
     }(window,document,'script','dataLayer',[GTM_CONTAINER_ID]));
 
     w.gtag('js', new Date());
@@ -33,10 +32,10 @@
     // product should be first match or null
     product = (matches && matches.length > 0) ? matches[1] : null;
 
-    if (product) {
-      w.gtag('config', GTM_CONTAINER_ID, {
-        'dimension1': product
-      });
-    }
+
+    w.gtag('config', GTM_CONTAINER_ID, {
+      'dimension1': product
+    });
+
   }
 })(window);
