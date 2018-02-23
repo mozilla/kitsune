835cb52503eae81127d3f7adc8dfff8a8d0a89f6
Giorgos Logiotatidis <seadog@sealabs.net>
2018-02-23 12:12:22 +0200

    Set AWS_S3_HOST.

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -806,6 +806,7 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
 AWS_S3_CUSTOM_DOMAIN = config('AWS_S3_CUSTOM_DOMAIN', default='prod-cdn.sumo.mozilla.net')
+AWS_S3_HOST = config('AWS_S3_HOST', default='s3-us-west-2.amazonaws.com')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=2592000',
 }
