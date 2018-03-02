a8b574885f742c00e4249e44487d8f95d8e9c31b
Giorgos Logiotatidis <seadog@sealabs.net>
2018-03-02 16:32:31 +0200

    PEP8 fixes.

--- kitsune/sumo/cron.py
+++ kitsune/sumo/cron.py
@@ -1,10 +1,6 @@
 from datetime import datetime
 
-from django.conf import settings
-from django.core.mail import mail_admins
-
 import cronjobs
-import requests
 
 from kitsune.sumo.tasks import measure_queue_lag
 
