a7c57130a0904e23c65037942e52af2b72880758
Paul McLanahan <pmac@mozilla.com>
2018-07-06 14:59:41 -0400

    Try uWSGI as a server instead of Gunicorn

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -3,6 +3,18 @@
 echo "$GIT_SHA" > static/revision.txt
 
 # https://stackoverflow.com/questions/25737589/gunicorn-doesnt-log-real-ip-from-nginx
-GUNICORN_LOGFORMAT='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({X_Forwarded_For}i)s"'
+# GUNICORN_LOGFORMAT='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({X_Forwarded_For}i)s"'
 
-exec gunicorn wsgi.app --access-logformat "${GUNICORN_LOGFORMAT}" --config wsgi/config.py
+# exec gunicorn wsgi.app --access-logformat "${GUNICORN_LOGFORMAT}" --config wsgi/config.py
+
+exec uwsgi --http "0.0.0.0:${PORT:-8000}" \
+           --processes "${WSGI_NUM_WORKERS:-2}" \
+           --harakiri 20 \
+           --max-requests 5000 \
+           --async 10 \
+           --ugreen \
+           --master \
+           --vacuum \
+           --wsgi-file /app/wsgi/app.py \
+           --pidfile /tmp/kitsune-master.pid \
+           --chdir /app/
