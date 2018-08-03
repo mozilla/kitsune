0691ad38f3a2c1fe1a9e99cb0d35f49d09d4532b
Jon Petto <jonpetto@gmail.com>
2018-08-03 11:20:44 -0500

    [fix bug 1416244] Enable new email collection survey.

--- kitsune/kpi/cron.py
+++ kitsune/kpi/cron.py
@@ -408,19 +408,25 @@ def process_exit_surveys():
 
     _process_exit_survey_results()
 
-    # Get the email addresses from two days ago and add them to the survey
+    # Get the email addresses from 4-5 hours ago and add them to the survey
     # campaign (skip this on stage).
 
-    startdate = date.today() - timedelta(days=2)
-    enddate = date.today() - timedelta(days=1)
+    # The cron associated with this process is set to run every hour,
+    # with the intent of providing a 4-5 hour wait period between when a
+    # visitor enters their email address and is then sent a follow-up
+    # survey.
+    # The range here is set between 4 and 8 hours to be sure no emails are
+    # missed should a particular cron run be skipped (e.g. during a deployment)
+    startdatetime = datetime.now() - timedelta(hours=8)
+    enddatetime = datetime.now() - timedelta(hours=4)
 
     for survey in SURVEYS.keys():
-        if 'email_collection_survey_id' not in SURVEYS[survey]:
+        if not SURVEYS[survey]['active'] or 'email_collection_survey_id' not in SURVEYS[survey]:
             # Some surveys don't have email collection on the site
             # (the askers survey, for example).
             continue
 
-        emails = get_email_addresses(survey, startdate, enddate)
+        emails = get_email_addresses(survey, startdatetime, enddatetime)
         for email in emails:
             add_email_to_campaign(survey, email)
 
--- kitsune/kpi/surveygizmo_utils.py
+++ kitsune/kpi/surveygizmo_utils.py
@@ -13,25 +13,35 @@ SURVEYS = {
         'exit_survey_campaign_id': 878533,
         'community_health': 2466367,
         'community_health_campaign_id': 3369235,
+        'active': False,  # allows cron job to skip this survey
     },
     'questions': {  # This is for users that are browsing questions.
         'email_collection_survey_id': 1717268,
         'exit_survey_id': 1724445,
         'exit_survey_campaign_id': 1687339,
+        'active': False,  # allows cron job to skip this survey
     },
     'askers': {  # This is for users that asked a question 2 days ago.
         'exit_survey_id': 1817790,
         'exit_survey_campaign_id': 1876443,
+        'active': False,  # allows cron job to skip this survey
     },
     'kb-firefox-android': {  # This is for KB users looking at Firefox for Android pages.
         'email_collection_survey_id': 1983780,
         'exit_survey_id': 1979872,
         'exit_survey_campaign_id': 2208951,
+        'active': False,  # allows cron job to skip this survey
+    },
+    'general-q3-2018': {  # General survey for en-US users enabled in Q3 of 2018
+        'email_collection_survey_id': 4494159,
+        'exit_survey_id': 4456859,
+        'exit_survey_campaign_id': 7259518,
+        'active': True,
     },
 }
 
 
-def get_email_addresses(survey, startdate, enddate):
+def get_email_addresses(survey, startdatetime, enddatetime):
     """Get the email addresses collected between startdate and enddate."""
     token = settings.SURVEYGIZMO_API_TOKEN
     secret = settings.SURVEYGIZMO_API_TOKEN_SECRET
@@ -39,6 +49,7 @@ def get_email_addresses(survey, startdate, enddate):
     page = 1
     more_pages = True
     survey_id = SURVEYS[survey]['email_collection_survey_id']
+    dtfmt = '%Y-%m-%d+%H:%M:%S'
 
     # Can't do anything without credentials.
     if token is None or secret is None:
@@ -49,17 +60,17 @@ def get_email_addresses(survey, startdate, enddate):
             'https://restapi.surveygizmo.com/v2/survey/{survey}'
             '/surveyresponse?'
             'filter[field][0]=datesubmitted'
-            '&filter[operator][0]=>=&filter[value][0]={start}+0:0:0'
+            '&filter[operator][0]=>=&filter[value][0]={start}'
             'filter[field][1]=datesubmitted'
-            '&filter[operator][1]=<&filter[value][1]={end}+0:0:0'
+            '&filter[operator][1]=<&filter[value][1]={end}'
             '&filter[field][2]=status&filter[operator][2]=='
             '&filter[value][2]=Complete'
             '&resultsperpage=500'
             '&page={page}'
             '&api_token={token}'
             '&api_token_secret={secret}'.format(
-                survey=survey_id, start=startdate,
-                end=enddate, page=page, token=token, secret=secret),
+                survey=survey_id, start=startdatetime.strftime(dtfmt),
+                end=enddatetime.strftime(dtfmt), page=page, token=token, secret=secret),
             timeout=300)
 
         results = json.loads(response.content)
