1591da8035a5cf53ce6827742c56e8238b66cc7e
Paul McLanahan <pmac@mozilla.com>
2018-10-03 15:10:10 -0400

    Add running of post-deployment tasks to deployment
    Re #3133

--- k8s/commander.sh
+++ k8s/commander.sh
@@ -14,13 +14,14 @@ function deploy {
     REGION_ENV=${3}
     COMMIT_HASH=${4}
     DEPLOY_SECRETS=${5:-NO}
+    K8S_NAMESPACE="sumo-${REGION_ENV}"
 
     export KUBECTL_BIN="./regions/${REGION}/kubectl"
     export KUBECONFIG="./regions/${REGION}/kubeconfig"
 
     if [[ "${DEPLOY_SECRETS}" == "secrets" ]]; then
         echo "Applying secrets";
-        ${KUBECTL_BIN} -n sumo-${REGION_ENV} apply -f "regions/${REGION}/${REGION_ENV}-secrets.yaml"
+        ${KUBECTL_BIN} -n "${K8S_NAMESPACE}" apply -f "regions/${REGION}/${REGION_ENV}-secrets.yaml"
     else
         echo "Secrets will *NOT* be applied";
     fi
@@ -31,6 +32,12 @@ function deploy {
     invoke  -f "regions/${REGION}/${REGION_ENV}.yaml" rollouts.status-cron
     invoke  -f "regions/${REGION}/${REGION_ENV}.yaml" deployments.create-web --apply --tag full-${COMMIT_HASH}
     invoke  -f "regions/${REGION}/${REGION_ENV}.yaml" rollouts.status-web
+
+    # run post-deployment tasks
+    echo "Running post-deployment tasks"
+    SUMO_POD=$(${KUBECTL_BIN} -n "${K8S_NAMESPACE}" get pods -o=jsonpath='{.items[0].metadata.name}')
+    ${KUBECTL_BIN} -n "${K8S_NAMESPACE}" exec "${SUMO_POD}" bin/run-post-deploy.sh
+
     printf "${GREEN}OK${NC}\n"
     if command -v slack-cli > /dev/null; then
         slack-cli -d "${SLACK_CHANNEL}" ":tada: Successfully deployed <${DOCKER_HUB}|full-${COMMIT_HASH}> to <https://${REGION_ENV}-${REGION}.sumo.moz.works/|SUMO-${REGION_ENV} in ${REGION}>"
