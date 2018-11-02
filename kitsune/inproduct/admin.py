ec42b5537492f7cc6d892b5344a8febe2454a4d0
Paul McLanahan <pmac@mozilla.com>
2018-11-02 14:40:47 -0400

    Add text search to inproduct redirects in the admin
    Fix bug 1504289

--- kitsune/inproduct/admin.py
+++ kitsune/inproduct/admin.py
@@ -7,8 +7,8 @@ class RedirectAdmin(admin.ModelAdmin):
     list_display = ('product', 'version', 'platform',
                     'locale', 'topic', 'target')
     list_display_links = ('topic', 'target')
-    list_filter = ('product', 'version', 'platform',
-                   'locale', 'topic')
+    list_filter = ('product', 'version', 'platform', 'locale')
+    search_fields = ('topic', 'target')
 
 
 admin.site.register(Redirect, RedirectAdmin)
