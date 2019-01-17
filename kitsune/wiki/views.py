8964ba144bf633368e606c79290433cece1ca2f6
Tasos Katsoulas <akatsoulas@gmail.com>
2019-01-17 16:55:43 +0200

    Add assets to django-compressor.
    * Remove swiper dependency to cdnjs.
    * Fix path for video.
    * Add GA into the experiments.

--- kitsune/wiki/views.py
+++ kitsune/wiki/views.py
@@ -52,6 +52,10 @@ from kitsune.wiki.tasks import (
 
 
 log = logging.getLogger('k.wiki')
+SUMO_UX_EXPERIMENTS_SLUGS = [
+    'enable-and-disable-cookies-website-preferences',
+    'insecure-password-warning-firefox'
+]
 
 
 def doc_page_cache(view):
@@ -126,6 +130,10 @@ def document(request, document_slug, template=None, document=None):
             # There is no translation
             # and OK to fall back to parent (parent is approved).
             fallback_reason = 'no_translation'
+
+    # Render the static pages if the slug matches the experiment
+    if doc.slug in SUMO_UX_EXPERIMENTS_SLUGS and request.LANGUAGE_CODE == 'en-US':
+        return render(request, 'kb-ux-experiment/{0}.html'.format(doc.slug))
 
     # Find and show the defined fallback locale rather than the English version of the document
     # The fallback locale is defined based on the ACCEPT_LANGUAGE header,
