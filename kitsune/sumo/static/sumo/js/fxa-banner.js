39863f9b48c4cb11963f3c0e11ea54f21a7c3c4b
Paul McLanahan <pmac@mozilla.com>
2019-03-20 16:20:04 -0400

    Fix #3504: Hide blue fxa-banner bar unless user is Firefox

--- kitsune/sumo/static/sumo/js/fxa-banner.js
+++ kitsune/sumo/static/sumo/js/fxa-banner.js
@@ -7,7 +7,9 @@
     var banners = ['send', 'monitor', 'sync'];
     var choice = Math.floor(Math.random() * banners.length);
     var copy = document.getElementById('fxa-banner-' + banners[choice]);
+    var bar = document.getElementById('fxa-banner');
 
+    bar.classList.add('visible');
     copy.classList.add('chosen');
 
     setTimeout(function() {
