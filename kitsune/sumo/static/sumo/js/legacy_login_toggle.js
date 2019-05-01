5b5af01cfb2dfbbb6d544f2b52d12293ed011410
Michael Cranston <mike.cranston@lightsonsoftware.com>
2019-05-01 10:15:47 -0400

    Wrap lines at 120 chars and update toggle

--- kitsune/sumo/static/sumo/js/legacy_login_toggle.js
+++ kitsune/sumo/static/sumo/js/legacy_login_toggle.js
@@ -1,11 +1,13 @@
 (function() {
-  const legacyLoginToggle = document.getElementById('legacy-login-toggle');
+  const legacyLoginToggles = document.querySelectorAll('.legacy-login-toggle');
   const loginLegacy = document.getElementById('login-legacy');
   const loginFFX = document.getElementById('login-ffx');
 
-  legacyLoginToggle.onclick = function(e) {
-    e.preventDefault();
-    loginLegacy.classList.toggle('hidden');
-    loginFFX.classList.toggle('hidden');
-  };
+  for (let legacyLoginToggle of legacyLoginToggles) {
+    legacyLoginToggle.onclick = function(e) {
+      e.preventDefault();
+      loginLegacy.classList.toggle('hidden');
+      loginFFX.classList.toggle('hidden');
+    };
+  }
 })();
