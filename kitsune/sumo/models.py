9f5e2af9f2761cf3fafc4b0a196717cbb8df6da0
Tasos Katsoulas <akatsoulas@gmail.com>
2019-09-12 17:28:44 +0300

    Remove update method from ModelBase abstract model.

--- kitsune/sumo/models.py
+++ kitsune/sumo/models.py
@@ -3,38 +3,10 @@ from django.db import models
 
 
 class ModelBase(models.Model):
-    """Base class for SUMO models.
-
-    * Adds update method.
-    """
+    """Base class for SUMO models."""
 
     class Meta:
         abstract = True
-
-    # TODO: Remove this in django 1.6, which comes with a smarter save().
-    def update(self, **kw):
-        """
-        Shortcut for doing an UPDATE on this object.
-
-        If _signal=False is in ``kw`` the post_save signal won't be sent.
-        """
-        signal = kw.pop('_signal', True)
-        cls = self.__class__
-        for k, v in kw.items():
-            setattr(self, k, v)
-        if signal:
-            # Detect any attribute changes during pre_save and add those to the
-            # update kwargs.
-            attrs = dict(self.__dict__)
-            models.signals.pre_save.send(sender=cls, instance=self)
-            for k, v in self.__dict__.items():
-                if attrs[k] != v:
-                    kw[k] = v
-                    setattr(self, k, v)
-        cls.objects.filter(pk=self.pk).update(**kw)
-        if signal:
-            models.signals.post_save.send(sender=cls, instance=self,
-                                          created=False)
 
 
 class LocaleField(models.CharField):
