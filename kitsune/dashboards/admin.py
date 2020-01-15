d30a6063f1025958bfe213770636a53b2874eb97
saadmk11 <saad.mk112@gmail.com>
2020-01-15 18:52:46 +0600

    Improve wikimetrics admin filters

--- kitsune/dashboards/admin.py
+++ kitsune/dashboards/admin.py
@@ -4,8 +4,14 @@ from kitsune.dashboards.models import WikiMetric
 
 
 class WikiMetricAdmin(admin.ModelAdmin):
-    list_display = ["code", "date", "locale", "product", "value"]
+    list_display = ["code", "date", "locale", "product", "value", "locale_code"]
     list_filter = ["code", "product", "locale"]
+    date_hierarchy = "date"
+
+    def locale_code(self, obj):
+        return obj.locale
+
+    locale_code.short_description = "Locale Code"
 
 
 admin.site.register(WikiMetric, WikiMetricAdmin)
