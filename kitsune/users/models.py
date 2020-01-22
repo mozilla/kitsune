405eded56cd7f00521739e94b73d5a80e1d18579
root <ben@lincolnloop.com>
2020-01-22 20:59:08 +0000

    Fix some str/bytes issues.

--- kitsune/users/models.py
+++ kitsune/users/models.py
@@ -427,7 +427,7 @@ class ConfirmationManager(models.Manager):
         The activation key will be a SHA1 hash, generated from a combination
         of the ``User``'s username and a random salt.
         """
-        salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
+        salt = hashlib.sha1(str(random.random()).encode()).hexdigest()[:5]
         activation_key = hashlib.sha1(salt + user.username).hexdigest()
         return self.create(user=user, activation_key=activation_key, **kwargs)
 
