cb0032f0089c8edfcd5fe22fd71e89f4103eec71
Ben Spaulding <ben@lincolnloop.com>
2020-01-23 11:49:32 -0700

    Revert mistaken 2to3 iterator fix.

--- kitsune/sumo/tests/__init__.py
+++ kitsune/sumo/tests/__init__.py
@@ -291,7 +291,7 @@ class SumoPyQuery(PyQuery):
     """Extends PyQuery with some niceties to alleviate its bugs"""
     def first(self):
         """:first doesn't work, so this is a meh substitute"""
-        return next(list(self.items()))
+        return next(self.items())
 
 
 def template_used(response, template_name):
