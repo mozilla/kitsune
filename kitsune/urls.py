db48bc5b239f1fd533efa3df5d57de17d852c7ae
Ben Spaulding <ben@lincolnloop.com>
2020-02-04 14:20:07 -0700

    Drop use of deprecated javascript_catalog view function
    mozilla/sumo-project#136

--- kitsune/urls.py
+++ kitsune/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.conf import settings
-from django.views.i18n import javascript_catalog
+from django.views.i18n import JavaScriptCatalog
 from django.views.decorators.cache import cache_page
 from django.views.generic.base import RedirectView
 from django.views.static import serve as servestatic
@@ -39,11 +39,11 @@ urlpatterns = [
     url(r'^badges/', include('kitsune.kbadge.urls')),
 
     # Javascript translations.
-    url(r'^jsi18n/.*$', cache_page(60 * 60 * 24 * 365)(javascript_catalog),
+    url(r'^jsi18n/.*$', cache_page(60 * 60 * 24 * 365)(JavaScriptCatalog.as_view()),
         {'domain': 'djangojs', 'packages': ['kitsune']}, name='jsi18n'),
     # App translations. These don't need cached because they are downloaded
     # in a build step, not on the client.
-    url(r'^jsi18n-yaocho/.*$', javascript_catalog,
+    url(r'^jsi18n-yaocho/.*$', JavaScriptCatalog.as_view(),
         {'domain': 'yaocho', 'packages': ['kitsune']}, name='jsi18n-yaocho'),
     # JavaScript Waffle.
     url(r'^wafflejs$', wafflejs, name='wafflejs'),
