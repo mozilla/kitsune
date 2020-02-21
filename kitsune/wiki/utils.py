c4cf998b2a506c220cd9364ccc084e14d067feab
Tasos Katsoulas <akatsoulas@gmail.com>
2020-02-21 15:09:06 +0200

    Add featured articles functionality.
    * Returns 4 random articles out of the 10 most visited.
    * Return 4 random articles out of the 10 most visited per product.
    * Blacken some files.

--- kitsune/wiki/utils.py
+++ kitsune/wiki/utils.py
@@ -1,3 +1,4 @@
+import random
 import requests
 
 from django.conf import settings
@@ -5,21 +6,26 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.utils.http import urlencode
 
+from kitsune.dashboards import LAST_7_DAYS
+from kitsune.dashboards.models import WikiDocumentVisits
 from kitsune.wiki.models import Revision
 
 
 class BitlyException(Exception):
     """Bitly Exception for any other errors."""
+
     pass
 
 
 class BitlyUnauthorizedException(BitlyException):
     """Bitly Exception for an unauthorized error."""
+
     pass
 
 
 class BitlyRateLimitException(BitlyException):
     """Bitly Exception for a rate limiting error."""
+
     pass
 
 
@@ -34,9 +40,9 @@ def active_contributors(from_date, to_date=None, locale=None, product=None):
     :arg locale: (optional) locale to filter on
     :arg product: (optional) only count documents for a product
     """
-    return (User.objects.filter(
-        id__in=_active_contributors_id(from_date, to_date, locale, product))
-        .order_by('username'))
+    return User.objects.filter(
+        id__in=_active_contributors_id(from_date, to_date, locale, product)
+    ).order_by("username")
 
 
 def generate_short_url(long_url):
@@ -46,34 +52,32 @@ def generate_short_url(long_url):
     """
 
     # Check for empty credentials.
-    if (settings.BITLY_LOGIN is None or
-            settings.BITLY_API_KEY is None):
-        return ''
+    if settings.BITLY_LOGIN is None or settings.BITLY_API_KEY is None:
+        return ""
 
     keys = {
-        'format': 'json',
-        'longUrl': long_url,
-        'login': settings.BITLY_LOGIN,
-        'apiKey': settings.BITLY_API_KEY
+        "format": "json",
+        "longUrl": long_url,
+        "login": settings.BITLY_LOGIN,
+        "apiKey": settings.BITLY_API_KEY,
     }
     params = urlencode(keys)
 
     resp = requests.post(settings.BITLY_API_URL, params).json()
-    if resp['status_code'] == 200:
-        short_url = resp.get('data', {}).get('url', '')
+    if resp["status_code"] == 200:
+        short_url = resp.get("data", {}).get("url", "")
         return short_url
-    elif resp['status_code'] == 401:
+    elif resp["status_code"] == 401:
         raise BitlyUnauthorizedException("Unauthorized access to bitly's API")
-    elif resp['status_code'] == 403:
-        raise BitlyRateLimitException("Rate limit exceeded while using "
-                                      "bitly's API.")
+    elif resp["status_code"] == 403:
+        raise BitlyRateLimitException("Rate limit exceeded while using " "bitly's API.")
     else:
-        raise BitlyException("Error code: {0} recieved from bitly's API."
-                             .format(resp['status_code']))
+        raise BitlyException(
+            "Error code: {0} recieved from bitly's API.".format(resp["status_code"])
+        )
 
 
-def num_active_contributors(from_date, to_date=None, locale=None,
-                            product=None):
+def num_active_contributors(from_date, to_date=None, locale=None, product=None):
     """Return number of active KB contributors for the specified parameters.
 
     An active KB contributor is a user that has created or reviewed a
@@ -98,13 +102,17 @@ def _active_contributors_id(from_date, to_date, locale, product):
     :arg locale: (optional) locale to filter on
     :arg product: (optional) only count documents for a product
     """
-    editors = (Revision.objects
-               .filter(created__gte=from_date)
-               .values_list('creator', flat=True).distinct())
+    editors = (
+        Revision.objects.filter(created__gte=from_date)
+        .values_list("creator", flat=True)
+        .distinct()
+    )
 
-    reviewers = (Revision.objects
-                 .filter(reviewed__gte=from_date)
-                 .values_list('reviewer', flat=True).distinct())
+    reviewers = (
+        Revision.objects.filter(reviewed__gte=from_date)
+        .values_list("reviewer", flat=True)
+        .distinct()
+    )
 
     if to_date:
         editors = editors.filter(created__lt=to_date)
@@ -116,10 +124,35 @@ def _active_contributors_id(from_date, to_date, locale, product):
 
     if product:
         editors = editors.filter(
-            Q(document__products=product) |
-            Q(document__parent__products=product))
+            Q(document__products=product) | Q(document__parent__products=product)
+        )
         reviewers = reviewers.filter(
-            Q(document__products=product) |
-            Q(document__parent__products=product))
+            Q(document__products=product) | Q(document__parent__products=product)
+        )
 
     return set(list(editors) + list(reviewers))
+
+
+def get_featured_articles(product=None):
+    """Returns 4 random articles from the most visited.
+
+    If a product is passed, it returns 4 random highly visited articles.
+    """
+
+    featured = (
+        WikiDocumentVisits.objects.filter(period=LAST_7_DAYS)
+        .order_by("-visits")
+        .select_related("document")
+    )
+
+    if product:
+        featured = featured.filter(document__products__in=[product.id])
+
+    # Limit this to 10 entries
+    if featured.count() < 10:
+        return featured
+
+    # This returns a WikiDocumentVisits object and not the actual Wiki doc.
+    # The wiki doc it's under .document which is prefetched b/c of select_related
+    featured = featured[:10]
+    return random.sample(featured, 4)
