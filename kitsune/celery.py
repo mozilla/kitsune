f56d2bdb2bd1bbabb90c511f1801a3cea9aa8422
Tasos Katsoulas <akatsoulas@gmail.com>
2020-03-03 13:18:53 +0200

    Update configuration for Celery 4x

--- kitsune/celery.py
+++ kitsune/celery.py
@@ -1,10 +1,10 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitsune.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kitsune.settings")
 
 from celery import Celery  # noqa
 from django.conf import settings  # noqa
 
-app = Celery('kitsune')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(settings.INSTALLED_APPS)
+app = Celery("kitsune")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
