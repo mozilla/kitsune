424308058c55cbb5c2d35909b951228b3f38074b
Kasey Kelly <kasey@servee.com>
2020-04-01 11:37:54 -0400

    rotate on open

--- kitsune/wiki/config.py
+++ kitsune/wiki/config.py
@@ -14,6 +14,7 @@ COLLAPSIBLE_DOCUMENTS = {
         u"how-answer-escalated-questions",
         u"navigate-support-forum-platform",
         u"how-stop-firefox-making-automatic-connections",
+        u"sample-article-1",
     ],
     u"cs": [u"jak-firefoxu-zabranit-v-automatickem-navazovani-sp",],
     u"de": [u"Firefox-baut-unaufgeforderte-Verbindungen-auf",],
