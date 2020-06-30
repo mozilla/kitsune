8d2f416ada89687245e9b2e5055bcde7f3b14fc4
Leo McArdle <lmcardle@mozilla.com>
2020-06-30 15:15:41 +0100

    update to django-pipeline 2.0.4 (#4437)

--- kitsune/sumo/storage.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
-from pipeline.storage import PipelineMixin
-
-
-class SumoFilesStorage(PipelineMixin, ManifestStaticFilesStorage):
-    def __init__(self, *args, **kwargs):
-        kwargs['file_permissions_mode'] = 0o644
-        kwargs['directory_permissions_mode'] = 0o755
-        super(SumoFilesStorage, self).__init__(*args, **kwargs)
