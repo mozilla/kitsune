7c0b630f1fa82533ad283834fb7c63242cf64431
Tasos Katsoulas <akatsoulas@gmail.com>
2020-07-09 13:55:39 +0300

    Revert "Add a second queue for FxA."

--- bin/run-celery-worker.sh
+++ bin/run-celery-worker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-newrelic-admin run-program celery -A kitsune worker -Q fxa -Ofair -n fxa@%h --maxtasksperchild=${CELERY_WORKER_MAX_TASKS_PER_CHILD:-25} &
-exec newrelic-admin run-program celery -A kitsune worker -Q deafult -n default@%h --maxtasksperchild=${CELERY_WORKER_MAX_TASKS_PER_CHILD:-25} &
+exec newrelic-admin run-program celery -A kitsune worker  --maxtasksperchild=${CELERY_WORKER_MAX_TASKS_PER_CHILD:-25}
