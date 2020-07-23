cacee420ad10917e697d6a064a05a56953c519d7
Leo McArdle <lmcardle@mozilla.com>
2020-07-23 10:59:26 +0100

    black: products

--- kitsune/products/urls_api.py
+++ kitsune/products/urls_api.py
@@ -5,9 +5,11 @@ from kitsune.products import api
 
 # API urls
 urlpatterns = [
-    url(r'^$', api.ProductList.as_view(), name='product-list'),
-    url(r'^(?P<product>[^/]+)/topic/?$', api.TopicList.as_view(), name='topic-list'),
-    url(r'^(?P<product>[^/]+)/topic/(?P<topic>[^/]+)/?$',
+    url(r"^$", api.ProductList.as_view(), name="product-list"),
+    url(r"^(?P<product>[^/]+)/topic/?$", api.TopicList.as_view(), name="topic-list"),
+    url(
+        r"^(?P<product>[^/]+)/topic/(?P<topic>[^/]+)/?$",
         api.TopicDetail.as_view(),
-        name='topic-detail'),
+        name="topic-detail",
+    ),
 ]
