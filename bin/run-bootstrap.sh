8964a799ed5b5cf382089392f8d273569f948d11
saadmk11 <saad.mk112@gmail.com>
2020-08-09 22:05:13 +0600

    run collectstatic with noinput

--- bin/run-bootstrap.sh
+++ bin/run-bootstrap.sh
@@ -9,6 +9,6 @@ yarn
 # ensure the DB server is ready
 urlwait
 # run collectstatic
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 # run DB migrations
 python manage.py migrate
