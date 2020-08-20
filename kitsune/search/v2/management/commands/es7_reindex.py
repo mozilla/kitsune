629f8a5549a7fa29a2a8a01b2ed02988dc714ad9
Leo McArdle <lmcardle@mozilla.com>
2020-08-20 12:52:35 +0100

    add percentage arg to es7_reindex command
    specify python3.8 in black pre-commit hook so it handles walrus operator

--- kitsune/search/v2/management/commands/es7_reindex.py
+++ kitsune/search/v2/management/commands/es7_reindex.py
@@ -15,6 +15,7 @@ class Command(BaseCommand):
             default="",
             help="Limit to specific doc types",
         )
+        parser.add_argument("--percentage", type=float, default=None)
 
     def handle(self, *args, **kwargs):
         doc_types = get_doc_types()
@@ -30,8 +31,13 @@ class Command(BaseCommand):
 
             model = dt.get_model()
             qs = model.objects.all()
-            progress = 0
             count = qs.count()
+            if percentage := kwargs["percentage"]:
+                total = count
+                count = int(count * percentage / 100)
+                print("Indexing {}%, so {} documents out of {}".format(percentage, count, total))
+                qs = qs[:count]
+            progress = 0
 
             for obj in qs:
                 index_object.delay(dt.__name__, obj.id)
