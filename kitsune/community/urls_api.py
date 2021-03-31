23ce7b60c1015290e71f04548951e4567da2fa78
Leo McArdle <lmcardle@mozilla.com>
2021-03-31 08:38:24 +0100

    remove /api/v2/topcontributors/ (#4735)
    depends on elastic 2.x, and isn't used anywhere
    
    https://github.com/mozilla/sumo-project/issues/762

--- kitsune/community/urls_api.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from kitsune.community import api
-
-
-urlpatterns = [
-    url("^topcontributors/questions/$", api.TopContributorsQuestions.as_view()),
-    url("^topcontributors/l10n/$", api.TopContributorsLocalization.as_view()),
-]
