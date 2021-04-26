mozilla/kitsune

commit 5472d99edf8320e20eab38f7b35aa1d9a14928da

Update user forums to ES7.
* Small fixes in v2 views.
* Add KW fields in ForumDocument

kitsune/search/forms.py
@@ -11,14 +11,11 @@ from kitsune.products.models import Product, Topic
 from kitsune.sumo.form_fields import TypedMultipleChoiceField
 from kitsune.wiki.config import CATEGORIES
 
-
 MAX_QUERY_LENGTH = 200
 SEARCH_LANGUAGES = [(k, LOCALES[k].native) for k in settings.SUMO_LANGUAGES]
 
 
-class SimpleSearchForm(forms.Form):
-    """Django form to handle the simple search case."""
-
+class BaseSearchForm(forms.Form):
     q = forms.CharField(required=True, max_length=MAX_QUERY_LENGTH)
 
     w = forms.TypedChoiceField(
@@ -33,6 +30,10 @@ class SimpleSearchForm(forms.Form):
             (constants.WHERE_DISCUSSION, None),
         ),
     )
+
+
+class SimpleSearchForm(BaseSearchForm):
+    """Django form to handle the simple search case."""
 
     explain = forms.BooleanField(required=False)
     all_products = forms.BooleanField(required=False)
@@ -70,32 +71,15 @@ class SimpleSearchForm(forms.Form):
             or "iphone" in lowered_q
         ):
             products.append("ios")
-        elif "firefox os" in lowered_q:
-            products.append("firefox-os")
         elif "firefox" in lowered_q:
             products.append("firefox")
         return products
 
 
-class AdvancedSearchForm(forms.Form):
+class AdvancedSearchForm(BaseSearchForm):
     """Django form for handling display and validation"""
 
     # Common fields
-    q = forms.CharField(required=False, max_length=MAX_QUERY_LENGTH)
-
-    w = forms.TypedChoiceField(
-        required=False,
-        coerce=int,
-        widget=forms.HiddenInput,
-        empty_value=constants.WHERE_BASIC,
-        choices=(
-            (constants.WHERE_SUPPORT, None),
-            (constants.WHERE_WIKI, None),
-            (constants.WHERE_BASIC, None),
-            (constants.WHERE_DISCUSSION, None),
-        ),
-    )
-
     # TODO: get rid of this.
     a = forms.IntegerField(required=False, widget=forms.HiddenInput)
 

kitsune/forums/jinja2/forums/forums.html
@@ -4,14 +4,14 @@
 {% set crumbs = [(None, title)] %}
 {% set canonical_url = canonicalize(viewname='forums.forums') %}
 {% if forums.number > 1 %}
-  {% set canonical_url = canonical_url|urlparams(page=forums.number) %}
+{% set canonical_url = canonical_url|urlparams(page=forums.number) %}
 {% endif %}
 
 {% block content %}
-  <article class="sumo-page-section">
-    <div id="forums" class="content-box">
-      <h1 class="sumo-page-heading">{{ title }}</h1>
-      <div class="table-scroll">
+<article class="sumo-page-section">
+  <div id="forums" class="content-box">
+    <h1 class="sumo-page-heading">{{ title }}</h1>
+    <div class="table-scroll">
       <table>
         <thead>
           <tr class="forums-columns">
@@ -23,38 +23,38 @@
         {% if forums.object_list %}
         <tbody class="forums">
           {% for forum in forums.object_list %}
-            <tr>
-              <td class="name">
-                <h5 class="sumo-card-heading"><a href="{{ url('forums.threads', forum_slug=forum.slug) }}">{{ forum.name }}</a></h5>
-                {{ forum.description|safe }}
-              </td>
-              <td class="threads">{{ forum.thread_count }}</td>
-              <td class="last-post">
-                {% if forum.last_post %}
-                  <a href="{{ forum.last_post.get_absolute_url() }}">
-                    {{ datetimeformat(forum.last_post.created) }}
-                  </a><br/>
-                  {{ _('by <a class="username" href="{profile_url}">{username}</a>')|fe(profile_url=profile_url(forum.last_post.author), username=display_name(forum.last_post.author)) }}
-                {% else %}
-                  {# Not localized because it's not worth localizers time. #}
-                  No posts.
-                {% endif %}
-              </td>
-            </tr>
+          <tr>
+            <td class="name">
+              <h5 class="sumo-card-heading"><a
+                  href="{{ url('forums.threads', forum_slug=forum.slug) }}">{{ forum.name }}</a></h5>
+              {{ forum.description|safe }}
+            </td>
+            <td class="threads">{{ forum.thread_count }}</td>
+            <td class="last-post">
+              {% if forum.last_post %}
+              <a href="{{ forum.last_post.get_absolute_url() }}">
+                {{ datetimeformat(forum.last_post.created) }}
+              </a><br />
+              {{ _('by <a class="username" href="{profile_url}">{username}</a>')|fe(profile_url=profile_url(forum.last_post.author), username=display_name(forum.last_post.author)) }}
+              {% else %}
+              {# Not localized because it's not worth localizers time. #}
+              No posts.
+              {% endif %}
+            </td>
+          </tr>
           {% endfor %}
         </tbody>
       </table>
-      </div>
-
-        {{ forums|paginator }}
-        {% else %}{# if forums #}
-        {# Not localized because it's not worth localizers time. #}
-        <p>There are no forums. You should create some!</p>
-      {% endif %}
     </div>
-  </article>
+    {{ forums|paginator }}
+    {% else %}{# if forums #}
+    {# Not localized because it's not worth localizers time. #}
+    <p>There are no forums. You should create some!</p>
+    {% endif %}
+  </div>
+</article>
 {% endblock %}
 
 {% block side %}
-  {{ for_contributors_sidebar(user, settings.WIKI_DEFAULT_LANGUAGE, active='forums') }}
+{{ for_contributors_sidebar(user, settings.WIKI_DEFAULT_LANGUAGE, active='forums') }}
 {% endblock %}

kitsune/forums/jinja2/forums/includes/forum_macros.html
@@ -1,31 +1,35 @@
 {% macro thread_list(threads, forum=None) -%}
-  {% for thread in threads.object_list %}
-    {% if forum %}
-      {% set _forum = forum %}
-    {% else %}
-      {% set _forum = thread.forum %}
+{% for thread in threads.object_list %}
+{% if forum %}
+{% set _forum = forum %}
+{% else %}
+{% set _forum = thread.forum %}
+{% endif %}
+<tr>
+  <td class="type">
+    {% if thread.is_locked %}
+    <img src="{{ STATIC_URL }}protocol/img/icons/lockbox.svg" alt="{{ pgettext('thread_type', 'Locked') }}"
+      title="{{ pgettext('thread_type', 'Locked') }}" />
     {% endif %}
-    <tr>
-      <td class="type">
-        {% if thread.is_locked %}
-          <img src="{{ STATIC_URL }}protocol/img/icons/lockbox.svg" alt="{{ pgettext('thread_type', 'Locked') }}" title="{{ pgettext('thread_type', 'Locked') }}" />
-        {% endif %}
-        {% if thread.is_sticky %}
-          <img src="{{ STATIC_URL }}protocol/img/icons/pin.svg" alt="{{ pgettext('thread_type', 'Sticky') }}" title="{{ pgettext('thread_type', 'Sticky') }}" />
-        {% endif %}
-      </td>
-      <td class="title"><a href="{{ url('forums.posts', forum_slug=_forum.slug, thread_id=thread.id)|urlparams(last=thread.last_post.id) }}">{{ thread.title }}</a></td>
-      <td class="author"><a class="username" href="{{ profile_url(thread.creator) }}">{{ display_name(thread.creator) }}</a></td>
-      <td class="replies">{{ thread.replies }}</td>
-      {% if thread.last_post %}
-        <td class="last-post">
-          <a href="{{ thread.get_last_post_url() }}">
-            {{ datetimeformat(thread.last_post.created) }}
-          </a><br/>
-          {{ _('by <a class="username" href="{profile_url}">{username}</a>')|fe(profile_url=profile_url(thread.last_post.author), username=display_name(thread.last_post.author)) }}
-        </td>
-      {% endif %}
-    </tr>
-  {% endfor %}
-  {{ threads|paginator }}
+    {% if thread.is_sticky %}
+    <img src="{{ STATIC_URL }}protocol/img/icons/pin.svg" alt="{{ pgettext('thread_type', 'Sticky') }}"
+      title="{{ pgettext('thread_type', 'Sticky') }}" />
+    {% endif %}
+  </td>
+  <td class="title"><a
+      href="{{ url('forums.posts', forum_slug=_forum.slug, thread_id=thread.id)|urlparams(last=thread.last_post.id) }}">{{ thread.title }}</a>
+  </td>
+  <td class="author"><a class="username" href="{{ profile_url(thread.creator) }}">{{ display_name(thread.creator) }}</a>
+  </td>
+  <td class="replies">{{ thread.replies }}</td>
+  {% if thread.last_post %}
+  <td class="last-post">
+    <a href="{{ thread.get_last_post_url() }}">
+      {{ datetimeformat(thread.last_post.created) }}
+    </a><br />
+    {{ _('by <a class="username" href="{profile_url}">{username}</a>')|fe(profile_url=profile_url(thread.last_post.author), username=display_name(thread.last_post.author)) }}
+  </td>
+  {% endif %}
+</tr>
+{% endfor %}
 {%- endmacro %}

kitsune/forums/jinja2/forums/threads.html
@@ -6,58 +6,74 @@
 {% set crumbs = [(url('forums.forums'), _('Contributor Forums')), (None, forum.name)] %}
 {% set canonical_url = canonicalize(viewname='forums.threads', forum_slug=forum.slug) %}
 {% if threads.number > 1 %}
-  {% set canonical_url = canonical_url|urlparams(page=threads.number) %}
+{% set canonical_url = canonical_url|urlparams(page=threads.number) %}
 {% endif %}
 
 {% block content %}
-    <article id="threads" class="content-box sumo-page-section">
-      <h1 class="sumo-page-heading">{{ forum.name }}</h1>
-      {{ forum.description|safe }}
-      <div class="forum-actions sumo-button-wrap">
-        {% if not user.is_authenticated or forum.allows_posting_by(user) %}
-          <a id="new-thread" class="sumo-button primary-button" href="{{ url('forums.new_thread', forum_slug=forum.slug) }}">{{ _('Post a new thread') }}</a>
-        {% endif %}
-        {% if user.is_authenticated %}
-          <form action="{{ url('forums.watch_forum', forum_slug=forum.slug) }}" method="post">
-            {% csrf_token %}
-            {% if is_watching_forum %}
-              {% set watch = _('Stop watching this forum') %}
-              <input type="hidden" name="watch" value="no" />
-            {% else %}
-              {% set watch = _('Watch this forum') %}
-              <input type="hidden" name="watch" value="yes" />
-            {% endif %}
-            <button type="submit" title="{{ watch }}" class="sumo-button secondary-button" id="watch-forum-toggle">{{ watch }}</button>
-          </form>
-        {% endif %}
-      </div>
+<article id="threads" class="content-box sumo-page-section">
+  <h1 class="sumo-page-heading">{{ forum.name }}</h1>
+  {{ forum.description|safe }}
+  <div class="forum-actions sumo-button-wrap">
+    {% if not user.is_authenticated or forum.allows_posting_by(user) %}
+    <a id="new-thread" class="sumo-button primary-button"
+      href="{{ url('forums.new_thread', forum_slug=forum.slug) }}">{{ _('Post a new thread') }}</a>
+    {% endif %}
+    {% if user.is_authenticated %}
+    <form action="{{ url('forums.watch_forum', forum_slug=forum.slug) }}" method="post">
+      {% csrf_token %}
+      {% if is_watching_forum %}
+      {% set watch = _('Stop watching this forum') %}
+      <input type="hidden" name="watch" value="no" />
+      {% else %}
+      {% set watch = _('Watch this forum') %}
+      <input type="hidden" name="watch" value="yes" />
+      {% endif %}
+      <button type="submit" title="{{ watch }}" class="sumo-button secondary-button"
+        id="watch-forum-toggle">{{ watch }}</button>
+    </form>
+    {% endif %}
+  </div>
 
+  {% if threads.object_list %}
+  <div class="sumo-page-section--sm">
+    <form name="find-thread" id="find-thread" class="simple-search-form"
+      action="{{ url('forums.search', forum_slug=forum.slug) }}" method="get">
+      <input type="search" name="q" id="search-q" class="searchbox" value="{{ q or '' }}"
+        placeholder="{{ _('Search this forum') }}" />
+      <input type="submit" value="{{ _('Search') }}" class="search-button" />
+    </form>
+  </div>
+  {% endif %}
 
-      <div class="table-scroll">
-      <table>
-        <thead>
-          <tr class="threads-columns {% if not desc_toggle %}desc{% endif %}">
-            <th class="type"><a href="{{ url('forums.threads', forum_slug=forum.slug) }}">{{ _('Type') }}</a></th>
-            <th class="title">{{ _('Title') }}</th>
-            <th class="author{% if sort == 3 %} sort{% endif %}"><a href="{{ request.path|urlparams(sort=3, desc=desc_toggle) }}">{{ _('Author') }}</a></th>
-            <th class="replies{% if sort == 4 %} sort{% endif %}"><a href="{{ request.path|urlparams(sort=4, desc=desc_toggle) }}">{{ _('Replies') }}</a></th>
-            <th class="last-post{% if sort == 5 %} sort{% endif %}"><a href="{{ request.path|urlparams(sort=5, desc=desc_toggle) }}">{{ _('Last Post') }}</a></th>
-          </tr>
-        </thead>
-        <tbody class="threads">
-          {% if threads.object_list %}
-            {{ thread_list(threads, forum) }}
-          {% else %}
-            <tr>
-              <td colspan="5">
-                <p>{{ _('There are no threads. Maybe you should create some!') }}</p>
-              </td>
-            </tr>
-          {% endif %}
-        </tbody>
-      </table>
-      </div>
-    </article>
+  <div class="table-scroll">
+    <table>
+      <thead>
+        <tr class="threads-columns {% if not desc_toggle %}desc{% endif %}">
+          <th class="type"><a href="{{ url('forums.threads', forum_slug=forum.slug) }}">{{ _('Type') }}</a></th>
+          <th class="title">{{ _('Title') }}</th>
+          <th class="author{% if sort == 3 %} sort{% endif %}"><a
+              href="{{ request.path|urlparams(sort=3, desc=desc_toggle) }}">{{ _('Author') }}</a></th>
+          <th class="replies{% if sort == 4 %} sort{% endif %}"><a
+              href="{{ request.path|urlparams(sort=4, desc=desc_toggle) }}">{{ _('Replies') }}</a></th>
+          <th class="last-post{% if sort == 5 %} sort{% endif %}"><a
+              href="{{ request.path|urlparams(sort=5, desc=desc_toggle) }}">{{ _('Last Post') }}</a></th>
+        </tr>
+      </thead>
+      <tbody class="threads">
+        {% if threads.object_list %}
+        {{ thread_list(threads, forum) }}
+        {% else %}
+        <tr>
+          <td colspan="5">
+            <p>{{ _('There are no threads. Maybe you should create some!') }}</p>
+          </td>
+        </tr>
+        {% endif %}
+      </tbody>
+    </table>
+  </div>
+  {{ threads|paginator }}
+</article>
 {% endblock %}
 
 {% block side %}

kitsune/forums/urls.py
@@ -19,6 +19,7 @@ else:
 forum_patterns = [
     path("", views.threads, name="forums.threads"),
     path("new", views.new_thread, name="forums.new_thread"),
+    path("search", views.search, name="forums.search"),
     path("<int:thread_id>", views.posts, name="forums.posts"),
     path("<int:thread_id>/reply", views.reply, name="forums.reply"),
     path("feed", threads_feed_view, name="forums.threads.feed"),

kitsune/forums/views.py
@@ -1,23 +1,26 @@
 import logging
 from datetime import datetime
 
+from authority.decorators import permission_required_or_403
+from django.contrib import messages
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseRedirect, Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST
 
-from authority.decorators import permission_required_or_403
-
-from kitsune import forums as constants
+from kitsune import forums as forum_constants
 from kitsune.access.decorators import has_perm_or_owns_or_403, login_required
 from kitsune.access.utils import has_perm
 from kitsune.forums.events import NewPostEvent, NewThreadEvent
-from kitsune.forums.feeds import ThreadsFeed, PostsFeed
-from kitsune.forums.forms import ReplyForm, NewThreadForm, EditThreadForm, EditPostForm
-from kitsune.forums.models import Forum, Thread, Post
+from kitsune.forums.feeds import PostsFeed, ThreadsFeed
+from kitsune.forums.forms import EditPostForm, EditThreadForm, NewThreadForm, ReplyForm
+from kitsune.forums.models import Forum, Post, Thread
+from kitsune.search.forms import BaseSearchForm
+from kitsune.search.v2.search import ForumSearch
 from kitsune.sumo.templatetags.jinja_helpers import urlparams
 from kitsune.sumo.urlresolvers import reverse
-from kitsune.sumo.utils import paginate, is_ratelimited
+from kitsune.sumo.utils import is_ratelimited, paginate
 from kitsune.users.models import Setting
 
 log = logging.getLogger("k.forums")
@@ -75,7 +78,7 @@ def threads(request, forum_slug):
     threads_ = sort_threads(forum.thread_set, sort, desc)
     count = threads_.count()
     threads_ = threads_.select_related("creator", "last_post", "last_post__author")
-    threads_ = paginate(request, threads_, per_page=constants.THREADS_PER_PAGE, count=count)
+    threads_ = paginate(request, threads_, per_page=forum_constants.THREADS_PER_PAGE, count=count)
 
     feed_urls = ((reverse("forums.threads.feed", args=[forum_slug]), ThreadsFeed().title(forum)),)
 
@@ -125,7 +128,7 @@ def posts(request, forum_slug, thread_id, form=None, post_preview=None, is_reply
             "forums_post.author_id = auth_user.id"
         }
     )
-    posts_ = paginate(request, posts_, constants.POSTS_PER_PAGE, count=count)
+    posts_ = paginate(request, posts_, forum_constants.POSTS_PER_PAGE, count=count)
 
     if not form:
         form = ReplyForm()
@@ -510,3 +513,36 @@ def post_preview_async(request):
     post.author_post_count = 1
 
     return render(request, "forums/includes/post_preview.html", {"post_preview": post})
+
+
+def search(request, forum_slug=None):
+    """Search a specific forum."""
+
+    try:
+        forum = Forum.objects.get(slug=forum_slug)
+    except Forum.DoesNotExist:
+        raise Http404
+
+    search_form = BaseSearchForm(request.GET, initial={"forum": forum})
+    if not search_form.is_valid():
+        messages.add_message(
+            request, messages.WARNING, _("Something went wrong. Cannot search this forum.")
+        )
+        return threads(request, forum_slug=forum_slug)
+
+    cdata = search_form.cleaned_data
+
+    search = ForumSearch()
+
+    # execute search
+    search.run(cdata["q"], thread_forum_id=forum.pk)
+    total = search.total
+    results = search.results
+    data = {
+        "q": cdata["q"],
+        "results": results,
+        "search_form": search_form,
+        "num_results": total,
+        "forum": forum,
+    }
+    return render(request, "search/search-results.html", data)

kitsune/search/jinja2/search/includes/result.html
@@ -1,53 +1,62 @@
 {% macro search_result(result) %}
-  <article class="topic-article result {{ result.type }}">
-    <div class="topic-article--icon-wrap">
-      {% if result.type == 'document' %}
-      <img class="topic-article--icon" src="/static/protocol/img/icons/reader-mode.svg" alt="">
-      {% elif result.type == 'question' %}
-      <img class="topic-article--icon" src="/static/protocol/img/icons/blog.svg" alt="">
-      {% endif %}
-    </div>
+<article class="topic-article result {{ result.type }}">
+  <div class="topic-article--icon-wrap">
+    {% if result.type == 'document' %}
+    <img class="topic-article--icon" src="/static/protocol/img/icons/reader-mode.svg" alt="">
+    {% elif result.type == 'question' %}
+    <img class="topic-article--icon" src="/static/protocol/img/icons/blog.svg" alt="">
+    {% elif result.type == 'thread' %}
+    <img class="topic-article--icon" src="/static/protocol/img/icons/get-involved.svg" alt="">
+    {% endif %}
+  </div>
 
-    {% set url =  result.url %}
-    <div class="topic-article--text">
-      <h3 class="sumo-card-heading">
-        <a class="title" href="{{ url }}" {% if as == 'aaq' %}target="_blank"{% endif %}>{{ result.title }}</a>
-      </h3>
-      {% if result.type == 'question' %}
-      <ul class="thread-meta topic-article--meta-list">
-        <li class="question-status text-body-sm">
-          {% if result.is_solved %}
-            <span class="is-solution">{{ _('Solved') }}</span>
-          {% endif %}
-          Last updated {{ result.last_updated|timesince }}
-        </li>
-        <li>
-          {% if result.num_answers > 0 %}
-            {{ ngettext('1 reply', '{n} replies',
-                    result.num_answers)|f(n=result.num_answers) }}
-          {% else %}
-            {{ _('No replies') }}
-          {% endif %}
-        </li>
-        <li>
-          {{ ngettext('1 person has this problem', '{n} people have this problem',
-                  result.num_votes)|f(n=result.num_votes) }}
-        </li>
-        <li>
-          {{ ngettext('1 new this week', '{n} new this week',
-                  result.num_votes_past_week)|f(n=result.num_votes_past_week) }}
-        </li>
-      </ul>{# .thread-meta #}
+  {% set url =  result.url %}
+  <div class="topic-article--text">
+    <h3 class="sumo-card-heading">
+      <a class="title" href="{{ url }}" {% if as == 'aaq' %}target="_blank" {% endif %}>{{ result.title }}</a>
+    </h3>
+
+    {% if result.type == 'question' or result.type == "thread" %}
+    <ul class="thread-meta topic-article--meta-list">
+      <li class="question-status text-body-sm">
+        {% if result.type == "question" and result.is_solved %}
+        <span class="is-solution">{{ _('Solved') }}</span>
+        {% endif %}
+        Last updated {{ result.last_updated|timesince }}
+      </li>
+      {% if result.type == "question" %}
+      <li>
+        {% if result.num_answers > 0 %}
+        {{ ngettext('1 reply', '{n} replies',
+                      result.num_answers)|f(n=result.num_answers) }}
+        {% else %}
+        {{ _('No replies') }}
+        {% endif %}
+      </li>
+      <li>
+        {{ ngettext('1 person has this problem', '{n} people have this problem',
+                    result.num_votes)|f(n=result.num_votes) }}
+      </li>
+      <li>
+        {{ ngettext('1 new this week', '{n} new this week',
+                    result.num_votes_past_week)|f(n=result.num_votes_past_week) }}
+      </li>
+      {% endif %}
+    </ul>{# .thread-meta #}
     {% endif %}
 
+    {% if result.type == "thread" %}
+    <p>{{ result.search_summary|safe }}</p>
+    {% else %}
     <p>{{ result.search_summary }}</p>
+    {% endif %}
 
 
     {% if 'explain' in request.GET and request.GET['explain'] == '1' %}
-      <div class="explain">
-        Explanation:<br>{{ result.explanation|replace('\n', '<br>')|replace(' ', '&nbsp;')|safe }}
-      </div>
-    {% endif %}
+    <div class="explain">
+      Explanation:<br>{{ result.explanation|replace('\n', '<br>')|replace(' ', '&nbsp;')|safe }}
     </div>
-  </article>{# .result #}
+    {% endif %}
+  </div>
+</article>{# .result #}
 {% endmacro %}

kitsune/search/jinja2/search/search-results.html
@@ -1,0 +1,38 @@
+{% extends "search/base.html" %}
+{% from 'includes/common_macros.html' import for_contributors_sidebar %}
+{% from "search/includes/result.html" import search_result with context %}
+{% set crumbs = [(url('forums.search', forum_slug=forum.slug), forum.name), (None, _('Results'))] %}
+
+{% block content %}
+<div id="search-results" class="sumo-page-section--inner">
+  {% if num_results > 0 %}
+  <p class="sumo-page-intro">
+    {# L10n: {n} is the number of search results, {q} is the search query, {product} is the product. #}
+    {{ ngettext('Found <strong>{n}</strong> result for <strong>{q}</strong>',
+                      'Found <strong>{n}</strong> results for <strong>{q}</strong>',
+                      num_results)|fe(n=num_results, q=q) }}
+  </p>
+
+  <div class="content-box">
+    {% for doc in results %}
+    {{ search_result(doc) }}
+    {% endfor %}
+  </div>
+
+  {#  {{ pages|paginator }} #}
+  {% else %}
+  <p class="sumo-page-intro">
+    {{ _("Sorry! 0 results found for <strong>‘{q}’</strong>.")|fe(q=q) }}
+  </p>
+
+  <p>{{ _("Try searching again with different keywords.") }}</p>
+
+  {% endif %}
+</div>
+</div>
+{% endblock %}
+
+
+{% block side %}
+{{ for_contributors_sidebar(user, settings.WIKI_DEFAULT_LANGUAGE) }}
+{% endblock %}

commit 72b0cb0ba2797d805d71de05adba657341abab0e

re-allow empty query in advanced search
5472d99edf8320e20eab38f7b35aa1d9a14928da accidentally made the q parameter required

https://bugzilla.mozilla.org/show_bug.cgi?id=1707552

kitsune/search/forms.py
@@ -79,7 +79,8 @@ class SimpleSearchForm(BaseSearchForm):
 class AdvancedSearchForm(BaseSearchForm):
     """Django form for handling display and validation"""
 
-    # Common fields
+    q = forms.CharField(required=False, max_length=MAX_QUERY_LENGTH)
+
     # TODO: get rid of this.
     a = forms.IntegerField(required=False, widget=forms.HiddenInput)
 