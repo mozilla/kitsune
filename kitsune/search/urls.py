9938fdd978cc74de659708549400a1590ac19743
Tasos Katsoulas <akatsoulas@gmail.com>
2021-04-28 17:55:39 +0300

    Use /search for ES7
    * Remove waffle flag.

--- kitsune/search/urls.py
+++ kitsune/search/urls.py
@@ -4,8 +4,7 @@ from kitsune.search import views
 from kitsune.search.v2 import views as v2_views
 
 urlpatterns = [
-    url(r"^$", views.simple_search, name="search"),
+    url(r"^$", v2_views.simple_search, name="search"),
     url(r"^/xml$", views.opensearch_plugin, name="search.plugin"),
     url(r"^/suggestions$", views.opensearch_suggestions, name="search.suggestions"),
-    url(r"^/v2/$", v2_views.simple_search, name="search.v2"),
 ]
