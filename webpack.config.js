67bebd8f8f53afdc84cfa16f0dde8d021cc28d3d
Leo McArdle <lmcardle@mozilla.com>
2021-06-21 11:10:11 +0100

    add basic webpack config, using previously defined entrypoints
    load all javascript through webpack - it's mostly broken because we haven't exposed globals yet - that's our next step

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+const entrypoints = require("./webpack/entrypoints");
+const entrypointsHtml = require("./webpack/entrypoints-html");
+
+module.exports = (env, argv) => {
+  const config = {
+    resolve: {
+      alias: {
+        protocol: "@mozilla-protocol/core/protocol",
+        sumo: path.resolve(__dirname, "kitsune/sumo/static/sumo"),
+        community: path.resolve(
+          __dirname,
+          "kitsune/community/static/community"
+        ),
+        kpi: path.resolve(__dirname, "kitsune/kpi/static/kpi"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.es6$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    entry: entrypoints,
+    plugins: [
+      ...entrypointsHtml,
+    ],
+    output: {
+      filename:
+        argv.mode === "development" ? "[name].js" : "[name].[contenthash].js",
+    },
+  };
+
+  return config;
+};
