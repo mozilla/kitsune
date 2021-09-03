048733185be822494d69fc0a6c808be7cc98b942
Tasos Katsoulas <akatsoulas@gmail.com>
2021-09-03 18:01:58 +0300

    Add method to update primary email

--- kitsune/customercare/tasks.py
+++ kitsune/customercare/tasks.py
@@ -1,11 +1,23 @@
 from celery import task
 from django.contrib.auth.models import User
+
 from kitsune.customercare.zendesk import ZendeskClient
 
 
 @task
-def update_zendesk_user(user_id):
+def update_zendesk_user(user_id: int) -> None:
     user = User.objects.get(pk=user_id)
     if user.profile.zendesk_id:
         zendesk = ZendeskClient()
         zendesk.update_user(user)
+
+
+@task
+def update_zendesk_identity(user_id: int, email: str) -> None:
+    user = User.objects.get(pk=user_id)
+    zendesk_user_id = user.profile.zendesk_id
+
+    # fetch identity id
+    if zendesk_user_id:
+        zendesk = ZendeskClient()
+        zendesk.update_primary_email(zendesk_user_id, email)
