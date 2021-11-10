e05e94158951e8150bf9a421d81193875d900d5f
Tasos Katsoulas <akatsoulas@gmail.com>
2021-11-10 18:21:37 +0200

    Update packages.
    * Drop selenium related packages
    * Add pip-tools in dev

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -10,4 +10,4 @@ urlwait http://elasticsearch7:9200 60
 
 ./manage.py es7_init --migrate-writes --migrate-reads
 
-./manage.py test --noinput --nologcapture -a '!search_tests' --with-nicedots
+./manage.py test --noinput --nologcapture -a '!search_tests'
