a977bcaae1bf6513d2fefa01d55c1a2147aebe8e
Tasos Katsoulas <akatsoulas@gmail.com>
2022-03-24 15:54:49 +0200

    Switch to gthread

--- wsgi/config.py
+++ wsgi/config.py
@@ -4,10 +4,11 @@ from os import getenv
 
 bind = "0.0.0.0:{}".format(getenv("PORT", 8000))
 workers = int(getenv("WSGI_NUM_WORKERS", 2))
+threads = int(getenv("WSGI_NUM_THREADS", 2))
 worker_tmp_dir = getenv("WSGI_WORKER_TMP_DIR", "/dev/shm")
 accesslog = "-"
 errorlog = "-"
 loglevel = getenv("WSGI_LOG_LEVEL", "info")
 keepalive = int(getenv("WSGI_KEEP_ALIVE", 2))
-worker_class = getenv("GUNICORN_WORKER_CLASS", "gevent")
+worker_class = getenv("GUNICORN_WORKER_CLASS", "gthread")
 reload = getenv("DEV", False)
