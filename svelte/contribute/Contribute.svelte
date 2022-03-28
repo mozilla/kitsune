dc329a7ee0a9b91477b3a7c10a010044d9f57b12
Leo McArdle <lmcardle@mozilla.com>
2022-03-28 15:54:03 +0100

    add pages from figma designs

--- svelte/contribute/Contribute.svelte
+++ svelte/contribute/Contribute.svelte
@@ -1,6 +1,14 @@
 <script>
     import { Router, Route } from "svelte-navigator";
-    import Picker from "./Picker";
+    import Area from "./Area";
+    import Landing from "./Landing";
+
+    import imgScreens from "./img/Screens.png";
+    import imgHeads from "./img/Heads.png";
+    import imgIntroduction from "./img/Introduction.png";
+    import imgFoundation from "./img/Foundation.png";
+    import imgDots from "./img/Dots.png";
+    import imgHands from "./img/Hands.png";
 
     export let url = "";
     export let locale = "";
@@ -10,33 +18,198 @@
     <title>{gettext("Contribute")} | {gettext("Mozilla Support")}</title>
 </svelte:head>
 
-<div class="sumo-page-section--inner">
-    <h1>{gettext("Contribute Page Scaffolding")}</h1>
-
-    <Router basepath="/{locale}/contribute" {url}>
-        <Route path="forum">
-            <p>{gettext("This is the forum contribution area")}</p>
-            <Picker />
-        </Route>
-        <Route path="kb">
-            <p>{gettext("This is the support article contribution area")}</p>
-            <Picker />
-        </Route>
-        <Route path="social">
-            <p>{gettext("This is the social support contribution area")}</p>
-            <Picker />
-        </Route>
-        <Route path="/">
-            <p>{gettext("This is the landing page")}</p>
-            <Picker />
-        </Route>
-    </Router>
-</div>
+<Router basepath="/{locale}/contribute" {url}>
+    <Route path="forum" let:location>
+        <Area
+            area={gettext("Support forum")}
+            img={imgHeads}
+            {location}
+            steps={{
+                steps: [
+                    gettext("Find support questions"),
+                    gettext("Start answering!"),
+                    gettext("Learn more about forum contribution"),
+                ],
+                fact: {
+                    number: gettext("1 → 1000"),
+                    text: gettext(
+                        "Solving one issue helps up to 1000 users a day"
+                    ),
+                },
+            }}
+        >
+            <h1>{gettext("Answer questions in the support forum")}</h1>
+            <h2>{gettext("Where all the action happens")}</h2>
+            <p>
+                {gettext(`From hardcore web developers to "how do I install Firefox"
+                    first time users, everybody needs your help in the forum!
+                    Share your knowledge by pointing people to the right help
+                    articles and providing troubleshooting steps for their
+                    individual questions.`)}
+            </p>
+        </Area>
+    </Route>
+    <Route path="kb" let:location>
+        <Area
+            area={gettext("Support articles")}
+            img={imgIntroduction}
+            {location}
+            steps={{
+                steps: [
+                    gettext("Explore the Knowledge Base Dashboard"),
+                    gettext(
+                        "Check the Needs Update column and start editing articles!"
+                    ),
+                    gettext("Learn more about Knowledge Base contribution"),
+                ],
+                fact: {
+                    number: gettext("400 → 70+"),
+                    text: gettext(
+                        "One article can be viewed by 400 million users and translated into 70+ languages"
+                    ),
+                },
+            }}
+        >
+            <h1>{gettext("Help us write help articles")}</h1>
+            <h2>{gettext("Share your wisdom with the world!")}</h2>
+            <p>
+                {gettext(
+                    "If you like writing and teaching then the Knowledge Base is the place for you. We need contributors that can write, edit or proofread articles in English. Thousands of people are accessing our Knowledge Base every week. Imagine how many will be helped by you!"
+                )}
+            </p>
+        </Area>
+    </Route>
+    <Route path="l10n" let:location>
+        <Area
+            area={gettext("Localization")}
+            img={imgFoundation}
+            {location}
+            steps={{
+                steps: [
+                    gettext(
+                        "Check if your locale is available and go to your localization dashboard"
+                    ),
+                    gettext("Start localizing an article!"),
+                    gettext("Learn more about localization contribution"),
+                ],
+                fact: {
+                    number: gettext("400 → 70+"),
+                    text: gettext(
+                        "One article can be viewed by 400 million users and translated into 70+ languages"
+                    ),
+                },
+            }}
+        >
+            <h1>{gettext("Translate help articles")}</h1>
+            <h2>
+                {gettext("Let’s make Mozilla Support speak your language!")}
+            </h2>
+            <p>
+                {gettext(
+                    "Help articles are not available in your language yet? You want to join the amazing team translating them? You will help millions of users in your language. Pretty heroic, right?"
+                )}
+            </p>
+        </Area>
+    </Route>
+    <Route path="social" let:location>
+        <Area
+            area={gettext("Social support")}
+            img={imgDots}
+            {location}
+            steps={{
+                steps: [
+                    gettext("Request access to Conversocial"),
+                    gettext("Start tweeting from @FirefoxSupport!"),
+                    gettext("Learn more about social support contribution"),
+                ],
+                fact: {
+                    number: gettext("1 → 300+"),
+                    text: gettext(
+                        "Help 300+ users who reach out for support on Twitter every month"
+                    ),
+                },
+            }}
+        >
+            <h1>{gettext("Support users on social media")}</h1>
+            <h2>
+                {gettext("Let’s help people so they can keep using Firefox!")}
+            </h2>
+            <p>
+                {gettext(
+                    "Many users ask support questions on social media. With social support, you can help them by reply to their questions from our official account, @FirefoxSupport. Pretty cool, right?"
+                )}
+            </p>
+        </Area>
+    </Route>
+    <Route path="play-store" let:location>
+        <Area
+            area={gettext("Play Store support")}
+            img={imgHands}
+            {location}
+            steps={{
+                steps: [
+                    gettext("Request access to Conversocial"),
+                    gettext(
+                        "Start replying to Play Store reviews for Firefox for Android!"
+                    ),
+                    gettext("Learn more about Play Store contribution"),
+                ],
+                fact: {
+                    number: gettext("1 → 20k+"),
+                    text: gettext(
+                        "Help 20K+ users who leave reviews in Play Store every month"
+                    ),
+                },
+            }}
+        >
+            <h1>{gettext("Support Firefox users on mobile")}</h1>
+            <h2>
+                {gettext(
+                    "Help us respond to Play Store reviews for Firefox for Android!"
+                )}
+            </h2>
+            <p>
+                {gettext(
+                    "Smartphones are taking over the world. We have many users leaving reviews while also asking for support on Play Store. By joining Play Store Support, you can help them by reply to their reviews from the comfort of your desk. Pretty simple, right?"
+                )}
+            </p>
+        </Area>
+    </Route>
+    <Route path="/">
+        <Landing img={imgScreens} />
+    </Route>
+</Router>
 
 <style lang="scss">
+    @use "@mozilla-protocol/core/protocol/css/includes/lib" as p;
     @use "../../kitsune/sumo/static/sumo/scss/config/typography-mixins";
 
-    h1 {
-        @include typography-mixins.text-display-sm;
+    :global(#svelte) {
+        --color: var(--color-dark-gray-10);
+        --header-bg: var(--color-shade-bg);
+        --tile-bg: var(--page-bg);
+        --tile-shadow: 0px 2px 6px rgba(58, 57, 68, 0.2);
+        --step-number-bg: #{p.$color-pink-50};
+        --step-number-color: var(--color-white);
+        --fact-bg: #241541;
+        --fact-number: #{p.$color-pink-50};
+        --fact-color: var(--color-white);
+        --spacing-sm: 16px;
+        --spacing-md: 32px;
+        --spacing-lg: 64px;
+
+        color: var(--color);
+    }
+
+    :global(h1) {
+        @include typography-mixins.text-display-xl(moz);
+    }
+
+    :global(h2) {
+        @include typography-mixins.text-display-sm(moz);
+    }
+
+    :global(p) {
+        @include typography-mixins.text-body-md;
     }
 </style>
---
+++ svelte/contribute/Header.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { Link } from "svelte-navigator";
+
+    export let area = "";
+    export let img;
+</script>
+
+<header>
+    <div class="mzp-l-content">
+        <nav class="breadcrumbs">
+            <ol id="breadcrumbs" class="breadcrumbs--list">
+                <li><a href="/">{gettext("Home")}</a></li>
+                <li>
+                    {#if area}
+                        <Link to="../">{gettext("Contribute")}</Link>
+                    {:else}
+                        {gettext("Contribute")}
+                    {/if}
+                </li>
+                {#if area}
+                    <li>{area}</li>
+                {/if}
+            </ol>
+        </nav>
+
+        <div class="hero">
+            <div class="text"><slot /></div>
+            <img src={img} alt="" />
+        </div>
+    </div>
+</header>
+
+<style lang="scss">
+    @use "@mozilla-protocol/core/protocol/css/includes/lib" as p;
+
+    header {
+        background-color: var(--header-bg);
+        padding-top: 40px;
+    }
+
+    @media #{p.$mq-lg} {
+        :global(.breadcrumbs--list) {
+            margin-bottom: 0;
+        }
+    }
+
+    .hero {
+        display: flex;
+        flex-direction: column-reverse;
+        align-items: center;
+
+        @media #{p.$mq-lg} {
+            flex-direction: row;
+        }
+
+        .text,
+        img {
+            flex: 1;
+        }
+
+        img {
+            height: 500px;
+            object-fit: contain;
+            margin-bottom: var(--spacing-md);
+        }
+    }
+</style>
