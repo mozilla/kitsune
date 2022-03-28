90fe659997626adf0384a09b03fb5c4fd9f3a249
Leo McArdle <lmcardle@mozilla.com>
2022-03-28 15:54:03 +0100

    add pages from figma designs

---
+++ svelte/contribute/Landing.svelte
@@ -0,0 +1,70 @@
+<script>
+    import Header from "./Header";
+    import Picker from "./Picker";
+    import aboutImg from "./img/About Us.png";
+
+    export let img;
+</script>
+
+<Header {img}>
+    <h1>{gettext("Save the world from the comfort of your couch")}</h1>
+
+    <p>
+        {gettext(
+            "Help millions of users get the most out of their favorite browser. Your contributions will reach millions of users worldwide, and you can do it from the comfort of your couch!"
+        )}
+    </p>
+
+    <h2>{gettext("We need your help!")}</h2>
+
+    <p>
+        {gettext(
+            "Used by more than 400 million people, Mozilla support is fully powered by volunteers, and it’s more important than ever. This is where you come in."
+        )}
+    </p>
+</Header>
+
+<Picker />
+
+<section class="mzp-l-content hero">
+    <div class="text">
+        <h2>{gettext("About us")}</h2>
+        <p>
+            {gettext(
+                "Mozilla Support is a community of enthusiastic volunteers and employees trying to support our users around the globe. Join us for an incredible adventure!"
+            )}
+        </p>
+    </div>
+    <img
+        src={aboutImg}
+        alt={gettext("Photo of Mozilla staff and volunteers.")}
+    />
+</section>
+
+<style lang="scss">
+    @use "@mozilla-protocol/core/protocol/css/includes/lib" as p;
+
+    .hero {
+        display: flex;
+        flex-direction: column-reverse;
+        align-items: center;
+        margin-bottom: var(--spacing-lg);
+
+        @media #{p.$mq-lg} {
+            flex-direction: row-reverse;
+        }
+
+        img,
+        .text {
+            flex: 1;
+        }
+
+        .text {
+            margin-top: 24px;
+
+            @media #{p.$mq-lg} {
+                margin-top: 0;
+            }
+        }
+    }
+</style>
---
+++ svelte/contribute/Steps.svelte
@@ -0,0 +1,123 @@
+<script>
+    export let steps = [];
+    export let fact = {};
+</script>
+
+<section class="mzp-l-content">
+    <h2>{gettext("How you can contribute")}</h2>
+
+    <div class="wrapper">
+        <ol>
+            <li>{gettext("Sign up as a volunteer")}</li>
+            <li>{gettext("Learn basic guidelines")}</li>
+            {#each steps as step}
+                <li>{step}</li>
+            {/each}
+        </ol>
+        <div class="fact">
+            <span class="number">{fact.number}</span>
+            <span class="text">{fact.text}</span>
+        </div>
+    </div>
+</section>
+
+<style lang="scss">
+    @use "@mozilla-protocol/core/protocol/css/includes/lib" as p;
+
+    h2 {
+        text-align: center;
+        margin: var(--spacing-lg) 0;
+    }
+
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+
+        @media #{p.$mq-lg} {
+            flex-direction: row;
+        }
+    }
+
+    ol,
+    .fact {
+        flex: 1;
+    }
+
+    ol {
+        margin: 0;
+        counter-reset: step;
+
+        @media #{p.$mq-lg} {
+            margin-right: var(--spacing-md);
+        }
+    }
+
+    li {
+        background: var(--tile-bg);
+        box-shadow: var(--tile-shadow);
+        border-radius: 4px;
+
+        font-family: var(--base-font-family);
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 24px;
+
+        padding: var(--spacing-sm);
+        margin-bottom: var(--spacing-md);
+
+        display: flex;
+        align-items: center;
+
+        @media #{p.$mq-lg} {
+            &:last-of-type {
+                margin-bottom: 0;
+            }
+        }
+
+        &::before {
+            content: counter(step);
+            counter-increment: step;
+
+            width: 48px;
+            height: 48px;
+            margin-right: var(--spacing-sm);
+            flex: none;
+
+            background: var(--step-number-bg);
+            border-radius: 52px;
+
+            line-height: 48px;
+            color: var(--step-number-color);
+
+            text-align: center;
+        }
+    }
+
+    .fact {
+        background: var(--fact-bg);
+        border-radius: 4px;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: var(--spacing-md);
+        font-family: var(--heading-font-family-moz);
+        font-weight: bold;
+
+        .number {
+            color: var(--fact-number);
+            font-size: 72px;
+            line-height: 120%;
+        }
+
+        .text {
+            color: var(--fact-color);
+            font-style: italic;
+            font-weight: 500;
+            font-size: 36px;
+            line-height: 120%;
+            text-align: center;
+        }
+    }
+</style>
