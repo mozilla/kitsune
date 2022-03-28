e5f6d9748e2f5c7390df2043015147766e16e7b9
Leo McArdle <lmcardle@mozilla.com>
2022-03-28 15:54:03 +0100

    add pages from figma designs

---
+++ svelte/contribute/Tile.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { Link } from "svelte-navigator";
+
+    export let to;
+    export let img;
+</script>
+
+<Link {to}>
+    <li class="zoom-on-hover">
+        <img src={img} alt="" />
+        <span><slot /></span>
+    </li>
+</Link>
+
+<style lang="scss">
+    li {
+        height: 100%;
+        background-color: var(--tile-bg);
+        box-shadow: var(--tile-shadow);
+        border-radius: 4px;
+
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+
+        font-size: 20px;
+        text-align: center;
+
+        img {
+            margin: 47px;
+            max-width: 136px;
+        }
+
+        span {
+            margin: var(--spacing-sm);
+        }
+    }
+</style>
