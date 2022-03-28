4b0655d97cfe60358e1bf9887ca3cdfaa210acef
Leo McArdle <lmcardle@mozilla.com>
2022-03-28 15:54:03 +0100

    add pages from figma designs

---
+++ svelte/contribute/Area.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from "./Header";
+    import Steps from "./Steps";
+    import Picker from "./Picker";
+
+    export let area;
+    export let img;
+    export let steps;
+    export let location;
+</script>
+
+<Header {area} {img}>
+    <slot />
+</Header>
+
+<Steps {...steps} />
+
+<Picker {location} />
