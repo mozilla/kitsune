462cfc8b3996c9bb544ec111928d0a84698ade32
Ryan Johnson <rjohnson@mozilla.com>
2022-06-28 14:48:50 -0700

    allow json serializer for claim_watches

--- kitsune/tidings/tasks.py
+++ kitsune/tidings/tasks.py
@@ -1,13 +1,14 @@
 from celery import shared_task
+from django.contrib.auth import get_user_model
 
 from kitsune.tidings.models import Watch
 
 
 @shared_task
-def claim_watches(user):
-    """Attach any anonymous watches having a user's email to that user.
-
-    Call this from your user registration process if you like.
-
+def claim_watches(user_id):
     """
+    Attach any anonymous watches having a user's email to that user.
+    Call this from your user registration process if you like.
+    """
+    user = get_user_model().objects.get(id=user_id)
     Watch.objects.filter(email=user.email).update(email=None, user=user)
