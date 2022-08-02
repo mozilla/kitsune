fc0205126758d2334694d4c18fd1b3603e18e808
Ryan Johnson <rjohnson@mozilla.com>
2022-08-02 16:26:59 -0700

    add pre-rendering of svelte templates to run-bootstrap.sh

--- bin/run-bootstrap.sh
+++ bin/run-bootstrap.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-# install Node dependencies
-npm run development && npm run webpack:build
-# run DB migrations
+# Collect the JavaScript catalog files.
+python manage.py compilejsi18n
+# Install Node dependencies, run the weback build, and pre-render the svelte templates.
+npm run development && npm run webpack:build && npm run webpack:build:pre-render
+# Run the DB migrations.
 python manage.py migrate
