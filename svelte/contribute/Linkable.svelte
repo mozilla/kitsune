150e713fa898b9c6aae75f2771b39bf573b425d6
Tasos Katsoulas <akatsoulas@gmail.com>
2022-09-26 18:33:59 +0300

    Introduce Linkable component

---
+++ svelte/contribute/Linkable.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let link;
+</script>
+
+{#if link}
+    <a href={link}><slot /></a>
+{:else}
+    <slot />
+{/if}
+
+<style lang="scss">
+    @use "@mozilla-protocol/core/protocol/css/includes/lib" as p;
+
+    a,
+    a:visited,
+    a:active {
+        color: var(--color-heading);
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    a:visited {
+        background-color: var(--color-link-active-bg);
+        font-weight: bold;
+    }
+</style>
