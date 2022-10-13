569f2f4b6169d0c0d97de684abc6c64bd8dd773a
smith <sellis@mozilla.com>
2022-10-13 15:54:51 -0400

    [1012] Puente removal (#5207)
    * Remove puente
    
    - add babel.py
    - add merge and extract management commands
    - add Babel to poetry requirements
    - alter settings.TEMPLATES to use NAME instead of fallback
    
    * Puente Removal
    
    * simplify the configuration of the extraction options
    
    * simplify the merge command
    
    * update babel and remove puente
    
    * adjustments to initialization during merge
    
    * eye candy for successful extraction message
    
    Co-authored-by: Ryan Johnson <rjohnson@mozilla.com>

---
+++ kitsune/lib/babel.py
@@ -0,0 +1,68 @@
+import django
+from django.conf import settings
+from jinja2.ext import babel_extract
+
+
+def generate_option(value):
+    """
+    Generate option to pass to babel_extract() from a TEMPLATES['OPTION'] value
+    setting.
+
+    babel_extract() options are meant to be coming from babel config files, so
+    everything is based on strings.
+    """
+    if isinstance(value, bool):
+        return str(value).lower()
+    if isinstance(value, (list, tuple)):
+        return ",".join(value)
+    return value
+
+
+def extract_jinja(fileobj, keywords, comment_tags, options):
+    """
+    Wrapper around jinja2's babel_extract() that sets the relevant options by
+    looking at our django settings.
+
+    This is necessary because jinja2's babel_extract() loads a default
+    environment which doesn't have our extensions and doesn't set
+    the options we need for trimming, so it can't process all our
+    templates and generates a po file that doesn't correspond to our
+    gettext calls because of the whitespace differences.
+    """
+
+    # It looks like this call is required to make sure jinja2 extensions load properly.
+    django.setup()
+
+    # Get the options settings for the Jinja2 templates.
+    jinja2_options = {}
+    for template in settings.TEMPLATES:
+        if template.get("NAME") == "jinja2":
+            jinja2_options = template.get("OPTIONS", {})
+            break
+
+    options.update(
+        {
+            name: generate_option(jinja2_options[name])
+            for name in [
+                "extensions",
+                "block_start_string",
+                "block_end_string",
+                "variable_start_string",
+                "variable_end_string",
+                "comment_start_string",
+                "comment_end_string",
+                "line_statement_prefix",
+                "line_comment_prefix",
+                "trim_blocks",
+                "lstrip_blocks",
+                "keep_trailing_newline",
+            ]
+            if name in jinja2_options
+        }
+    )
+
+    options["trimmed"] = generate_option(
+        jinja2_options.get("policies", {}).get("ext.i18n.trimmed", False)
+    )
+
+    return babel_extract(fileobj, keywords, comment_tags, options)
---
+++ kitsune/sumo/management/commands/merge.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+from babel import Locale, UnknownLocaleError
+from babel.messages.frontend import CommandLineInterface
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+def is_supported_for_init(locale):
+    try:
+        Locale.parse(locale)
+    except UnknownLocaleError:
+        return False
+    return True
+
+
+class Command(BaseCommand):
+    """
+    Merge will update the .po files for all locales based on the .pot files, or
+    if the .po files don't yet exist for a locale, initialize them. It assumes that
+    you've cloned the https://github.com/mozilla-l10n/sumo-l10n repo into the "locale"
+    sub-directory.
+    """
+
+    help = "Updates or initializes the .po files for all locales."
+
+    def handle(self, *args, **options):
+        for locale in settings.SUMO_LANGUAGES:
+            locale = locale.replace("-", "_")
+
+            if locale in ("en_US", "xx"):
+                continue
+
+            if Path(f"locale/{locale}").is_dir():
+                sub_cmd = "update"
+            elif not is_supported_for_init(locale):
+                # NOTE: Babel only supports initializing locales included in the CLDR.
+                self.stdout.write(
+                    self.style.WARNING(f"WARNING: skipping locale {locale} (unsupported for init)")
+                )
+                continue
+            else:
+                sub_cmd = "init"
+
+            CommandLineInterface().run(
+                [
+                    "pybabel",
+                    sub_cmd,
+                    "-d",
+                    "locale/",
+                    "-l",
+                    locale,
+                    "-D",
+                    "django",
+                    "-i",
+                    "locale/templates/LC_MESSAGES/django.pot",
+                ]
+            )
+            CommandLineInterface().run(
+                [
+                    "pybabel",
+                    sub_cmd,
+                    "-d",
+                    "locale/",
+                    "-l",
+                    locale,
+                    "-D",
+                    "djangojs",
+                    "-i",
+                    "locale/templates/LC_MESSAGES/djangojs.pot",
+                ]
+            )
