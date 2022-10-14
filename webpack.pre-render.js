b24db6d65b4bc500a2f379d380f02bd49ab61207
Tasos Katsoulas <akatsoulas@gmail.com>
2022-10-14 11:28:59 +0300

    Fix playstore tile

--- webpack.pre-render.js
+++ webpack.pre-render.js
@@ -25,7 +25,7 @@ module.exports = mergeWithRules({
         "/contribute/kb",
         "/contribute/social",
         "/contribute/l10n",
-        "/contribute/play-store",
+        "/contribute/playstore",
       ],
     }),
   ],
