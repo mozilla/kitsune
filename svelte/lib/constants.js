99a8ea8580f3e62e288c03b482030b154ded195e
Ryan Johnson <rjohnson@mozilla.com>
2022-10-25 13:41:12 -0700

    use relative urls to avoid SUMO_URL

--- svelte/lib/constants.js
+++ svelte/lib/constants.js
@@ -1,7 +1,4 @@
-export const SUMO_URL =
-  process.env["SUMO_URL"] || "https://support.mozilla.org";
 export const TEACHABLE_URL =
   process.env["TEACHABLE_URL"] ||
   "https://mozilla.teachable.com/p/cpg-training-contributors";
-export const GRAPHQL_ENDPOINT =
-  process.env["GRAPHQL_ENDPOINT"] || SUMO_URL + "/graphql";
+export const GRAPHQL_ENDPOINT = process.env["GRAPHQL_ENDPOINT"] || "/graphql";
