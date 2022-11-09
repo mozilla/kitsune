a26b7d4c0cc97e36908cbdf526c1f4883174b0e5
Smith Ellis <sellis@mozilla.com>
2022-11-09 14:50:14 +0000

    Remove leading / from announcements urls

--- kitsune/announcements/urls.py
+++ kitsune/announcements/urls.py
@@ -4,6 +4,6 @@ from kitsune.announcements import views
 
 
 urlpatterns = [
-    re_path(r"^/create/locale$", views.create_for_locale, name="announcements.create_for_locale"),
-    re_path(r"^/(?P<announcement_id>\d+)/delete$", views.delete, name="announcements.delete"),
+    re_path(r"^create/locale$", views.create_for_locale, name="announcements.create_for_locale"),
+    re_path(r"^(?P<announcement_id>\d+)/delete$", views.delete, name="announcements.delete"),
 ]
