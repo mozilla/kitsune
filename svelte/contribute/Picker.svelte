7b8cc94c2f1650b50fc04ee434af77c675112c9a
Ryan Johnson <rjohnson@mozilla.com>
2022-11-10 14:16:57 -0800

    fix gettext extraction from Picker.svelte

--- svelte/contribute/Picker.svelte
+++ svelte/contribute/Picker.svelte
@@ -9,41 +9,51 @@
     import storeImg from "./img/Play Store.svg";
 
     export let location;
+
+    let localized = {
+        "other-ways": gettext("Other ways to contribute"),
+        "pick-a-way": gettext("Pick a way to contribute"),
+        "forum-header": gettext("Answer questions in the support forum"),
+        "kb-header": gettext("Write help articles"),
+        "l10n-header": gettext("Localize support content"),
+        "social-header": gettext("Provide support on social channels"),
+        "store-header": gettext("Respond to mobile store reviews"),
+    };
 </script>
 
 <section class="mzp-l-content">
     <h2>
         {#if location}
-            {gettext("Other ways to contribute")}
+            {localized["other-ways"]}
         {:else}
-            {gettext("Pick a way to contribute")}
+            {localized["pick-a-way"]}
         {/if}
     </h2>
     <nav>
         <ul>
             {#if location?.pathname != "/forum"}
                 <Tile to="../forum" img={forumImg}>
-                    {gettext("Answer questions in the support forum")}
+                    {localized["forum-header"]}
                 </Tile>
             {/if}
             {#if location?.pathname != "/kb"}
                 <Tile to="../kb" img={kbImg}>
-                    {gettext("Write help articles")}
+                    {localized["kb-header"]}
                 </Tile>
             {/if}
             {#if location?.pathname != "/l10n"}
                 <Tile to="../l10n" img={l10nImg}>
-                    {gettext("Localize support content")}
+                    {localized["l10n-header"]}
                 </Tile>
             {/if}
             {#if location?.pathname != "/social"}
                 <Tile to="../social" img={socialImg}>
-                    {gettext("Provide support on social channels")}
+                    {localized["social-header"]}
                 </Tile>
             {/if}
             {#if location?.pathname != "/store"}
                 <Tile to="../store" img={storeImg}>
-                    {gettext("Respond to mobile store reviews")}
+                    {localized["store-header"]}
                 </Tile>
             {/if}
         </ul>
