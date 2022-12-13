2c9f4898709a8b187e38432647dec40f96919582
Tasos Katsoulas <akatsoulas@gmail.com>
2022-12-13 10:55:35 +0200

    Update wsgi config

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -5,4 +5,4 @@ echo "$GIT_SHA" > static/revision.txt
 # https://stackoverflow.com/questions/25737589/gunicorn-doesnt-log-real-ip-from-nginx
 GUNICORN_LOGFORMAT='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({x-forwarded-for}i)s'
 
-exec gunicorn wsgi.app --access-logformat "${GUNICORN_LOGFORMAT}" --config wsgi/config.py
+exec gunicorn --access-logformat "${GUNICORN_LOGFORMAT}" --config wsgi/config.py
