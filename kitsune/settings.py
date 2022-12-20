9baabb9ff634bf6944cc18cadac33e4538c52d21
Tasos Katsoulas <akatsoulas@gmail.com>
2022-12-20 18:55:59 +0200

    allow inline css

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -1214,6 +1214,7 @@ CSP_FONT_SRC = (
 
 CSP_STYLE_SRC = (
     "'self'",
+    "'unsafe-inline'",
     "https://*.itsre-sumo.mozilla.net",
     "https://*.webservices.mozgcp.net",
     "https://*.jsdelivr.net",
