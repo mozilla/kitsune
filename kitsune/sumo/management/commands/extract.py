ed5f6985fbdc0fd0ba1af1161fa48ff8ca30c3fa
Ryan Johnson <rjohnson@mozilla.com>
2023-05-18 17:50:53 -0700

    better reporting

--- kitsune/sumo/management/commands/extract.py
+++ kitsune/sumo/management/commands/extract.py
@@ -1,6 +1,7 @@
+import glob
 import subprocess
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from babel.messages.frontend import CommandLineInterface
 
 
@@ -37,7 +38,7 @@ class Command(BaseCommand):
                 ".",
             ]
         )
-        # First run the Babel-based extraction on the JS-based Svelte and Nunjucks files.
+        # First run the Babel-based extraction on the Svelte and Nunjucks files.
         CommandLineInterface().run(
             [
                 "pybabel",
@@ -62,29 +63,36 @@ class Command(BaseCommand):
             ]
         )
         # Finally run the extraction on the JS files themselves. This will add to the
-        # djangojs.pot file created by the previous command due to the "--join-existing"
-        # option.
-        subprocess.run(
-            " ".join(
-                [
-                    "xgettext",
-                    "--join-existing",
-                    "--sort-by-file",
-                    "--copyright-holder=Mozilla",
-                    "--package-name=kitsune",
-                    "--package-version=1.0",
-                    "--msgid-bugs-address='https://github.com/mozilla-l10n/sumo-l10n/issues'",
-                    "--output=locale/templates/LC_MESSAGES/djangojs.pot",
-                    "--width=80",
-                    "--add-comments='L10n,L10n:'",
-                    "--keyword='_lazy'",
-                    "--keyword='pgettext_lazy:1c,2'",
-                    "--from-code=utf-8",
-                    "kitsune/**/static/**/js/*.js",
-                ]
-            ),
-            shell=True,
-            check=True,
-        )
+        # existing djangojs.pot file created by the previous command (because we're
+        # using the "--join-existing" option).
+        # NOTE: This loop is for reporting purposes only. The "--verbose" option of
+        #       "xgettext" doesn't seem to work.
+        for fname in glob.glob("kitsune/**/static/**/js/*.js"):
+            self.stdout.write(f"extracting messages from {fname}")
+            try:
+                subprocess.run(
+                    [
+                        "xgettext",
+                        "--join-existing",
+                        "--sort-by-file",
+                        "--copyright-holder=Mozilla",
+                        "--package-name=kitsune",
+                        "--package-version=1.0",
+                        "--msgid-bugs-address=https://github.com/mozilla-l10n/sumo-l10n/issues",
+                        "--output=locale/templates/LC_MESSAGES/djangojs.pot",
+                        "--width=80",
+                        "--add-comments=L10n,L10n:",
+                        "--keyword=_lazy",
+                        "--keyword=pgettext_lazy:1c,2",
+                        "--from-code=utf-8",
+                        fname,
+                    ],
+                    text=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                    check=True,
+                )
+            except subprocess.CalledProcessError as err:
+                raise CommandError(err.output)
 
         self.stdout.write(self.style.SUCCESS("extraction complete"))
