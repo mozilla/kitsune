5222d4f92684638621e5796a70e2dfcfa951ed96
Ryan Johnson <rjohnson@mozilla.com>
2023-07-13 16:57:03 -0700

    disable introspection and add tests

--- kitsune/schema.py
+++ kitsune/schema.py
@@ -1,15 +1,6 @@
 import graphene
 
-from kitsune.graphql import query as kitsune_query
-
-
-class Query(kitsune_query.Query, graphene.ObjectType):
-    """Top level Query.
-
-    Inherits from Query classes in other apps.
-    """
-
-    ...
+from kitsune.graphql.query import Query
 
 
 schema = graphene.Schema(query=Query)
