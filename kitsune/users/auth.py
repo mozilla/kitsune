6a770523fdf1c9bdedf31418d219ec33246229df
Ryan Johnson <rjohnson@mozilla.com>
2023-08-10 13:46:13 -0700

    ignore email when falsy in FXAAuthBackend.update_user

--- kitsune/users/auth.py
+++ kitsune/users/auth.py
@@ -214,7 +214,7 @@ class FXAAuthBackend(OIDCAuthenticationBackend):
 
         # There is a change in the email in Firefox Accounts. Let's update user's email
         # unless we have a superuser
-        if user.email != email and not user.is_staff:
+        if email and (email != user.email) and not user.is_staff:
             if User.objects.exclude(id=user.id).filter(email=email).exists():
                 if request:
                     msg = _(
