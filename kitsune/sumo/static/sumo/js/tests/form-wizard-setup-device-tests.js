5aa02800c33ef219e2227b9c839228b2dd26e16d
Mike Conley <mike.d.conley@gmail.com>
2023-10-13 13:25:37 -0400

    Add an email field and calendar reminder button to Step 3 of the switching-devices wizard.
    Fixes mozilla/sumo#1505.

--- kitsune/sumo/static/sumo/js/tests/form-wizard-setup-device-tests.js
+++ kitsune/sumo/static/sumo/js/tests/form-wizard-setup-device-tests.js
@@ -1,10 +1,37 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import { SetupDeviceStep } from "sumo/js/form-wizard-setup-device-step";
+import { ReminderDialog } from "sumo/js/form-wizard-reminder-dialog";
+
+function assertFormElements(form, expectedElements) {
+  let expectations = Object.assign({}, expectedElements);
+
+  for (let element of form.elements) {
+    if (element.type == "submit") {
+      continue;
+    }
+
+    expect(expectations[element.name], `Checking ${element.name}`).to.exist;
+    expect(element.type).to.equal(expectations[element.name].type);
+    expect(element.disabled).to.equal(expectations[element.name].disabled);
+    expect(element.value).to.equal(expectations[element.name].value);
+    delete expectations[element.name];
+  }
+
+  expect(Object.keys(expectations).length).to.equal(0);
+}
 
 describe("setup-device-step custom element", () => {
   const DOWNLOAD_LINK = "https://mzl.la/newdevice"
   let step;
+
+  before(() => {
+    ReminderDialog.prototype.showModal = () => {};
+  });
+
+  after(() => {
+    delete ReminderDialog.prototype.showModal;
+  })
 
   beforeEach(() => {
     step = new SetupDeviceStep();
@@ -16,27 +43,99 @@ describe("setup-device-step custom element", () => {
     expect(step).to.be.an.instanceof(SetupDeviceStep);
   });
 
-  it("should display a link to download Firefox on a new device", () => {
-    let downloadLink = step.shadowRoot.getElementById("download-link");
-    expect(downloadLink).to.exist;
-    expect(downloadLink.textContent).to.equal(DOWNLOAD_LINK);
-    expect(downloadLink.href).to.equal(DOWNLOAD_LINK);
+  it("should offer a button to open the calendar reminder dialog", async () => {
+    let openDialogBtn = step.shadowRoot.querySelector("#open-reminder-dialog-button");
+    openDialogBtn.click();
+
+    expect(document.querySelector(".reminder-dialog")).to.exist;
   });
 
-  it("should contain a button that allows for copying the download link", () => {
-    let writeTextSpy = sinon.spy();
-    // The clipboard property is not provided by JSDom.
-    Object.defineProperty(navigator, "clipboard", {
-      value: {
-        writeText: writeTextSpy,
-      },
-      configurable: true,
+  describe("form valdiation", () => {
+    let form;
+    let emailErrorMessage;
+    let emailField;
+    let submitBtn;
+    let otherBtn;
+
+    beforeEach(() => {
+      form = step.shadowRoot.querySelector("form");
+      emailErrorMessage = form.querySelector("#email-error");
+      emailField = form.querySelector("input[name=email]");
+      submitBtn = form.querySelector("#submit");
+      otherBtn = step.shadowRoot.querySelector("#open-reminder-dialog-button");
     });
 
-    let copyButton = step.shadowRoot.getElementById("copy-button");
-    expect(copyButton).to.exist;
-    copyButton.click();
-    expect(writeTextSpy.calledOnceWith(DOWNLOAD_LINK)).to.be.true;
-    delete navigator.clipboard;
-  })
+    it("should have a form that can register for a one-time newsletter", () => {
+      assertFormElements(form, {
+        newsletters: { type: "hidden", value: "download-firefox-desktop-migration", disabled: false },
+        "source-url": { type: "hidden", value: window.location.href, disabled: false },
+        lang: { type: "hidden", value: navigator.language, disabled: false },
+        email: { type: "email", value: "", disabled: false },
+      });
+    });
+
+    it("should display an error message if an incomplete email address is supplied", () => {
+      expect([...emailErrorMessage.classList]).to.not.include("visible");
+
+      emailField.value = "this-is-not-an-email-address";
+      emailField.dispatchEvent(new CustomEvent("input", { bubbles: true }));
+      emailField.dispatchEvent(new CustomEvent("blur", { bubbles: true }));
+      expect(submitBtn.disabled).to.be.true;
+
+      expect(emailField.validity.valid).to.be.false;
+      expect([...emailErrorMessage.classList]).to.include("visible");
+      expect(emailErrorMessage.getAttribute("error-type")).to.equal("invalid-email");
+    });
+
+    it("should allow a valid email address to be submitted", async () => {
+      expect([...emailErrorMessage.classList]).to.not.include("visible");
+
+      let preventSubmitListener = new Promise((resolve) => {
+        form.addEventListener(
+          "submit",
+          (e) => {
+            e.preventDefault();
+            resolve();
+          },
+          { once: true }
+        );
+      });
+
+      emailField.value = "example@example.com";
+      emailField.dispatchEvent(new CustomEvent("input", { bubbles: true }));
+      emailField.dispatchEvent(new CustomEvent("blur", { bubbles: true }));
+      expect(submitBtn.disabled).to.be.false;
+
+      expect(emailField.validity.valid).to.be.true;
+      expect([...emailErrorMessage.classList]).to.not.include("visible");
+      submitBtn.click();
+      await preventSubmitListener;
+    });
+
+    it("should clear the success state if the email input is modified", async () => {
+      submitBtn.toggleAttribute("success", true);
+
+      emailField.value = "example@example.com";
+      emailField.dispatchEvent(new CustomEvent("input", { bubbles: true }));
+      emailField.dispatchEvent(new CustomEvent("blur", { bubbles: true }));
+      expect(submitBtn.hasAttribute("success")).to.be.false;
+    });
+
+    it("should hide the error message if the reminder dialog is opened", async () => {
+      emailField.value = "this-is-not-an-email-address";
+      emailField.dispatchEvent(new CustomEvent("input", { bubbles: true }));
+      emailField.dispatchEvent(new CustomEvent("blur", { bubbles: true }));
+      expect([...emailErrorMessage.classList]).to.include("visible");
+
+      ReminderDialog.prototype.showModal = () => {};
+
+      let openDialogBtn = step.shadowRoot.querySelector("#open-reminder-dialog-button");
+      openDialogBtn.click();
+      expect([...emailErrorMessage.classList]).to.not.include("visible");
+
+      expect(document.querySelector(".reminder-dialog")).to.exist;
+
+      delete ReminderDialog.prototype.showModal;
+    });
+  });
 });
