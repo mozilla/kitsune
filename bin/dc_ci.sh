4ae5c7f87681b2800c518694f29469ecc37159e8
Tasos Katsoulas <akatsoulas@gmail.com>
2023-12-28 16:37:52 +0200

    Introduce docker compose watch.
    * Drop web volume in favor of watch
    * Update Makefile
    * Rename docker-compose.yml to compose.yaml

--- bin/dc_ci.sh
+++ bin/dc_ci.sh
@@ -3,4 +3,4 @@
 source docker/bin/set_git_env_vars.sh
 export DOCKER_BUILDKIT=1
 
-docker-compose -f docker-compose.yml -f docker/docker-compose.ci.yml "$@"
+docker-compose -f compose.yaml -f docker/compose.ci.yaml "$@"
