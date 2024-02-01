aab4569b5b239782790f0a03d49ae9e6dff14963
Ryan Johnson <rjohnson@mozilla.com>
2024-02-01 08:00:13 -0800

    adjust UI messages

--- kitsune/sumo/static/sumo/js/wiki.editor.js
+++ kitsune/sumo/static/sumo/js/wiki.editor.js
@@ -3,6 +3,6 @@ import "@selectize/selectize";
 (function($) {
   // Improve the interface for restricting articles.
   $("select[id='id_restrict_to_groups']").selectize({
-    placeholder: "No group(s) selected ",
+    placeholder: "Restrict visibility to selected group(s)   ",
   });
 })(jQuery);
