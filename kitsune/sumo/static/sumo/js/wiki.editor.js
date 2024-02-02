035a40aa48d0efcf636555a11266180ab1c789da
Ryan Johnson <escattone@gmail.com>
2024-02-01 16:12:50 -0800

    remove flicker in restriction select (#5867)

--- kitsune/sumo/static/sumo/js/wiki.editor.js
+++ kitsune/sumo/static/sumo/js/wiki.editor.js
@@ -2,7 +2,9 @@ import "@selectize/selectize";
 
 (function($) {
   // Improve the interface for restricting articles.
-  $("select[id='id_restrict_to_groups']").selectize({
-    placeholder: "Restrict visibility to selected group(s)   ",
+  $(function () {
+    $("select[id='id_restrict_to_groups']").selectize({
+      placeholder: "Restrict visibility to selected group(s)   ",
+    });
   });
 })(jQuery);
