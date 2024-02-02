8333dff629fdd4d2d61a761f8ca115e065b663dc
Ryan Johnson <rjohnson@mozilla.com>
2024-02-02 14:31:06 -0800

    add remove and clear buttons to article restriction selector

--- kitsune/sumo/static/sumo/js/wiki.editor.js
+++ kitsune/sumo/static/sumo/js/wiki.editor.js
@@ -4,6 +4,8 @@ import "@selectize/selectize";
   // Improve the interface for restricting articles.
   $(function () {
     $("select[id='id_restrict_to_groups']").selectize({
+      closeAfterSelect: true,
+      plugins: ["clear_button", "remove_button"],
       placeholder: "Restrict visibility to selected group(s)   ",
     });
   });
