8bf56624c1bc558262cce1071d70692eadd8461a
Smith Ellis <sellis@mozilla.com>
2024-03-28 23:10:36 -0400

    comment code and update var names

--- kitsune/messages/utils.py
+++ kitsune/messages/utils.py
@@ -9,22 +9,29 @@ def send_message(to, to_group=None, text=None, sender=None):
     """Send a private message.
 
     :arg to: a list of Users to send the message to
-    :arg to_groups:Groups to send the message to
+    :arg to_groups: Groups to send the message to
     :arg sender: the User who is sending the message
     :arg text: the message text
     """
-    if sender:
-        obm = OutboxMessage.objects.create(sender=sender, message=text)
-        obm.to.set(to)
+    if not sender or not text:
+        return
+
+    # Assuming OutboxMessage doesn't necessarily need to be created for every recipient
+    if to:
+        outbox_message = OutboxMessage.objects.create(sender=sender, message=text)
+        outbox_message.to.set(to)
         if to_group:
-            obm.to_group.set(to_group)
-    for obj in to:
-        if isinstance(obj, User):
-            im = InboxMessage.objects.create(sender=sender, to=obj, message=text)
+            outbox_message.to_group.set(to_group)
+
+    for recipient in to:
+        if isinstance(recipient, User):
+            inbox_message = InboxMessage.objects.create(sender=sender, to=recipient, message=text)
+            # If we had a user, and we made them an inbox message,
+            # we should also add the to_groups to their message as well
             if to_group:
-                im.to_group.set(to_group)
-            if Setting.get_for_user(obj, "email_private_messages"):
-                email_private_message(inbox_message_id=im.id)
+                inbox_message.to_group.set(to_group)
+            if Setting.get_for_user(recipient, "email_private_messages"):
+                email_private_message(inbox_message_id=inbox_message.id)
 
     message_sent.send(sender=InboxMessage, to=to, to_group=to_group, text=text, msg_sender=sender)
 
