14d17311fea47fc0a4cf2cf80e794a1e255d49cd
Smith Ellis <sellis@mozilla.com>
2024-04-19 12:31:08 -0400

    fix utils

--- kitsune/messages/utils.py
+++ kitsune/messages/utils.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import Group
 from kitsune.messages.models import InboxMessage, OutboxMessage
 from kitsune.users.models import User
 from kitsune.messages.signals import message_sent
@@ -18,32 +17,30 @@ def send_message(to, text=None, sender=None):
     if not sender or not text or not to:
         return
     # User pks from users in To field
-    usernames = []
-    group_names = []
+    users = []
+    groups = []
 
     for obj in to:
         if isinstance(obj, User):
-            usernames.append(obj)
+            users.append(obj)
         else:
-            group_names.append(obj)
+            groups.append(obj)
 
-    groups_qs = Group.objects.filter(name__in=group_names)
-    users_qs = User.objects.filter(username__in=usernames).values_list("id", flat=True)
+    # User pks from to field users
+    to_users = set(User.objects.filter(username__in=users).values_list("id", flat=True))
 
     # Group users' pks only - could be a lot of them
-    group_users_qs = User.objects.filter(groups__in=groups_qs).values_list("id", flat=True)
+    group_users = set(User.objects.filter(groups__in=groups).values_list("id", flat=True))
 
     # Resolve all unique user ids, including those in groups
-    # but keep up with users from groups
-    to_users = set(users_qs)
-    group_users = set(group_users_qs)
+    # We need to keep group_users separate for email notifications
     all_recipients_of_message = to_users.union(group_users)
 
     # Create the outbox message
     outbox_message = OutboxMessage.objects.create(sender=sender, message=text)
     to_users = User.objects.filter(id__in=to_users)
     outbox_message.to.set(to_users)
-    outbox_message.to_group.set(groups_qs)
+    outbox_message.to_group.set(groups)
 
     # Fetch settings for email notifications in one go
     # For now, we only send emails to users who were individually
@@ -60,13 +57,14 @@ def send_message(to, text=None, sender=None):
         for user_id in all_recipients_of_message
     ]
 
-    InboxMessage.objects.bulk_create(inbox_messages)
+    recipient_id_to_message_id = {
+        msg.to_id: msg.id for msg in InboxMessage.objects.bulk_create(inbox_messages)
+    }
 
     # Assuming messages are fetched again with IDs if necessary
     for user_id in users_to_email:
-        inbox_messages = InboxMessage.objects.filter(sender=sender, to__id=user_id, message=text)
-        for message in inbox_messages:
-            email_private_message.delay(inbox_message_id=message.id)
+        if message_id := recipient_id_to_message_id.get(user_id):
+            email_private_message.delay(inbox_message_id=message_id)
 
     # Send a signal if needed
     message_sent.send(sender=InboxMessage, to=to, text=text, msg_sender=sender)
