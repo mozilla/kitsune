2b9d43842bf406676b4cec45a220037ee67530a3
Tasos Katsoulas <akatsoulas@gmail.com>
2024-05-15 13:12:28 +0300

    Viewport centered upload modal

--- kitsune/sumo/static/sumo/js/kbox.js
+++ kitsune/sumo/static/sumo/js/kbox.js
@@ -75,7 +75,7 @@ var TEMPLATE = (
   '<div class="kbox-wrap"><div class="kbox-placeholder"/></div>' +
   '</div>'
 ),
-OVERLAY = '<div id="kbox-overlay"></div>';
+  OVERLAY = '<div id="kbox-overlay"></div>';
 
 // The KBox type
 export default function KBox(el, options) {
@@ -83,7 +83,7 @@ export default function KBox(el, options) {
 }
 
 KBox.prototype = {
-  init: function(el, options) {
+  init: function (el, options) {
     var self = this;
     self.el = el;
     self.html = typeof el === 'string' && el;
@@ -92,7 +92,7 @@ KBox.prototype = {
       // defaults
       clickTarget: self.$el.data('target'),
       closeOnEsc: self.$el.data('close-on-esc') === undefined ?
-      true : !!self.$el.data('close-on-esc'),
+        true : !!self.$el.data('close-on-esc'),
       closeOnOutClick: !!self.$el.data('close-on-out-click'),
       container: self.html && $('body'),
       // TODO: maxHeight: self.$el.data('max-height') || 'window',
@@ -118,21 +118,21 @@ KBox.prototype = {
 
     // If we have a click target, open the kbox when it is clicked.
     if (self.$clickTarget) {
-      self.$clickTarget.on("click", function(ev) {
+      self.$clickTarget.on("click", function (ev) {
         ev.preventDefault();
         self.open();
       });
     }
 
   },
-  updateOptions: function(options) {
+  updateOptions: function (options) {
     // Ability to update options programmatically after kbox creation.
     var self = this;
     self.options = $.extend(self.options, options);
     self.$clickTarget = options.clickTarget && $(options.clickTarget);
     self.$container = options.container && $(options.container);
   },
-  render: function() {
+  render: function () {
     var self = this;
     self.$kbox = $(self.options.template);
 
@@ -163,14 +163,14 @@ KBox.prototype = {
     self.$kbox.find('.kbox-placeholder').replaceWith(self.$el.detach());
 
     // Handle close events
-    self.$kbox.on('click', '.kbox-close, .kbox-cancel', function(ev) {
+    self.$kbox.on('click', '.kbox-close, .kbox-cancel', function (ev) {
       ev.preventDefault();
       self.close();
     });
 
     self.rendered = true;
   },
-  open: function() {
+  open: function () {
     var self = this;
     if (self.options.preOpen && !self.options.preOpen.call(self)) {
       // If we have a preOpen callback and it returns false,
@@ -192,7 +192,7 @@ KBox.prototype = {
 
     // Handle ESC
     if (self.options.closeOnEsc) {
-      self.keypressHandler = function(ev) {
+      self.keypressHandler = function (ev) {
         if (ev.keyCode === 27) {
           self.close();
         }
@@ -202,18 +202,18 @@ KBox.prototype = {
 
     // Handle outside clicks
     if (self.options.closeOnOutClick) {
-      self.clickHandler = function(ev) {
+      self.clickHandler = function (ev) {
         if ($(ev.target).closest('.kbox-container').length === 0) {
           // The click isn't inside the kbox, so lets close it.
           self.close();
         }
       };
-      setTimeout(function() { // so it doesn't get triggered on this click
+      setTimeout(function () { // so it doesn't get triggered on this click
         $('body').on("click", self.clickHandler);
       }, 0);
     }
   },
-  setPosition: function(position) {
+  setPosition: function (position) {
     var self = this,
       toX, toY, $parent, parentOffset, minX, minY, scrollL, scrollT;
     if (!position) {
@@ -223,29 +223,21 @@ KBox.prototype = {
       return;
     }
     if (position === 'center') {
-      $parent = self.$kbox.offsetParent();
-      parentOffset = $parent.offset();
-      scrollL = $(window).scrollLeft();
-      scrollT = $(window).scrollTop();
-      minX = -parentOffset.left + scrollL;
-      minY = -parentOffset.top + scrollT;
-      toX = ($(window).width() - self.$kbox.outerWidth()) / 2 - parentOffset.left + scrollL;
-      toY = ($(window).height() - self.$kbox.outerHeight()) / 2 - parentOffset.top + scrollT;
-      if (toX < minX) {
-        toX = minX;
-      }
-      if (toY < minY) {
-        toY = minY;
-      }
+      let windowWidth = $(window).width();
+      let windowHeight = $(window).height();
+      let modalWidth = self.$kbox.outerWidth();
+      let modalHeight = self.$kbox.outerHeight();
+
       self.$kbox.css({
-        'left': toX,
-        'top': toY,
+        'left': (windowWidth - modalWidth) / 2,
+        'top': (windowHeight - modalHeight) / 2,
         'right': 'inherit',
-        'bottom': 'inherit'
+        'bottom': 'inherit',
+        'position': 'fixed'
       });
     }
   },
-  close: function() {
+  close: function () {
     var self = this;
     if (self.options.preClose && !self.options.preClose.call(self)) {
       // If we have a preClose callback and it returns false,
@@ -270,7 +262,7 @@ KBox.prototype = {
       $('body').off('click', self.clickHandler);
     }
   },
-  destroy: function() {
+  destroy: function () {
     // return DOM to how it was originally, if possible.
     var self = this;
     if (self.$container && self.$ph) {
@@ -278,12 +270,12 @@ KBox.prototype = {
     }
     self.$kbox.remove();
   },
-  createOverlay: function() {
+  createOverlay: function () {
     var self = this;
     self.$overlay = $(OVERLAY);
     self.$kbox.before(self.$overlay);
   },
-  destroyOverlay: function() {
+  destroyOverlay: function () {
     if (this.$overlay) {
       this.$overlay.remove();
       delete this.$overlay;
@@ -292,8 +284,8 @@ KBox.prototype = {
 };
 
 // Create the jQuery plugin.
-$.fn.kbox = function(options) {
-  return this.each(function() {
+$.fn.kbox = function (options) {
+  return this.each(function () {
     new KBox(this, options);
   });
 };
