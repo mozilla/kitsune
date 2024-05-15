ac983396e9a64ee66d2c99db2cfd5d53ad2cb03b
Tasos Katsoulas <akatsoulas@gmail.com>
2024-05-15 18:03:17 +0300

    MVP list all topics across products.

--- kitsune/products/urls.py
+++ kitsune/products/urls.py
@@ -3,7 +3,9 @@ from django.urls import re_path
 from kitsune.products import views
 
 urlpatterns = [
-    re_path(r"^$", views.product_list, name="products"),
+    re_path(r"^products/", views.product_list, name="products"),
+    re_path(r"^topics$", views.topic_list, name="products.topics"),
+    re_path(r"^topics/(?P<product_slug>[^/]+)$", views.topic_list, name="products.topics"),
     re_path(r"^(?P<slug>[^/]+)$", views.product_landing, name="products.product"),
     re_path(
         r"^(?P<product_slug>[^/]+)/(?P<topic_slug>[^/]+)$",
