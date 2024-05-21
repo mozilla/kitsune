a5b7b9e90f2fdf0969505daf2660decd96080591
Tasos Katsoulas <akatsoulas@gmail.com>
2024-05-21 13:41:03 +0300

    Navigate per topic in KB and AAQ

--- kitsune/sumo/static/sumo/js/topics.js
+++ kitsune/sumo/static/sumo/js/topics.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var dropdown = document.getElementById('products-dropdown');
+    var dropdown = document.getElementById('products-topics-dropdown');
     dropdown.addEventListener('change', function () {
         var selectedUrl = this.value;
         if (selectedUrl) {
