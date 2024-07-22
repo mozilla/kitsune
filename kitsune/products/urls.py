131d4bb03742881bc864a17ad39c3abeec372eb0
Smith Ellis <sellis@mozilla.com>
2024-07-22 09:48:01 -0400

    back to re_path

--- kitsune/products/urls.py
+++ kitsune/products/urls.py
@@ -1,29 +1,33 @@
-from django.urls import include, path
+from django.urls import include, re_path
 
 from kitsune.products import views
 from kitsune.sumo.decorators import prefer_cms
 
 product_patterns = [
-    path("", views.product_list, name="products"),
-    path("<slug>/", prefer_cms(views.product_landing), name="products.product"),
-    path("<product_slug>/<topic_slug>/", views.document_listing, name="products.documents"),
-    path(
-        "<product_slug>/<topic_slug>/<subtopic_slug>/",
+    re_path(r"^$", views.product_list, name="products"),
+    re_path(r"^(?P<slug>[^/]+)$", prefer_cms(views.product_landing), name="products.product"),
+    re_path(
+        r"^(?P<product_slug>[^/]+)/(?P<topic_slug>[^/]+)$",
+        views.document_listing,
+        name="products.documents",
+    ),
+    re_path(
+        r"^(?P<product_slug>[^/]+)/(?P<topic_slug>[^/]+)/" r"(?P<subtopic_slug>[^/]+)$",
         views.document_listing,
         name="products.subtopics",
     ),
 ]
 
 topic_patterns = [
-    path("<topic_slug>/", views.document_listing, name="products.topic_documents"),
-    path(
-        "<topic_slug>/<slug>/",
+    re_path(r"^(?P<topic_slug>[^/]+)$", views.document_listing, name="products.topic_documents"),
+    re_path(
+        r"(?P<topic_slug>[^/]+)/(?P<product_slug>[^/]+)$",
         views.document_listing,
         name="products.topic_product_documents",
     ),
 ]
 
 urlpatterns = [
-    path("products/", include(product_patterns)),
-    path("topics/", include(topic_patterns)),
+    re_path(r"^products/", include(product_patterns)),
+    re_path(r"^topics/", include(topic_patterns)),
 ]
