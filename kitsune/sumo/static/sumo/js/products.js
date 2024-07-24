babb6b151e2a5f3e432768b846d2fb47259da141
Ryan Johnson <rjohnson@mozilla.com>
2024-07-24 09:19:12 -0700

    fix products.js and convert to vanilla JS

--- kitsune/sumo/static/sumo/js/products.js
+++ kitsune/sumo/static/sumo/js/products.js
@@ -1,17 +1,22 @@
 import UITour from "./libs/uitour";
 import compareVersions from "./compare_versions";
 
-(function($) {
-  'use strict';
+// The "DOMContentLoaded" event is guaranteed not to have been
+// called by the time the following code is run, because it always
+// waits until all deferred scripts have been loaded, and the code
+// in this file is always bundled into a script that is deferred.
+document.addEventListener("DOMContentLoaded", () => {
+  let downloadButton = document.querySelector(".download-firefox .download-button");
 
-  $(function() {
-    var latestVersion = $('.download-firefox .download-button').data('latest-version');
-
-    UITour.getConfiguration('appinfo', function(info) {
-      if (compareVersions(info.version, latestVersion) === 0) {
-        $('.refresh-firefox').show();
-        $('.download-firefox').hide();
-      }
-    });
-  });
-})(jQuery);
+  if (downloadButton) {
+    let latestVersion = downloadButton.dataset.latestVersion;
+    if (latestVersion) {
+      UITour.getConfiguration("appinfo", function(info) {
+        if (compareVersions(info.version, latestVersion) === 0) {
+          document.querySelector(".refresh-firefox").hidden = false;
+          document.querySelector(".download-firefox").hidden = true;
+        }
+      });
+    }
+  }
+});
