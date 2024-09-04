6f8798e249fe026218446a4976528fe92fcb7d11
Tasos Katsoulas <akatsoulas@gmail.com>
2024-09-04 12:15:33 +0300

    Remove obsolete code for win7

--- kitsune/urls.py
+++ kitsune/urls.py
@@ -48,7 +48,6 @@ urlpatterns = i18n_patterns(
     path("", include("kitsune.tidings.urls")),
     path("", include("kitsune.users.urls")),
     path("locales", sumo_views.locales, name="sumo.locales"),
-    re_path(r"^windows7-support(?:\\/)?$", RedirectView.as_view(url="/home/?as=u")),
     re_path(
         rf"wagtail/{serve_pattern.lstrip('^')}",
         waffle_flag("wagtail_experiments")(wagtail_serve),
