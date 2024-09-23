7762a1e9dff93c174f135d4f058ed7c95c76df5e
Emil Ghitta <eghitta@mozilla.com>
2024-09-23 11:42:14 +0300

    - Mark the group page functions as public. - Replace super() with self in group_page class. - Use composition instead of inheritance for the user_group_flow.py

--- playwright_tests/pages/contribute/groups_page.py
+++ playwright_tests/pages/contribute/groups_page.py
@@ -14,33 +14,33 @@ class GroupsPage(BasePage):
         super().__init__(page)
 
     # Add Group member
-    def _get_user_added_successfully_message(self) -> str:
-        return super()._get_text_of_element(self.__user_added_notification)
+    def get_user_added_successfully_message(self) -> str:
+        return self._get_text_of_element(self.__user_added_notification)
 
-    def _get_pm_group_members_button(self) -> Locator:
-        return super()._get_element_locator(self.__private_message_group_members_button)
+    def get_pm_group_members_button(self) -> Locator:
+        return self._get_element_locator(self.__private_message_group_members_button)
 
-    def _click_on_a_particular_group(self, group_name):
-        super()._click(f"//a[text()='{group_name}']")
+    def click_on_a_particular_group(self, group_name):
+        self._click(f"//a[text()='{group_name}']")
 
-    def _click_on_pm_group_members_button(self):
-        super()._click(self.__private_message_group_members_button)
+    def click_on_pm_group_members_button(self):
+        self._click(self.__private_message_group_members_button)
 
-    def _click_on_edit_group_members(self):
-        super()._click(self.__edit_group_members_option)
+    def click_on_edit_group_members(self):
+        self._click(self.__edit_group_members_option)
 
-    def _click_on_remove_a_user_from_group_button(self, username: str):
-        super()._click(f"//div[@class='info']/a[text()='{username}']/../..//a"
-                       f"[@title='Remove user from group']")
+    def click_on_remove_a_user_from_group_button(self, username: str):
+        self._click(f"//div[@class='info']/a[text()='{username}']/../..//a"
+                    f"[@title='Remove user from group']")
 
-    def _click_on_remove_member_confirmation_button(self):
-        super()._click(self.__remove_user_from_group_confirmation_button)
+    def click_on_remove_member_confirmation_button(self):
+        self._click(self.__remove_user_from_group_confirmation_button)
 
-    def _type_into_add_member_field(self, text: str):
-        super()._type(self.__add_group_member_field, text, delay=0)
+    def type_into_add_member_field(self, text: str):
+        self._type(self.__add_group_member_field, text, delay=0)
 
-    def _group_click_on_a_searched_username(self, username: str):
-        super()._click(f"//div[@class='name_search']/b[text()='{username}']")
+    def group_click_on_a_searched_username(self, username: str):
+        self._click(f"//div[@class='name_search']/b[text()='{username}']")
 
-    def _click_on_add_member_button(self):
-        super()._click(self.__add_member_button)
+    def click_on_add_member_button(self):
+        self._click(self.__add_member_button)
