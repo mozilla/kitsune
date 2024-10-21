5e24e0b1e6377ba772c3100779a23424fcf974c7
Emil Ghitta <eghitta@mozilla.com>
2024-10-21 23:02:35 +0300

    - Improve auth_flow.py and auth_page.py classes.

--- playwright_tests/flows/auth_flows/auth_flow.py
+++ playwright_tests/flows/auth_flows/auth_flow.py
@@ -11,39 +11,64 @@ class AuthFlowPage:
 
     # Providing OTP code to FxA auth.
     def __provide_otp_code(self, otp_code: str):
+        """Add data to 'Enter your OTP code' input field and click on 'Submit' button
+
+        Args:
+            otp_code (str): OTP code to be added to the input field.
+        """
         self.auth_page.add_data_to_otp_code_input_field(otp_code)
         self.auth_page.click_on_otp_code_confirm_button()
 
     # Providing the needed login credentials to FxA auth.
     def __provide_login_credentials_and_submit(self, username: str, password: str):
+        """Provide login credentials to FxA auth and submit them.
+
+        Args:
+            username (str): Username to be added to the input field.
+            password (str): Password to be added to the input field
+        """
         self.auth_page.add_data_to_email_input_field(username)
         self.auth_page.click_on_enter_your_email_submit_button()
 
         if self.auth_page.is_logged_in_sign_in_button_displayed():
+            """If the user the sign in button is displayed, click on it."""
             self.auth_page.click_on_user_logged_in_sign_in_button()
         else:
+            """If the password input field is displayed, add the password to it and
+            click on the submit button."""
             self.auth_page.add_data_to_password_input_field(password)
             self.auth_page.click_on_enter_your_password_submit_button()
 
     def login_with_existing_session(self):
+        """Login with an existing session."""
         if self.auth_page.is_continue_with_firefox_button_displayed():
+            """If the 'Continue with Firefox Accounts' button is displayed, click on it."""
             self.auth_page.click_on_continue_with_firefox_accounts_button()
         self.auth_page.click_on_user_logged_in_sign_in_button()
 
     # Sign in flow.
     def sign_in_flow(self, username: str, account_password: str) -> str:
+        """Sign in flow.
+
+        Args:
+            username (str): Username to be used for sign in.
+            account_password (str): Password to be used for sign in.
+        """
         # Forcing an email clearance
         self.utilities.clear_fxa_email(username)
 
         if self.auth_page.is_continue_with_firefox_button_displayed():
+            """If the 'Continue with Firefox Accounts' button is displayed, click on it."""
             self.auth_page.click_on_continue_with_firefox_accounts_button()
 
         if self.auth_page.is_use_a_different_account_button_displayed():
+            """If the 'Use a different account' button is displayed, click on it."""
             self.auth_page.click_on_use_a_different_account_button()
 
         self.__provide_login_credentials_and_submit(username, account_password)
 
         if self.auth_page.is_enter_otp_code_input_field_displayed():
+            """If the OTP code input field is displayed, provide the OTP code."""
             self.__provide_otp_code(self.utilities.get_fxa_verification_code(
                 fxa_username=username))
 
