e5f228356cd4dac4463604b36c6cba0ec0fa31b5
Emil Ghitta <eghitta@mozilla.com>
2024-10-21 19:03:42 +0300

    - Improve footer.py page object. - Improve homepage.py page object. - Improve test_footer.py by using playwright's API for making the GET request to the sumo footer links.

--- playwright_tests/pages/footer.py
+++ playwright_tests/pages/footer.py
@@ -3,22 +3,26 @@ from playwright_tests.core.basepage import BasePage
 
 
 class FooterSection(BasePage):
-
     # Footer section locators.
-    __all_footer_links = "//footer//a"
-    __language_selector = "//select[@id='mzp-c-language-switcher-select']"
-    __language_selector_options = "//select[@id='mzp-c-language-switcher-select']/option"
+    FOOTER_SECTION_LOCATORS = {
+        "all_footer_links": "//footer//a",
+        "language_selector": "//select[@id='mzp-c-language-switcher-select']",
+        "language_selector_options": "//select[@id='mzp-c-language-switcher-select']/option"
+    }
 
     def __init__(self, page: Page):
         super().__init__(page)
 
     # Footer section actions.
     def get_all_footer_links(self) -> list[ElementHandle]:
-        return self._get_element_handles(self.__all_footer_links)
+        """Returns all footer links."""
+        return self._get_element_handles(self.FOOTER_SECTION_LOCATORS["all_footer_links"])
 
     def get_all_footer_locales(self) -> list[str]:
+        """Returns all footer locales."""
         return self._get_element_attribute_value(self._get_elements_locators(
-            self.__language_selector_options), "value")
+            self.FOOTER_SECTION_LOCATORS["language_selector_options"]), "value")
 
     def switch_to_a_locale(self, locale: str):
-        self._select_option_by_value(self.__language_selector, locale)
+        """Switches to a locale."""
+        self._select_option_by_value(self.FOOTER_SECTION_LOCATORS["language_selector"], locale)
