89dceac23d59c12789e00a4e545c546e97fabd1c
Ryan Johnson <escattone@gmail.com>
2025-01-13 11:53:38 -0800

    Revert "Revert "Make AlpineJS CSP friendly (#6442)" (#6447)"
    This reverts commit 3c16d1b5e62a93e2eae0c8bfc518c3046c9dbbcc.

--- kitsune/sumo/static/sumo/js/alpine.js
+++ kitsune/sumo/static/sumo/js/alpine.js
@@ -1,5 +1,7 @@
-import Alpine from 'alpinejs';
+import Alpine from "@alpinejs/csp";
 import trackEvent from "sumo/js/analytics";
+// we need to import surveyForm here so it's available to Alpine components
+import surveyForm from "sumo/js/survey_form";
 
 window.Alpine = Alpine;
 // Add trackEvent to the window object so it's available to Alpine components
