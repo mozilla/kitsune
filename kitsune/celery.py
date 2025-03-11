386e8b6cc8ddbd7822d1a1092914d56f6c3a4b08
Tasos Katsoulas <akatsoulas@gmail.com>
2025-03-11 20:13:14 +0200

    Move email related tasks in the same queue (#6553)

--- kitsune/celery.py
+++ kitsune/celery.py
@@ -8,4 +8,12 @@ from django.conf import settings  # noqa
 app = Celery("kitsune")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-app.conf.task_routes = {"post_office.tasks.send_queued_mail": {"queue": "email"}}
+app.conf.task_routes = {
+    "post_office.tasks.send_queued_mail": {"queue": "email"},
+    "kitsune.tidings.tasks.send_emails": {"queue": "email"},
+    "kitsune.messages.tasks.email_private_message": {"queue": "email"},
+    "kitsune.announcements.tasks.send_group_email": {"queue": "email"},
+    "kitsune.kbadge.tasks.send_award_notification": {"queue": "email"},
+    "kitsune.wiki.tasks.send_reviewed_notification": {"queue": "email"},
+    "kitsune.wiki.tasks.send_contributor_notification": {"queue": "email"},
+}
