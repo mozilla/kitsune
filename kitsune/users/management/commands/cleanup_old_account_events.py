f6117da83e78da13917075b80c186e3fb4728fc6
smithellis <sellis@mozilla.com>
2025-03-20 10:11:11 -0400

    lint

--- kitsune/users/management/commands/cleanup_old_account_events.py
+++ kitsune/users/management/commands/cleanup_old_account_events.py
@@ -5,11 +5,11 @@ from kitsune.users.models import AccountEvent
 
 
 class Command(BaseCommand):
-    help = 'Deletes account events that are older than two years'
+    help = "Deletes account events that are older than two years"
 
     def handle(self, *args, **options):
         two_years_ago = timezone.now() - timedelta(days=730)  # 2 years * 365 days
         deleted_count = AccountEvent.objects.filter(created_at__lt=two_years_ago).delete()[0]
         self.stdout.write(
-            self.style.SUCCESS(f'Successfully deleted {deleted_count} old account events')
-        ) 
+            self.style.SUCCESS(f"Successfully deleted {deleted_count} old account events")
+        )
