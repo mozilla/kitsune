7af79fce8e1e40618134b770e1dac071d3e5f777
Ryan Johnson <rjohnson@mozilla.com>
2025-05-13 08:10:23 -0700

    change default temperature to 1

--- kitsune/llm/__init__.py
+++ kitsune/llm/__init__.py
@@ -5,7 +5,7 @@ from langchain_google_vertexai import ChatVertexAI
 
 @cache
 def get_llm(
-    model_name: str, temperature: int = 0, max_tokens: int | None = None, max_retries: int = 2
+    model_name: str, temperature: int = 1, max_tokens: int | None = None, max_retries: int = 2
 ) -> ChatVertexAI:
     """
     Returns a LangChain chat model instance based on the given LLM model name.
