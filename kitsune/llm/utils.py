8585b424062a3ff90dc29dff8570b86736da8a00
Ryan Johnson <escattone@gmail.com>
2025-05-29 14:14:45 -0700

    move import to avoid slow startup due to creds check (#6690)

--- kitsune/llm/utils.py
+++ kitsune/llm/utils.py
@@ -1,6 +1,6 @@
 from functools import cache
 
-from langchain_google_vertexai import ChatVertexAI
+from langchain.chat_models.base import BaseChatModel
 
 
 @cache
@@ -9,10 +9,12 @@ def get_llm(
     temperature: int = 1,
     max_tokens: int | None = None,
     max_retries: int = 2,
-) -> ChatVertexAI:
+) -> BaseChatModel:
     """
     Returns a LangChain chat model instance based on the given LLM model name.
     """
+    from langchain_google_vertexai import ChatVertexAI
+
     return ChatVertexAI(
         model=model_name, temperature=temperature, max_tokens=max_tokens, max_retries=max_retries
     )
