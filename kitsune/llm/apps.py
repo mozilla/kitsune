0fb9d63e2969469921f549ba2e4974e158cb472a
Tasos Katsoulas <akatsoulas@gmail.com>
2025-05-30 12:52:04 +0300

    Prewarm LLM cache

--- kitsune/llm/apps.py
+++ kitsune/llm/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class LLMConfig(AppConfig):
     name = "kitsune.llm"
     default_auto_field = "django.db.models.AutoField"
+
+    def ready(self):
+        from kitsune.llm.utils import get_llm
+
+        # pre-warm the LLM cache
+        get_llm()
