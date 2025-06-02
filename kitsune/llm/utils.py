b49ae25458cf33b75162ad2c56e2dc7b60ba2c8a
Tasos Katsoulas <akatsoulas@gmail.com>
2025-06-02 23:02:32 +0200

    Use lru_cache of size 1 (#6697)

--- kitsune/llm/utils.py
+++ kitsune/llm/utils.py
@@ -1,11 +1,13 @@
-from functools import cache
+from functools import lru_cache
 
 from langchain.chat_models.base import BaseChatModel
 
+DEFAULT_LLM_MODEL = "gemini-2.5-flash-preview-04-17"
 
-@cache
+
+@lru_cache(maxsize=1)
 def get_llm(
-    model_name: str,
+    model_name: str = DEFAULT_LLM_MODEL,
     temperature: int = 1,
     max_tokens: int | None = None,
     max_retries: int = 2,
