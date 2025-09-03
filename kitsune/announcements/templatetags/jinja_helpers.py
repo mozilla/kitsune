a063d0e99110ffbed7f315e12407a2d1781f74d5
smith <sellis@mozilla.com>
2025-09-03 11:21:12 -0400

    2488 - Logged out users should see announcments (#6878)
    - Allow logged out users to see announcements that that are
    locale restricted

--- kitsune/announcements/templatetags/jinja_helpers.py
+++ kitsune/announcements/templatetags/jinja_helpers.py
@@ -9,9 +9,10 @@ def get_announcements(request):
     user = request.user if request.user.is_authenticated else None
 
     user_platforms = detect_platform_from_user_agent(request)
+    current_locale = request.LANGUAGE_CODE
 
     if user:
         user_groups = user.groups.values_list("id", flat=True)
         return Announcement.get_for_groups(user_groups, platform_slugs=user_platforms)
 
-    return Announcement.get_site_wide(platform_slugs=user_platforms)
+    return Announcement.get_site_wide(platform_slugs=user_platforms, locale_name=current_locale)
