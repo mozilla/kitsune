42c9918b99398e53480d7bf497737fc81f9d759f
Tasos Katsoulas <akatsoulas@gmail.com>
2026-01-14 13:54:05 +0200

    Treabeard scaffolding

--- kitsune/groups/models.py
+++ kitsune/groups/models.py
@@ -3,14 +3,38 @@ from django.contrib.auth.models import Group, User
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.utils.translation import gettext_lazy as _lazy
+from treebeard.mp_tree import MP_Node
 
 from kitsune.sumo.models import ModelBase
 from kitsune.sumo.urlresolvers import reverse
 from kitsune.wiki.parser import wiki_to_html
 
 
-class GroupProfile(ModelBase):
-    """Profile model for groups."""
+class TreeModelBase(MP_Node, ModelBase):
+    """
+    Abstract base for tree-based models in Kitsune.
+
+    Combines treebeard's MP_Node (tree operations) with Kitsune's
+    ModelBase (objects_range, update methods).
+
+    Provides:
+    - Tree operations: get_parent(), get_children(), add_child(), etc.
+    - ModelBase methods: objects_range(), update()
+
+    If another app needs tree hierarchy, this can be moved to
+    kitsune.sumo.models for wider reuse.
+    """
+
+    class Meta:
+        abstract = True
+
+
+class GroupProfile(TreeModelBase):
+    """Profile model for groups with hierarchy support."""
+
+    path = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    depth = models.PositiveIntegerField(null=True, blank=True)
+    numchild = models.PositiveIntegerField(default=0, null=True, blank=True)
 
     slug = models.SlugField(unique=True, editable=False, blank=False, null=False, max_length=80)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="profile")
