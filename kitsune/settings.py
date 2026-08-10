0a0bf0be690621f13c1ade4dcd41415a33c96c31
denyshon <denyshon@outlook.com>
2026-08-10 05:54:05 +0200

    Improve karma title l10n comment

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -402,7 +402,7 @@ DB_LOCALIZE = {
     "karma": {
         "Title": {
             "attrs": ["name"],
-            "comments": ["This is a karma title."],
+            "comments": ["This is a karma title, displayed for eligible users next to their username on the forums."],
         }
     },
     "products": {
